6285c4ae06b35bdc307cdd45866fa6db12cdd576
Nicolas GAILLY <nikkolasg@gmail.com>
2015-09-09 23:02:19 +0200

    master public poly added

--- protocols/shamir/shamir.go
+++ protocols/shamir/shamir.go
@@ -5,11 +5,13 @@ import (
 	"github.com/dedis/crypto/abstract"
 	"github.com/dedis/crypto/config"
 	"github.com/dedis/crypto/edwards"
+	"github.com/dedis/crypto/poly"
 	"github.com/dedis/crypto/poly/promise"
 	"github.com/dedis/crypto/random"
 	"os"
 )
 
+/////////////// HELPERS FUNCTIONS ////////////////
 var suite = edwards.NewAES128SHA256Ed25519(false)
 
 func produceKeyPair() *config.KeyPair {
@@ -34,6 +36,10 @@ func producePublicList(insurerKeys []*config.KeyPair) []abstract.Point {
 	}
 	return arr
 }
+
+//////////////// END OF HELPERS FUNCTIONS ///////////////
+
+/////////////// JOINT SECRET SHARING ///////////////////
 
 type ShamirInfo struct {
 	T int // how many insurer needed to reconstruct private key
@@ -63,6 +69,8 @@ type JointPromiser struct {
 
 	// info about the promise (and related response) for each other peer
 	PeerDatas []PeerData
+
+	MasterPubPoly *poly.PubPoly
 }
 
 // produce an array of JointPromiser struct that each one has the other promises ==> Matrix !
@@ -85,10 +93,10 @@ func produceJointPromiserMatrix(secretKeys, promiserKeys []*config.KeyPair, publ
 		insurerList := make([]abstract.Point, n, n)
 		copy(insurerList, publicPeerKeys)
 
-		//fmt.Printf("Insurerlist for peer %d : %v\n", i, insurerList)
+		//fmt.Printf("[+]Insurerlist for peer %d : %v\n", i, insurerList)
 		jp.Promise = new(promise.Promise).ConstructPromise(jp.PromiserKeyPair, jp.SecretKey, info.T, info.R, insurerList)
 		jp.State = new(promise.State).Init(*jp.Promise)
-		//fmt.Printf("Promise created for peer %d : %v\n", i, jp.Promise)
+		//fmt.Printf("[+] Promise created for peer %d : %v\n", i, jp.Promise)
 	}
 
 	// Then for each peer i, reference every other promises of peer j(thus the matrix)
@@ -100,8 +108,8 @@ func produceJointPromiserMatrix(secretKeys, promiserKeys []*config.KeyPair, publ
 			}
 			jp.PeerDatas[j] = peerData
 		}
-		//fmt.Printf("Peer Data (peer %d) Promises len(%d) %+v\n", i, len(jp.PeerDatas), jp.PeerDatas[:])
-		//fmt.Printf("*** JointPromise[0].Promise == JointPromise[0].PeerData[0].Promise ? %+v", matrix[0].Promise.Equal(matrix[0].PeerDatas[0].Promise))
+		//fmt.Printf("[+] Peer Data (peer %d) Promises len(%d) %+v\n", i, len(jp.PeerDatas), jp.PeerDatas[:])
+		//fmt.Printf("[+] *** JointPromise[0].Promise == JointPromise[0].PeerData[0].Promise ? %+v", matrix[0].Promise.Equal(matrix[0].PeerDatas[0].Promise))
 	}
 	return matrix
 }
@@ -127,7 +135,7 @@ func produceSharesMatrix(promisers []*JointPromiser) {
 			// peer i produce response share for pubpoly j at index i PUBj(i)
 			resp, err := peerData.Promise.ProduceResponse(peerData.Index, jp.SecretKey)
 			if err != nil {
-				fmt.Printf("Peer %d producing response share error (index %d,secretKey:%+v)  : %v\n", i, peerData.Index, jp.SecretKey, err)
+				fmt.Printf("[+] Peer %d producing response share error (index %d,secretKey:%+v)  : %v\n", i, peerData.Index, jp.SecretKey, err)
 				os.Exit(1)
 			}
 			jp.PeerDatas[j].Response = resp
@@ -137,58 +145,80 @@ func produceSharesMatrix(promisers []*JointPromiser) {
 
 // this function will , for each peer i, add every responses of peers j to its promise to see if its verified
 func certifyMatrix(matrix []*JointPromiser) {
-	var err error
+	var err error = nil
 	for i, jp := range matrix {
 		state := jp.State
 		for j, jjp := range matrix {
 			resp := jjp.PeerDatas[i].Response
 			err = state.AddResponse(jjp.PeerDatas[i].Index, resp)
 			if err != nil {
-				fmt.Printf("Error while certifying matrix shares : Peer %d certifying response from peer %d (err: %v)\n", i, j, err)
+				fmt.Printf("[-] Error while certifying matrix shares : Peer %d certifying response from peer %d (err: %v)\n", i, j, err)
 			}
 		}
 
 		err = state.PromiseCertified()
 		if err != nil {
-			fmt.Printf("Peer %d could not certified its promise : %v\n", i, err)
+			fmt.Printf("[+] Peer %d could not certified its promise : %v\n", i, err)
 			os.Exit(1)
 		}
-		fmt.Printf("Peer %d successfully CERTIFIED its promise !\n", i)
 	}
+	fmt.Printf("[+] All Peers successfully CERTIFIED their promises !\n")
+}
+
+// Produce the master pub polynomial which consists off adding off each PubPoly's peer
+func produceMasterPublicPoly(matrix []*JointPromiser) {
+	for i, jp := range matrix {
+		master := jp.PeerDatas[0].Promise.PubPoly()
+		// range over the promises of each peers
+		for _, peerData := range jp.PeerDatas[1:] {
+			// Add homomorphically the different pubpoly to produce the master
+			master = master.Add(master, peerData.Promise.PubPoly())
+		}
+		matrix[i].MasterPubPoly = master
+	}
+	// verify that we get the same Master PubPoly for each one
+	for i, jp := range matrix {
+
+		for j, jjp := range matrix {
+			if i == j {
+				continue
+			}
+			if !jp.MasterPubPoly.Equal(jjp.MasterPubPoly) {
+				fmt.Printf("[-] Error while checking the Master PubPoly for peer %d vs %d : Not Equals ...\n", i, j)
+				os.Exit(1)
+			}
+		}
+	}
+
+	fmt.Printf("[+] Master PubPoly successfully created and verified amongst each peer !")
 }
 
 func jointShamirPromiseTest() {
 	fmt.Printf("########## Shamir Promiser Test ###########\n")
 	info := ShamirInfo{
-		T: 2,
-		R: 2,
-		N: 3,
+		T: 5,
+		R: 7,
+		N: 10,
 	}
-	fmt.Printf("Shamir Configuration : T = %d, R = %d, N = %d\n", info.T, info.R, info.N)
+	fmt.Printf("[+] Shamir Configuration : T = %d, R = %d, N = %d\n", info.T, info.R, info.N)
 	// every peer's priv/pub long term key pair
 	peerSecretKeys := produceKeyPairList(info.N)
-	for i, pk := range peerSecretKeys {
-		fmt.Printf("Long Term priv/pub key of peer %d : %+v\n", i, *pk)
-	}
 	// list of peer's long term public key
 	publicPeerKeys := producePublicList(peerSecretKeys)
-	fmt.Printf("Public Peer Key List :\n%+v\n", publicPeerKeys)
 
 	// every peer' priv/pub "promiser role" key pair
 	promiserKeys := produceKeyPairList(info.N)
-	for i, pk := range promiserKeys {
-		fmt.Printf("Promiser priv/pub key of peer %d : %+v\n", i, *pk)
-	}
 
 	// create JointPromiser structure ==> Matrix of peers with each having the other's promises
 	matrix := produceJointPromiserMatrix(peerSecretKeys, promiserKeys, publicPeerKeys, info)
 
-	fmt.Printf("Matrix of Joint Promiser constructed (%d x %d) ...\n", len(matrix), len(matrix))
-	fmt.Printf("Is Matrix valid ? %+v\n", verifyMatrix(matrix))
+	fmt.Printf("[+] Matrix of Joint Promiser constructed (%d x %d) ...\n", len(matrix), len(matrix))
+	fmt.Printf("[+] Is Matrix valid ? %+v\n", verifyMatrix(matrix))
 	// INSURERS PART
 	produceSharesMatrix(matrix)
-	fmt.Printf("Matrix of Shares constructed ...\n")
+	fmt.Printf("[+] Matrix of Shares constructed ...\n")
 	certifyMatrix(matrix)
+	produceMasterPublicPoly(matrix)
 
 }
 
@@ -228,7 +258,7 @@ func singleShamirPromiseTest() {
 		//insurer point of view : construct response from own private key
 		responses[i], err = basicPromise.ProduceResponse(i, insurerKeys[i])
 		if err != nil {
-			fmt.Printf("Error while producing response share from insurers %d : %v\n", i, err)
+			fmt.Printf("[-] Error while producing response share from insurers %d : %v\n", i, err)
 			os.Exit(1)
 		}
 	}
@@ -239,13 +269,13 @@ func singleShamirPromiseTest() {
 	for i, resp := range responses {
 		err = state.AddResponse(i, resp)
 		if err != nil {
-			fmt.Printf("Error while adding response share %d to promiser's promise : %v\n", i, err)
+			fmt.Printf("[-] Error while adding response share %d to promiser's promise : %v\n", i, err)
 		}
 	}
 	fmt.Printf("** Promiser **\tAdded all responses to the state's promise.\n")
 	err = state.PromiseCertified()
 	if err != nil {
-		fmt.Printf("Error: Promiser's promise is not certified !!\n")
+		fmt.Printf("[-] Error: Promiser's promise is not certified !!\n")
 		os.Exit(1)
 	} else {
 		fmt.Printf("** Promiser **\tPromiser's promise IS INDEED Certified ;)\n")
@@ -254,10 +284,21 @@ func singleShamirPromiseTest() {
 	for i := 0; i < N; i++ {
 		secret, err := state.RevealShare(i, insurerKeys[i])
 		if err != nil {
-			fmt.Printf("Error while revealing private share from insurer %d : %v", i, err)
+			fmt.Printf("[-] Error while revealing private share from insurer %d : %v", i, err)
 		}
 		fmt.Printf("** Insurer  **\tProduced response share %d : %v\n", i, secret)
+		state.PriShares.SetShare(i, secret)
 	}
+
+	reconstructedSecret := state.PriShares.Secret()
+	if reconstructedSecret.Equal(secretKey.Secret) {
+		fmt.Printf("** Promiser / Client ** Reconstruction of the secret succeeded !!\n")
+	} else {
+		fmt.Printf("** Promiser / Client ** Reconstruction of the secret failed :/\n")
+		os.Exit(1)
+	}
+
+	fmt.Printf("Public Poly of Promise : %+v\n", basicPromise.PubPoly())
 
 }
 
