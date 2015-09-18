cc8611436de55f8479b9197f3195140d414a1642
Nicolas GAILLY <nikkolasg@gmail.com>
2015-09-18 10:44:35 +0200

    questions on config/structs.go

--- lib/config/structs.go
+++ lib/config/structs.go
@@ -1,8 +1,8 @@
 package config
+
 import "github.com/dedis/cothority/lib/graphs"
 
 // This file has only the structures in it, for easy references
-
 
 type ConfigFile struct {
 	Hosts []string     `json:"hosts"`
@@ -15,15 +15,14 @@ type ConfigFileOld struct {
 	Tree  *Node    `json:"tree"`
 }
 
-type AppConfig struct{
-	Hostname string
-	Logger string
-	PhysAddr string
-	AmRoot bool
-	TestConnect bool
-	App string
-	Mode string
-	Name string
-	Server string
+type AppConfig struct {
+	Hostname    string // Hostname like server-0.cs-dissent ?
+	Logger      string // ip addr of the logger to connect to
+	PhysAddr    string // physical IP addr of the host
+	AmRoot      bool   // is the host root (i.e. special operations)
+	TestConnect bool   // ?
+	App         string // which app are we running on this host
+	Mode        string // ?
+	Name        string // ?
+	Server      string // ?
 }
-
