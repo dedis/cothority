499efb2cc137da119e2b5520ce6ab5e2c042ac4b
Nicolas GAILLY <nikkolasg@gmail.com>
2015-09-22 15:20:39 +0200

    copying files with for-loop instead of cp

--- deploy/deploy_deterlab.go
+++ deploy/deploy_deterlab.go
@@ -22,52 +22,51 @@ import (
 	"os/exec"
 	"sync"
 
-	"github.com/dedis/cothority/lib/cliutils"
-	dbg "github.com/dedis/cothority/lib/debug_lvl"
-	"fmt"
-	"strings"
-	"io/ioutil"
-	"github.com/dedis/cothority/lib/graphs"
-	"encoding/json"
-	"strconv"
-	"bytes"
-	"github.com/BurntSushi/toml"
-	"time"
-	_ "errors"
-	"github.com/dedis/cothority/lib/config"
 	"bufio"
+	"bytes"
+	"encoding/json"
+	_ "errors"
+	"fmt"
+	"github.com/BurntSushi/toml"
+	"github.com/dedis/cothority/lib/cliutils"
+	"github.com/dedis/cothority/lib/config"
+	dbg "github.com/dedis/cothority/lib/debug_lvl"
+	"github.com/dedis/cothority/lib/graphs"
+	"io/ioutil"
 	"path"
+	"strconv"
+	"strings"
+	"time"
 )
 
-
 type Deter struct {
-	Config       *Config
+	Config *Config
 	// The login on the platform
-	Login        string
+	Login string
 	// The outside host on the platform
-	Host         string
+	Host string
 	// The name of the internal hosts
-	Project      string
+	Project string
 	// Directory where everything is copied into
-	DeployDir    string
+	DeployDir string
 	// Directory for building
-	BuildDir     string
+	BuildDir string
 	// Working directory of deterlab
-	DeterDir     string
+	DeterDir string
 	// Where the main logging machine resides
 	masterLogger string
 	// DNS-resolvable names
-	phys         []string
+	phys []string
 	// VLAN-IP names
-	virt         []string
-	physOut      string
-	virtOut      string
+	virt    []string
+	physOut string
+	virtOut string
 
 	// Channel to communication stopping of experiment
-	sshDeter     chan string
+	sshDeter chan string
 
 	// Testing the connection?
-	TestConnect	bool
+	TestConnect bool
 }
 
 func (d *Deter) Configure(config *Config) {
@@ -85,7 +84,7 @@ func (d *Deter) Configure(config *Config) {
 	d.checkDeterlabVars()
 }
 
-func (d *Deter) Build(build string) (error) {
+func (d *Deter) Build(build string) error {
 	dbg.Lvl1("Building for", d.Login, d.Host, d.Project, build)
 	start := time.Now()
 
@@ -143,7 +142,7 @@ func (d *Deter) Build(build string) (error) {
 	return nil
 }
 
-func (d *Deter) Deploy() (error) {
+func (d *Deter) Deploy() error {
 	dbg.Lvl1("Assembling all files and configuration options")
 	os.RemoveAll(d.DeployDir)
 	os.Mkdir(d.DeployDir, 0777)
@@ -154,15 +153,22 @@ func (d *Deter) Deploy() (error) {
 	d.WriteConfig()
 
 	// copy the webfile-directory of the logserver to the remote directory
-	err := exec.Command("cp", "-a", d.DeterDir + "/logserver/webfiles", d.BuildDir + "/",
-		d.DeterDir + "/cothority.conf", d.DeployDir).Run()
+	err := exec.Command("cp", "-a", d.DeterDir+"/logserver/webfiles",
+		d.DeterDir+"/cothority.conf", d.DeployDir).Run()
 	if err != nil {
-		log.Fatal("error copying webfiles and build-dir:", err)
+		log.Fatal("error copying webfiles:", err)
+	}
+	build, err := ioutil.ReadDir(d.BuildDir)
+	for _, file := range build {
+		err = exec.Command("cp", d.BuildDir+"/"+file.Name(), d.DeployDir).Run()
+		if err != nil {
+			log.Fatal("error copying build-file:", err)
+		}
 	}
 
 	dbg.Lvl1("Copying over to", d.Login, "@", d.Host)
 	// Copy everything over to deterlabs
-	err = cliutils.Rsync(d.Login, d.Host, d.DeployDir + "/", "remote/")
+	err = cliutils.Rsync(d.Login, d.Host, d.DeployDir+"/", "remote/")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -175,9 +181,9 @@ func (d *Deter) Deploy() (error) {
 	return nil
 }
 
-func (d *Deter) Start() (error) {
+func (d *Deter) Start() error {
 	dbg.Lvl1("Running with", d.Config.Nmachs, "nodes *", d.Config.Hpn, "hosts per node =",
-		d.Config.Nmachs * d.Config.Hpn, "and", d.Config.Nloggers, "loggers")
+		d.Config.Nmachs*d.Config.Hpn, "and", d.Config.Nloggers, "loggers")
 
 	// setup port forwarding for viewing log server
 	dbg.Lvl3("setup port forwarding for master logger: ", d.masterLogger, d.Login, d.Host)
@@ -187,7 +193,7 @@ func (d *Deter) Start() (error) {
 		"-t",
 		fmt.Sprintf("%s@%s", d.Login, d.Host),
 		"-L",
-		"8081:" + d.masterLogger + ":10000")
+		"8081:"+d.masterLogger+":10000")
 	err := cmd.Start()
 	if err != nil {
 		log.Fatal("failed to setup portforwarding for logging server")
@@ -200,17 +206,17 @@ func (d *Deter) Start() (error) {
 
 	go func() {
 		dbg.Lvl3(cliutils.SshRunStdout(d.Login, d.Host,
-			"GOMAXPROCS=8 remote/deter -nmsgs=" + strconv.Itoa(d.Config.Nmsgs) +
-			" -hpn=" + strconv.Itoa(d.Config.Hpn) +
-			" -bf=" + strconv.Itoa(d.Config.Bf) +
-			" -rate=" + strconv.Itoa(d.Config.Rate) +
-			" -rounds=" + strconv.Itoa(d.Config.Rounds) +
-			" -debug=" + strconv.Itoa(d.Config.Debug) +
-			" -failures=" + strconv.Itoa(d.Config.Failures) +
-			" -rfail=" + strconv.Itoa(d.Config.RFail) +
-			" -ffail=" + strconv.Itoa(d.Config.FFail) +
-			" -app=" + d.Config.App +
-			" -suite=" + d.Config.Suite))
+			"GOMAXPROCS=8 remote/deter -nmsgs="+strconv.Itoa(d.Config.Nmsgs)+
+				" -hpn="+strconv.Itoa(d.Config.Hpn)+
+				" -bf="+strconv.Itoa(d.Config.Bf)+
+				" -rate="+strconv.Itoa(d.Config.Rate)+
+				" -rounds="+strconv.Itoa(d.Config.Rounds)+
+				" -debug="+strconv.Itoa(d.Config.Debug)+
+				" -failures="+strconv.Itoa(d.Config.Failures)+
+				" -rfail="+strconv.Itoa(d.Config.RFail)+
+				" -ffail="+strconv.Itoa(d.Config.FFail)+
+				" -app="+d.Config.App+
+				" -suite="+d.Config.Suite))
 		dbg.Lvl3("Sending stop of ssh")
 		d.sshDeter <- "stop"
 	}()
@@ -218,7 +224,7 @@ func (d *Deter) Start() (error) {
 	return nil
 }
 
-func (d *Deter) Stop() (error) {
+func (d *Deter) Stop() error {
 	killssh := exec.Command("pkill", "-f", "ssh -t -t")
 	killssh.Stdout = os.Stdout
 	killssh.Stderr = os.Stderr
@@ -230,7 +236,7 @@ func (d *Deter) Stop() (error) {
 	case msg := <-d.sshDeter:
 		if msg == "stop" {
 			dbg.Lvl3("SSh is stopped")
-		}else {
+		} else {
 			dbg.Lvl1("Received other command", msg)
 		}
 	case <-time.After(time.Second * 3):
@@ -248,7 +254,7 @@ func (d *Deter) WriteConfig(dirOpt ...string) {
 	if len(dirOpt) > 0 {
 		dir = dirOpt[0]
 	}
-	err := ioutil.WriteFile(dir + "/config.toml", buf.Bytes(), 0660)
+	err := ioutil.WriteFile(dir+"/config.toml", buf.Bytes(), 0660)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -279,7 +285,7 @@ func ReadConfig(dirOpt ...string) (*Deter, error) {
 * Write the hosts.txt file automatically
 * from project name and number of servers
  */
-func (d *Deter)generateHostsFile() error {
+func (d *Deter) generateHostsFile() error {
 	hosts_file := d.DeployDir + "/hosts.txt"
 	num_servers := d.Config.Nmachs + d.Config.Nloggers
 
@@ -300,7 +306,7 @@ func (d *Deter)generateHostsFile() error {
 	ip := "10.255.0."
 	name := "SAFER.isi.deterlab.net"
 	for i := 1; i <= num_servers; i++ {
-		f.WriteString(fmt.Sprintf("server-%d.%s.%s\t%s%d\n", i - 1, d.Project, name, ip, i))
+		f.WriteString(fmt.Sprintf("server-%d.%s.%s\t%s%d\n", i-1, d.Project, name, ip, i))
 	}
 	dbg.Lvl4(fmt.Sprintf("Created hosts file description (%d hosts)", num_servers))
 	return err
@@ -310,7 +316,7 @@ func (d *Deter)generateHostsFile() error {
 // parse the hosts.txt file to create a separate list (and file)
 // of physical nodes and virtual nodes. Such that each host on line i, in phys.txt
 // corresponds to each host on line i, in virt.txt.
-func (d *Deter)readHosts() {
+func (d *Deter) readHosts() {
 	hosts_file := d.DeployDir + "/hosts.txt"
 	nmachs, nloggers := d.Config.Nmachs, d.Config.Nloggers
 
@@ -319,14 +325,14 @@ func (d *Deter)readHosts() {
 		log.Panic("Couldn't find", hosts_file)
 	}
 
-	d.phys = make([]string, 0, len(physVirt) / 2)
-	d.virt = make([]string, 0, len(physVirt) / 2)
+	d.phys = make([]string, 0, len(physVirt)/2)
+	d.virt = make([]string, 0, len(physVirt)/2)
 	for i := 0; i < len(physVirt); i += 2 {
 		d.phys = append(d.phys, physVirt[i])
-		d.virt = append(d.virt, physVirt[i + 1])
+		d.virt = append(d.virt, physVirt[i+1])
 	}
-	d.phys = d.phys[:nmachs + nloggers]
-	d.virt = d.virt[:nmachs + nloggers]
+	d.phys = d.phys[:nmachs+nloggers]
+	d.virt = d.virt[:nmachs+nloggers]
 	d.physOut = strings.Join(d.phys, "\n")
 	d.virtOut = strings.Join(d.virt, "\n")
 	d.masterLogger = d.phys[0]
@@ -335,19 +341,19 @@ func (d *Deter)readHosts() {
 
 	// phys.txt and virt.txt only contain the number of machines that we need
 	dbg.Lvl3("Reading phys and virt")
-	err = ioutil.WriteFile(d.DeployDir + "/phys.txt", []byte(d.physOut), 0666)
+	err = ioutil.WriteFile(d.DeployDir+"/phys.txt", []byte(d.physOut), 0666)
 	if err != nil {
 		log.Fatal("failed to write physical nodes file", err)
 	}
 
-	err = ioutil.WriteFile(d.DeployDir + "/virt.txt", []byte(d.virtOut), 0666)
+	err = ioutil.WriteFile(d.DeployDir+"/virt.txt", []byte(d.virtOut), 0666)
 	if err != nil {
 		log.Fatal("failed to write virtual nodes file", err)
 	}
 }
 
 // Calculates a tree that is used for the timestampers
-func (d *Deter)calculateGraph() {
+func (d *Deter) calculateGraph() {
 	d.virt = d.virt[d.Config.Nloggers:]
 	d.phys = d.phys[d.Config.Nloggers:]
 	t, hostnames, depth, err := graphs.TreeFromList(d.virt, d.Config.Hpn, d.Config.Bf)
@@ -356,14 +362,14 @@ func (d *Deter)calculateGraph() {
 	total := d.Config.Nmachs * d.Config.Hpn
 	if len(hostnames) != total {
 		dbg.Lvl1("Only calculated", len(hostnames), "out of", total, "hosts - try changing number of",
-		"machines or hosts per node")
+			"machines or hosts per node")
 		log.Fatal("Didn't calculate enough hosts")
 	}
 
 	// generate the configuration file from the tree
 	cf := config.ConfigFromTree(t, hostnames)
 	cfb, err := json.Marshal(cf)
-	err = ioutil.WriteFile(d.DeployDir + "/tree.json", cfb, 0666)
+	err = ioutil.WriteFile(d.DeployDir+"/tree.json", cfb, 0666)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -373,7 +379,7 @@ func (d *Deter)calculateGraph() {
 // ask on the command-line.
 // For the login-variable, it will try to set up a connection to d.Host and copy over the
 // public key for a more easy communication
-func (d *Deter)checkDeterlabVars() {
+func (d *Deter) checkDeterlabVars() {
 	// Write
 	var config, err = ReadConfig(d.DeterDir)
 
@@ -389,7 +395,7 @@ func (d *Deter)checkDeterlabVars() {
 	}
 
 	if config.Login == "" {
-		d.Login = readString("Please enter the login-name on " + d.Host + ":", "")
+		d.Login = readString("Please enter the login-name on "+d.Host+":", "")
 	} else {
 		d.Login = config.Login
 	}
@@ -404,7 +410,7 @@ func (d *Deter)checkDeterlabVars() {
 }
 
 // Shows a messages and reads in a string, eventually returning a default (dft) string
-func readString(msg, dft string) (string) {
+func readString(msg, dft string) string {
 	fmt.Print(msg)
 
 	reader := bufio.NewReader(os.Stdin)
