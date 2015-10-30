c53ee3fc45ad0564a535bb003873ad4f483e7382
Linus Gasser <linus.gasser@epfl.ch>
2015-10-30 08:59:57 +0100

    add an exit-command and get the script to update afterwards

--- app/conode/check.go
+++ app/conode/check.go
@@ -22,9 +22,21 @@ func init() {
 		Usage:       "Check a host to determine if it is a valid node to get incorporated into the cothority tree.",
 		Description: "It checks the public key given and the availability of the server. It will be contacted multiple times a day during 24 hours",
 		ArgsUsage:   "Public-key-file : file where reside the public key of the host to check",
+		Subcommands: []cli.Command{
+			{
+				Name:  "exit",
+				Usage: "Asks the remote node to exit",
+				Action: func(c *cli.Context) {
+					if c.Args().First() == "" {
+						dbg.Fatal("No public key file given for exit.")
+					}
+					CheckExit(c.Args().First())
+				},
+			},
+		},
 		Action: func(c *cli.Context) {
 			if c.Args().First() == "" {
-				dbg.Fatal("No public key file given !!")
+				dbg.Fatal("No public key file given for check.")
 			}
 			Check(c.Args().First())
 		},
@@ -34,18 +46,50 @@ func init() {
 
 // Main entry point for the check mode
 func Check(pubKeyFile string) {
+	// Verifies the remote host and returns the status
+	conn, ack := verifyHost(pubKeyFile)
+	defer conn.Close()
+
+	if err := suite.Write(conn, ack); err != nil {
+		dbg.Fatal("Error writing back the ACK : ", err)
+	}
+}
+
+// Main entry point for the check mode
+func CheckExit(pubKeyFile string) {
+	// Verifies the remote host and returns the status
+	conn, ack := verifyHost(pubKeyFile)
+	defer conn.Close()
+
+	// We only ask the node to exit if everything is OK
+	if ack.Code != SYS_OK{
+		dbg.Fatal("Not correct key-file?")
+	} else {
+		ack.Code = SYS_EXIT
+	}
+
+	// Finally send the message
+	dbg.Lvl1("Sending exit to node")
+	if err := suite.Write(conn, ack); err != nil {
+		dbg.Fatal("Error asking for exit : ", err)
+	}
+}
+
+// verifyHost will anaylze the systempacket information and verify the signature
+// It will return a ACK properly initialized with the right codes in it.
+func verifyHost(pubKeyFile string) (net.Conn, Ack ) {
 	//  get the right public key
 	pub, host, err := cliutils.ReadPubKey(suite, pubKeyFile)
 	if err != nil {
 		dbg.Fatal("Could not read the public key from the file : ", err)
 	}
 	dbg.Lvl1("Public key file read")
+
 	// Then get a connection
 	conn, err := net.Dial("tcp", host)
 	if err != nil {
 		dbg.Fatal("Error when getting the connection to the host : ", err)
 	}
-	defer conn.Close()
 
 	dbg.Lvl1("Verifier connected to the host. Validation in progress...")
 	// Get the system packet message
@@ -64,19 +108,6 @@ func Check(pubKeyFile string) {
 		dbg.Fatal("Error reading the signature :", err)
 	}
 
-	// analyse the results and send back the corresponding ack
-	ack := verifyHost(sys, sig, pub)
-	if err = suite.Write(conn, ack); err != nil {
-		dbg.Fatal("Error writing back the ACK : ", err)
-	}
-}
-
-// verifyHost will anaylze the systempacket information and verify the signature
-// Of course, it needs the public key to verify it
-// It will return a ACK properly initialized with the right codes in it.
-func verifyHost(sys SystemPacket, sig []byte, pub abstract.Point) Ack {
-	var ack Ack
-	ack.Type = TYPE_SYS
 	// First, encode the sys packet
 	var b bytes.Buffer
 	if err := suite.Write(&b, sys); err != nil {
@@ -84,9 +115,13 @@ func verifyHost(sys SystemPacket, sig []byte, pub abstract.Point) Ack {
 	}
 	X := make([]abstract.Point, 1)
 	X[0] = pub
+
 	// Verify signature
+	var ack Ack
+	ack.Type = TYPE_SYS
+	ack.Code = SYS_EXIT
 	if _, err := anon.Verify(suite, b.Bytes(), anon.Set(X), nil, sig); err != nil {
-		// Wrong sig ;(
+		// Wrong signature
 		ack.Code = SYS_WRONG_SIG
 		dbg.Lvl1("WARNING : signature provided is wrong.")
 	} else {
@@ -95,5 +130,5 @@ func verifyHost(sys SystemPacket, sig []byte, pub abstract.Point) Ack {
 		dbg.Lvl1("Host's signature verified and system seems healty. OK")
 	}
 
-	return ack
+	return conn, ack
 }
