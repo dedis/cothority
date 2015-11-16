0028a3f4a74661b5432efa8fb70c26d7ed15936e
Linus Gasser <linus.gasser@epfl.ch>
2015-11-16 08:58:59 +0100

    simulations for the ieee-security submission

--- deploy/matplotlib/stats.py
+++ deploy/matplotlib/stats.py
@@ -9,6 +9,7 @@ import unittest
 csv.register_dialect('deploy', delimiter=',', doublequote=False, quotechar='', lineterminator='\n', escapechar='',
                      quoting=csv.QUOTE_NONE, skipinitialspace=True)
 
+
 class CSVStats:
     x = []
 
@@ -16,10 +17,9 @@ class CSVStats:
     # also fills in xmin, xmax, ymin and ymax which are
     # valid over multiple calls to readCVS!
     # If you want to start a new set, put xmin = -1
-    def __init__(self, file, x_id = 0):
+    def __init__(self, file, x_id=0):
         self.x = []
         self.columns = {}
-        self.reset_min_max()
         # Read in all lines of the CSV and store in the arrays
         with open(file) as csvfile:
             reader = csv.DictReader(csvfile, dialect='deploy')
@@ -27,52 +27,49 @@ class CSVStats:
                 for column, value in row.iteritems():
                     if not column in self.columns:
                         self.columns[column] = []
-                    self.columns[column] += [int(value)]
+                    self.columns[column] += [float(value)]
 
         if type(x_id) == str:
-            print "String: " + x_id
             self.x = self.columns[x_id]
         else:
-            print "Int: " + str(x_id) + " - " + ":".join(self.columns.keys())
             col = sorted(self.columns.keys())[x_id]
-            print "Col: " + col
             self.x = self.columns[col]
 
-    # Updates (x|y)(max|min) with the given column
-    def update_values(self, column):
+    # Returns a Values-object with the requested column.
+    # Updates the self.(x|y)(min|max)
+    def get_values(self, column):
+        values = Values(self.x, column, self.columns)
+        return values
+
+
+    @staticmethod
+    def get_min_max(*vals):
+        values_y = []
+        values_x = []
+        for v in vals:
+            values_y += [v.ymin, v.ymax]
+            values_x += v.x
+        return (min(values_x), max(values_x),min(values_y), max(values_y))
+
+
+# Value holds the min / max / avg / dev for a single named value
+class Values:
+    def __init__(self, x, column, columns):
+        self.name = column
+        self.x = x
+
         # Set min, max, avg, dev-values from csv-file
-        self.min = self.columns[column + "_min"]
-        self.max = self.columns[column + "_max"]
-        self.avg = self.columns[column + "_avg"]
-        self.dev = self.columns[column + "_dev"]
-
-        # I suppose that x is > 0 anyway, so I can test on -1
-        # and max will always be >= 0
-        if self.xmin == -1:
-            # Suppose it's the start, so also init ymin
-            self.xmin = min(self.x)
-            self.ymin = min(self.min)
-        else:
-            self.xmin = min(self.xmin, min(self.x))
-            self.ymin = min(self.ymin, min(self.min))
-        self.xmax = max(self.xmax, max(self.x))
-        self.ymax = max(self.ymax, max(self.max))
-
-        return self.avg
-
-    # Resets (x|y)(min|max)
-    def reset_min_max(self):
-        self.xmin = -1
-        self.xmax = 0
-        self.ymin = -1
-        self.ymax = 0
+        self.min = columns[column + "_min"]
+        self.max = columns[column + "_max"]
+        self.avg = columns[column + "_avg"]
+        self.dev = columns[column + "_dev"]
+        self.ymin = min(self.min)
+        self.ymax = max(self.max)
 
 
 class TestStringMethods(unittest.TestCase):
-
     def test_load(self):
         stats = CSVStats("test.csv")
-        print stats.x
         self.assertEqual(stats.x, [1, 2, 4, 8], "x-values not correct")
         stats = CSVStats("test.csv", 0)
         self.assertEqual(stats.x, [1, 2, 4, 8], "x-values not correct")
@@ -92,6 +89,7 @@ class TestStringMethods(unittest.TestCase):
         self.assertEqual(stats.xmax, 8)
         self.assertEqual(stats.ymin, 2)
         self.assertEqual(stats.ymax, 9)
+
 
 if __name__ == '__main__':
     unittest.main()
