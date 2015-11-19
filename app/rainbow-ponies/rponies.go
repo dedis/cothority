f6b244c577ba9cb3fca73de360363d3c99ed744f
Linus Gasser <linus.gasser@epfl.ch>
2015-11-19 16:04:19 +0100

    updating ponies

--- app/rainbow-ponies/rponies.go
+++ app/rainbow-ponies/rponies.go
@@ -10,7 +10,7 @@ import (
 	"github.com/dedis/cothority/lib/monitor"
 )
 
-var rpc conode.RPC
+var stampApp conode.StampApplication
 
 // main initialises a new server/peer/whatever that listens on
 // port 2000 (or whatever the app-flags tell it to listen to)
@@ -19,19 +19,30 @@ func main() {
 	app.FlagInit()
 
 	// Creates a new server on port 2000 and starts the connections
-	server := conode.NewPeer()
+	server := conode.NewSigningNode(app.Conf)
 	// Listens on port 2001 for eventual RPC-calls
-	rpc = conode.NewRPC()
+	stampApp = conode.NewStampApplication()
 
-	for i := 1; i < 10; i++ {
-		dbg.Lvl1("Starting round", i)
-		server.startAnnouncement(NewRoundPony(server))
+	if server.IsRoot() {
+		for i := 1; i < 10; i++ {
+			dbg.Lvl1("Starting round", i)
+			server.startAnnouncement(NewRoundPony(server))
+		}
+	} else {
+		// server.Close is a channel that receives
+		switch server.Result{
+		case conode.Close:
+			return
+		case conode.Data:
+			// get data
+		}
 	}
 }
 
 // roundPony is our round which we initialise with the conode.RoundMerkle
 // structure to have automatic setup of a merkle-tree
 type roundPony struct {
+	conode.RoundSchnorr
 	conode.RoundMerkle
 	time monitor.Measure
 }
@@ -55,7 +66,12 @@ func (r *roundPony)Setup(server conode.Server) {
 // Announcement prepares the collection of the round-time
 func (r *roundPony)Announcement(in *conode.AnnouncementMessage,
 out []*conode.AnnouncementMessage) error {
-	err := r.RoundMerkle.Announcement(in, out)
+	err := r.RoundSchnorr.Announcement(in, out)
+	if err != nil {
+		return err
+	}
+
+	err = r.RoundMerkle.Announcement(in, out)
 	if err != nil {
 		return err
 	}
@@ -69,25 +85,46 @@ out []*conode.AnnouncementMessage) error {
 // Commit collects the messages received through the rpc and sends them to
 // the root-node
 func (r *roundPony)Commit(in []*conode.ChallengeMessage,
-out *conode.ChallengeMessage) error{
-	err := r.RoundMerkle.Commit(in, out)
-	if err != nil{
+out *conode.ChallengeMessage) error {
+	err := r.RoundSchnorr.Commit(in, out)
+	if err != nil {
 		return err
 	}
 
-	round.Message, err = rpc.CollectMessages()
+	err = r.RoundMerkle.Commit(in, out)
+	if err != nil {
+		return err
+	}
+
+	round.Message, err = stampApp.CollectMessages()
 	return err
+}
+
+// Challenge asks every node to prepare for the signature
+func (r *roundPony)Challenge(in *conode.ChallengeMessage,
+out []*conode.ChallengeMessage) error {
+	err := r.RoundSchnorr.Challenge(in, out)
+	if err != nil {
+		return err
+	}
+
+	err = r.RoundMerkle.Challenge(in, out)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
 // Response measures the round-time
 func (r *roundPony)Response(in []*conode.ResponseMessage,
-out *conode.ResponseMessage) error{
+out *conode.ResponseMessage) error {
 	err := r.RoundMerkle.Response(in, out)
-	if err != nil{
+	if err != nil {
 		return err
 	}
 
-	if app.RunFlags.AmRoot{
+	if app.RunFlags.AmRoot {
 		r.time.Measure()
 	}
 	return nil
