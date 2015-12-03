320e03ba697e0a847ee29732413dd4d84f5f80ab
Liamsi <Liamsi@users.noreply.github.com>
2015-12-03 19:51:05 +0100

    Work in progress: test still fails

--- lib/conode/stamp_test.go
+++ lib/conode/stamp_test.go
@@ -3,6 +3,7 @@ package conode_test
 import (
 	"github.com/dedis/cothority/lib/conode"
 	"github.com/dedis/cothority/lib/dbg"
+	"github.com/dedis/cothority/lib/sign"
 	"strconv"
 	"testing"
 	"time"
@@ -43,3 +44,65 @@ func TestStamp(t *testing.T) {
 }
 
 // Runs two peers and looks for the exception-message in the stamp
+func TestStampWithExceptionRaised(t *testing.T) {
+	dbg.TestOutput(testing.Verbose(), 4)
+
+	peer1, peer2 := createPeers()
+	// 2nd node should fail:
+	// FIXME this does not work:
+	sign.ExceptionForceFailure = peer2.Name()
+
+	// root node:
+	go func() {
+		dbg.Lvl2(peer1.Name(), "Stamp server in round", 1, "of", 2)
+		round, err := sign.NewRoundFromType(conode.RoundStamperListenerType, peer1.Node)
+		if err != nil {
+			dbg.Fatal("Couldn't create", conode.RoundStamperListenerType, err)
+		}
+		err = peer1.StartAnnouncement(round)
+		if err != nil {
+			dbg.Lvl3(err)
+			time.Sleep(1 * time.Second)
+		}
+	}()
+
+	go func() {
+		dbg.Lvl2(peer1.Name(), "Stamp server in round", 2, "of", 2)
+		round, err := sign.NewRoundFromType(conode.RoundStamperListenerType, peer1.Node)
+		if err != nil {
+			dbg.Fatal("Couldn't create", conode.RoundStamperListenerType, err)
+		}
+		err = peer1.StartAnnouncement(round)
+		if err != nil {
+			dbg.Lvl3(err)
+			time.Sleep(1 * time.Second)
+		}
+	}()
+
+	stampClient, err := conode.NewStamp("testdata/config.toml")
+	if err != nil {
+		t.Fatal("Couldn't open config-file:", err)
+	}
+
+	for _, port := range []int{7000, 7010} { // constants from config.toml
+		stamper := "localhost:" + strconv.Itoa(port)
+		dbg.Lvl2("Contacting stamper", stamper)
+		// FIXME this cannot work, because GetStamp knows nothing of the Exception which occurred:
+		tsm, err := stampClient.GetStamp([]byte("test"), stamper)
+		dbg.Lvl3("Evaluating results of", stamper)
+		if err != nil {
+			t.Fatal("Couldn't get stamp from server:", err)
+		}
+
+		if !tsm.Srep.AggPublic.Equal(stampClient.X0) {
+			t.Fatal("Not correct aggregate public key")
+		}
+	}
+	peer1.SendCloseAll()
+
+	dbg.Lvl2("Closing peer1")
+	peer1.Close()
+	dbg.Lvl2("Closing peer2")
+	peer2.Close()
+	dbg.Lvl3("Done with test")
+}
