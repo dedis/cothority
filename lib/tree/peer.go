707ea65386d4b3103d83f072675ea03e0259bccc
Linus Gasser <linus.gasser@epfl.ch>
2015-12-04 09:34:13 +0100

    working tests

--- lib/tree/peer.go
+++ lib/tree/peer.go
@@ -1,13 +1,43 @@
 package tree
 
 import (
+	"github.com/dedis/cothority/lib/hashid"
+	"github.com/dedis/cothority/lib/network"
 	"github.com/dedis/crypto/abstract"
 	"strconv"
 )
 
+// Peer represents a Cothority-member
+type Peer struct {
+	// The hostname of the peer
+	Name string
+	// The port-number
+	Port int
+	// The public-key
+	PubKey abstract.Point
+	// The private-key - mostly for ourselves
+	PrivKey abstract.Secret
+	// A network-connection - if already set up
+	Conn network.Conn
+}
+
+// PeerList regroup a number of peers in a list. One peer can be
+// member of more than one PeerList.
+type PeerList struct {
+	// A list of all peers that are part of this list
+	Peers []*Peer
+	// The hash-id of this list
+	Hash hashid.HashId
+	// The suite used in this list
+	Suite abstract.Suite
+}
+
+// NewPeerListLocalhost creates a list of n localhosts, starting
+// with port p and random keys.
 func NewPeerListLocalhost(s abstract.Suite, n, port int) *PeerList {
 	pl := &PeerList{
 		Peers: make([]*Peer, n),
+		Suite: s,
 	}
 
 	// Pick a random generator with a seed of n and port
@@ -15,11 +45,30 @@ func NewPeerListLocalhost(s abstract.Suite, n, port int) *PeerList {
 	for i := 0; i < n; i++ {
 		privKey := s.Secret().Pick(rand)
 		pl.Peers[i] = &Peer{
-			Name:   "localhost",
-			Port:   port + i,
-			PubKey: s.Point().Mul(nil, privKey),
+			Name:    "localhost",
+			Port:    port + i,
+			PrivKey: privKey,
+			PubKey:  s.Point().Mul(nil, privKey),
 		}
 	}
 	//pl.Hash = s.Secret().Pick(rand)
 	return pl
+}
+
+// NewNaryTree creates a tree of peers recursively with branching
+// factor bf. If bf = 2, it will create a binary tree.
+func (pl *PeerList) NewNaryTree(bf int) *Tree {
+	// Create a hash of (peerList.Hash || #peers || bf )
+	hash := pl.Suite.Hash()
+	hash.Write(pl.Hash)
+	hash.Write([]byte(strconv.Itoa(len(pl.Peers))))
+	hash.Write([]byte(strconv.Itoa(bf)))
+	t := &Tree{
+		Hash:   hash.Sum(nil),
+		HashPL: pl.Hash,
+		BF:     bf,
+	}
+	root := t.AddRoot(pl.Peers[0])
+	root.NewNaryTree(pl.Peers[1:])
+	return t
 }
--- lib/tree/tree.go
+++ lib/tree/tree.go
@@ -1,9 +1,7 @@
 package tree
 
 import (
-	"github.com/dedis/cothority/lib/hashid"
-	"github.com/dedis/cothority/lib/network"
-	"github.com/dedis/crypto/abstract"
+	"github.com/dedis/cothority/lib/dbg"
 )
 
 /*
@@ -12,37 +10,89 @@ retrieve the hosts - either in a n-ary tree, broadcast or just
  a selection.
 */
 
-// Peer represents a Cothority-member
-type Peer struct {
-	// The hostname of the peer
-	Name string
-	// The port-number
-	Port int
-	// The public-key - if known
-	PubKey abstract.Point
-	// A network-connection - if already set up
-	Conn network.Conn
+// Tree holds the structure about the current tree
+type Tree struct {
+	// The top-most peer of this tree
+	Root *TreeEntry
+	// The hash-id of this tree - same for the whole tree
+	Hash []byte
+	// The hash of the peer-list
+	HashPL []byte
+	// The branching factor
+	BF int
 }
 
-// PeerList regroup a number of peers in a list. One peer can be
-// member of more than one PeerList.
-type PeerList struct {
-	// A list of all peers that are part of this list
-	Peers []*Peer
-	// The hash-id of this list
-	Hash hashid.HashId
-}
-
-// Node is double-linked to parent and children
-type Node struct {
+// TreeEntry is one entry in the tree and linked to it's parent and
+// the tree-structure it's in.
+type TreeEntry struct {
+	// Pointer to the tree-structure we're in
+	Tree *Tree
 	// A list of all child-nodes - the indexes are relative to the PeerList
-	Children []*Node
+	Children []*TreeEntry
 	// The parent node - or nil if this is the root
-	Parent *Node
-	// The actual Host stored in this node.
+	Parent *TreeEntry
+	// The actual Peer stored in this node.
 	Peer *Peer
-	// The hash-id of this tree - every sub-tree has its own hash-id
-	Hash hashid.HashId
-	// The peer-list
-	PeerList *PeerList
+}
+
+// AddRoot creates the root-entry for the tree
+func (t *Tree) AddRoot(peer *Peer) *TreeEntry {
+	t.Root = &TreeEntry{
+		Tree:     t,
+		Children: make([]*TreeEntry, 0),
+		Peer:     peer,
+	}
+	return t.Root
+}
+
+// AddChild adds a Children to the TreeEntry and returns the
+// children added
+func (te *TreeEntry) AddChild(peer *Peer) *TreeEntry {
+	child := &TreeEntry{
+		Tree:     te.Tree,
+		Parent:   te,
+		Peer:     peer,
+		Children: make([]*TreeEntry, 0),
+	}
+	te.Children = append(te.Children, child)
+	return child
+}
+
+// Count returns the number of children of that Tree
+func (t *Tree) Count() int {
+	return t.Root.CountRec()
+}
+
+// CountRec counts the number of children recursively
+func (te *TreeEntry) CountRec() int {
+	dbg.Lvlf3("Children are: %+v", te.Children)
+	nbr := 1
+	for _, t := range te.Children {
+		nbr += t.CountRec()
+	}
+	return nbr
+}
+
+// NewNaryTree creates a tree with branching factor bf and attaches it
+// to the TreeEntry
+func (te *TreeEntry) NewNaryTree(peers []*Peer) {
+	bf := te.Tree.BF
+	numberLeft := len(peers)
+	dbg.Lvl3("Called with", numberLeft, "peers and bf =", bf)
+
+	start := 0
+	for b := 1; b <= bf; b++ {
+		// Remember: slice-ranges are exclusive of the end. So
+		// len(peers[0..1]) == 1!
+		end := b * numberLeft / bf
+		if end > start {
+			dbg.Lvl3(b, ": Creating children", start, ":", end, "of", numberLeft)
+			nte := te.AddChild(peers[start])
+			if end > (start + 1) {
+				nte.NewNaryTree(peers[start+1 : end])
+			}
+			start = end
+		}
+	}
+	dbg.Lvlf3("Finished node %+v", te)
 }
