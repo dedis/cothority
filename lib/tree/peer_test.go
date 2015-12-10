79fee8230f9ef1cf80f62cf1477fcc5dd4840858
Nicolas GAILLY <nikkolasg@gmail.com>
2015-12-10 16:09:34 +0100

    decoupled tree / id  & peer list generation

--- lib/tree/peer_test.go
+++ lib/tree/peer_test.go
@@ -3,21 +3,21 @@ package tree_test
 import (
 	"github.com/dedis/cothority/lib/tree"
 	"github.com/dedis/crypto/edwards"
+	"strconv"
 	"testing"
 )
 
 func TestNewPeerListLocal(t *testing.T) {
 	s := edwards.NewAES128SHA256Ed25519(false)
-	pl := tree.NewPeerListLocalhost(s, 11, 2000)
+	nPeers := 11
+	names := genLocalhostPeerNames(nPeers, 2000)
+	pl := tree.GenPeerList(s, names)
 	if len(pl.Peers) != 11 {
 		t.Fatal("Did not get 11 peers")
 	}
 	for i, p := range pl.Peers {
-		if p.Name != "localhost" {
+		if p.Name != "localhost"+strconv.Itoa(2000+i) {
 			t.Fatal("Peer", i, "is not from localhost")
-		}
-		if p.Port != 2000+i {
-			t.Fatal("Port of peer", i, "is not correct")
 		}
 	}
 }
