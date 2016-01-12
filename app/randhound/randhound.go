4df46b039371ae7f11bbca79d123a6ec23feb1b2
Philipp Jovanovic <philipp@jovanovic.io>
2016-01-12 18:42:33 +0100

    some comments on the RandHound phases

--- app/randhound/randhound.go
+++ app/randhound/randhound.go
@@ -42,34 +42,79 @@ func (p *ProtocolRandHound) Dispatch(m []*sda.SDAData) error {
 
 // Ix: Messages from client to server
 // Rx: Messages from server to client
+
+// Phase 1
 func (*ProtocolRandHound) HandleI1(m *sda.SDAData) error {
+	if p.IsRoot() {
+		// Step 1 (client): form I1 message and send it to all servers
+	} else {
+		// Step 2 (server): receive I1 and process (=validate) it
+	}
 	return nil
 }
 
 func (*ProtocolRandHound) HandleR1(m *sda.SDAData) error {
+	if p.IsRoot() {
+		// Step 4 (client): receive R1 and process it
+	} else {
+		// Step 3 (server): choose server's trustee-selection randomness, form R1 and send it to client
+	}
 	return nil
 }
 
+// Phase 2
 func (*ProtocolRandHound) HandleI2(m *sda.SDAData) error {
+	if p.IsRoot() {
+		// Step 5 (client): form I2 message and send it to all servers
+	} else {
+		// Step 6 (server): receive I2, extract Rc, compute HRc and verify it
+	}
 	return nil
 }
 
 func (*ProtocolRandHound) HandleR2(m *sda.SDAData) error {
+	if p.IsRoot() {
+		// Step 8 (client): receive R2 and process it
+	} else {
+		// Step 7 (server): construct deal, form R2 and send it to the client
+	}
 	return nil
 }
 
+// Phase 3
 func (*ProtocolRandHound) HandleI3(m *sda.SDAData) error {
+	if p.IsRoot() {
+		// Step 9 (client): form I3 and send it to the servers
+	} else {
+		// Step 10 (server): receive I3 and verify it
+	}
 	return nil
 }
 
 func (*ProtocolRandHound) HandleR3(m *sda.SDAData) error {
+	if p.IsRoot() {
+		// Step 12 (client): receive R3 and process it
+	} else {
+		// Step 11 (server): Decrypt and validate all the shares we've been dealt, form R3, and send it to the client
+	}
 	return nil
 }
 
+// Phase 4
 func (*ProtocolRandHound) HandleI4(m *sda.SDAData) error {
+	if p.IsRoot() {
+		// Step 13 (client): form I4 and send it to the servers
+	} else {
+		// Step 14 (server): receive R4, verify it
+	}
 	return nil
 }
 
 func (*ProtocolRandHound) HandleR4(m *sda.SDAData) error {
+	if p.IsRoot() {
+		// Step 16 (client): receive R3 and reconstruct the final secret (print output value to log for debugging)
+	} else {
+		// Step 15 (server): form R3 and send it to client
+	}
 	return nil
 }
