a98141e54ff6171e633616d3cd345c4a755dc2db
Linus Gasser <linus.gasser@epfl.ch>
2016-02-01 11:15:31 +0100

    added two skeletons - one for channels and one for handlers

--- protocols/protocols.go
+++ protocols/protocols.go
@@ -5,7 +5,8 @@ Only used to include the different protocols
 */
 
 import (
-	_ "github.com/dedis/cothority/protocols/example"
 	_ "github.com/dedis/cothority/protocols/jvss"
 	_ "github.com/dedis/cothority/protocols/manage"
+	_ "github.com/dedis/cothority/protocols/skeleton_channels"
+	_ "github.com/dedis/cothority/protocols/skeleton_handlers"
 )
