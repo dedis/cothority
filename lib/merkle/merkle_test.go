4c4f1615007767740860dfa57648b13b6c405e5e
Liamsi <Liamsi@users.noreply.github.com>
2016-02-05 11:00:02 +0100

    Test if we can (un)marshal using lib/network

--- lib/merkle/merkle_test.go
+++ lib/merkle/merkle_test.go
@@ -6,6 +6,8 @@ import (
 	"testing"
 
 	"github.com/dedis/cothority/lib/merkle"
+	"github.com/dedis/cothority/lib/network"
+	"github.com/dedis/crypto/abstract"
 	"github.com/dedis/crypto/nist"
 )
 
@@ -30,4 +32,37 @@ func TestCommitHash(t *testing.T) {
 		t.Fatal("Collision")
 	}
 	fmt.Printf("FYI:\nh1=%v\nh2=%v\nh3=%v\n", h1, h2, h3)
+}
+
+// see if we can (un)marshal a point and its using the network lib
+type TestMsg struct {
+	Point abstract.Point
+	H     merkle.Hash
+}
+
+var TestType = network.RegisterMessageType(TestMsg{})
+
+func TestNetworkMarshaling(t *testing.T) {
+	suite := nist.NewAES128SHA256P256()
+	m := merkle.NewMerkle(suite)
+	V1 := suite.Point().Base()
+	h1, err := m.HashCommits(V1)
+	if err != nil {
+		t.Fatalf("Couldn't compute hash of V1=%s", V1)
+	}
+
+	tm := TestMsg{V1, h1}
+	b, err := network.MarshalRegisteredType(&tm)
+	if err != nil {
+		t.Fatalf("Couldn't marhsal=%+v", tm)
+	}
+	cons := network.DefaultConstructors(suite)
+	_, msg, err := network.UnmarshalRegisteredType(b, cons)
+	if err != nil {
+		t.Fatalf("Couldn't unmarhsal %+v from %s", tm, b)
+	}
+	msgStruct := msg.(TestMsg)
+	if !bytes.Equal(msgStruct.H, tm.H) {
+		t.Fatalf("Umarshaling didn't work")
+	}
 }
