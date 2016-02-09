75a0aaab9e7ffb8bb373a6e1480bf21fe7093dc3
Liamsi <Liamsi@users.noreply.github.com>
2016-02-09 16:49:23 +0100

    Added skeleton for simulation

---
+++ protocols/bizcoin/simulation.go
@@ -0,0 +1,49 @@
+package bizcoin
+
+import (
+	"errors"
+	"strconv"
+
+	"github.com/BurntSushi/toml"
+	"github.com/dedis/cothority/lib/dbg"
+	"github.com/dedis/cothority/lib/monitor"
+	"github.com/dedis/cothority/lib/sda"
+)
+
+func init() {
+	sda.SimulationRegister("ExampleChannels", NewBizCoinSimulation)
+}
+
+type BizCoinSimulation struct {
+	sda.SimulationBFTree
+}
+
+func NewBizCoinSimulation(config string) (sda.Simulation, error) {
+	es := &BizCoinSimulation{}
+	_, err := toml.Decode(config, es)
+	if err != nil {
+		return nil, err
+	}
+	return es, nil
+}
+
+func (e *BizCoinSimulation) Setup(dir string, hosts []string) (*sda.SimulationConfig, error) {
+	// TODO will the tree be re-created / broadcasted in every round? (in Run())
+	sc := &sda.SimulationConfig{}
+	e.CreateEntityList(sc, hosts, 2000)
+	err := e.CreateTree(sc)
+	if err != nil {
+		return nil, err
+	}
+	return sc, nil
+}
+
+func (e *BizCoinSimulation) Run(config *sda.SimulationConfig) error {
+	for round := 0; round < e.Rounds; round++ {
+		dbg.Lvl1("Starting round", round)
+		round := monitor.NewMeasure("round_XXX")
+		// TODO what exactly to measure?
+		round.Measure()
+	}
+	return nil
+}
