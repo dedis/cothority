5b244bd73976e9d41ddf366c458896ef32d73f1e
Liamsi <Liamsi@users.noreply.github.com>
2016-02-16 18:20:44 +0100

    PBFT simultaion (almost done; blocks somewhere)

--- protocols/bizcoin/pbft/pbft.go
+++ protocols/bizcoin/pbft/pbft.go
@@ -1,11 +1,29 @@
 package pbft
 
 import (
+	"encoding/json"
+	"fmt"
+	"math"
+	"time"
+
+	"github.com/dedis/cothority/lib/dbg"
 	"github.com/dedis/cothority/lib/sda"
+	"github.com/dedis/cothority/protocols/bizcoin/blockchain"
 	"github.com/dedis/crypto/abstract"
+	"github.com/satori/go.uuid"
+)
+
+const (
+	NotFound = -1
 )
 
 // Protocol implements sda.Protocol
+// we do basically the same as in http://www.pmg.lcs.mit.edu/papers/osdi99.pdf
+// with the following diffs:
+// there is no client/server request and reply (or first line in Figure 1)
+// instead of MACs we just send around the hash of the block
+// this will make the protocol faster, but the network latency will overweigh
+// this skipped computation anyways
 type Protocol struct {
 	// the node we are represented-in
 	*sda.Node
@@ -13,30 +31,61 @@ type Protocol struct {
 	suite abstract.Suite
 	// aggregated public key of the peers
 	aggregatedPublic abstract.Point
+	// a flat list of all TreeNodes (similar to jvss)
+	nodeList []*sda.TreeNode
+	// our index in the entitylist
+	index int
 
+	// we do not care for servers or clients (just store one block here)
+	trBlock *blockchain.TrBlock
+
+	prepMsgCount   int
+	commitMsgCount int
+	threshold      int
 	// channels:
 	prePrepareChan chan prePrepareChan
 	prepareChan    chan prepareChan
 	commitChan     chan commitChan
+
+	onDoneCB func()
 }
 
 func NewProtocol(n *sda.Node) (*Protocol, error) {
 	pbft := new(Protocol)
+	tree := n.Tree()
+	pbft.Node = n
+	pbft.nodeList = tree.ListNodes()
+	idx := NotFound
+	for i, tn := range pbft.nodeList {
+		if uuid.Equal(tn.Id, n.TreeNode().Id) {
+			idx = i
+		}
+	}
+	if idx == NotFound {
+		panic(fmt.Sprintf("Could not find ourselves %+v in the list of nodes %+v", n, pbft.nodeList))
+	}
+	pbft.index = idx
+	// 2/3 * #participants == threshold
+	pbft.threshold = 2 * int(math.Ceil(float64(len(pbft.nodeList))/3.0))
 	n.RegisterChannel(&pbft.prePrepareChan)
 	n.RegisterChannel(&pbft.prepareChan)
 	n.RegisterChannel(&pbft.commitChan)
 	return pbft, nil
 }
 
-func NewRootProtocol(n *sda.Node) (*Protocol, error) {
-	pbft := new(Protocol)
-
+func NewRootProtocol(n *sda.Node, trBlock *blockchain.TrBlock, onDoneCb func()) (*Protocol, error) {
+	pbft, err := NewProtocol(n)
+	if err != nil {
+		return nil, err
+	}
+	pbft.trBlock = trBlock
+	pbft.onDoneCB = onDoneCb
 	return pbft, nil
 }
 
 // Start will start pre-prepare and TODO ???
 func (p *Protocol) Start() error {
-
+	// DO nothing?
 	return nil
 }
 
@@ -55,20 +104,99 @@ func (p *Protocol) Dispatch() error {
 	}
 }
 
-func (p *Protocol) handlePrePrepare(prePre *PrePrepare) {
+// PrePrepare intializes a full run of the protocol
+func (p *Protocol) PrePrepare() error {
+	// pre-prepare: broadcast the block
+	var err error
+	dbg.Print(p.Node.Name(), "Broadcast PrePrepare")
+	p.broadcast(func(tn *sda.TreeNode) {
+		prep := PrePrepare{p.trBlock}
+		tempErr := p.Node.SendTo(tn, &prep)
+		if tempErr != nil {
+			err = tempErr
+		}
+	})
+	return err
+}
 
+func (p *Protocol) handlePrePrepare(prePre *PrePrepare) {
+	// prepare: verify the structure of the block and broadcast
+	// prepare msg (with header hash of the block)
+	var err error
+	if verifyBlock(prePre.TrBlock, "", "") {
+		p.broadcast(func(tn *sda.TreeNode) {
+			prep := Prepare{prePre.TrBlock.HeaderHash}
+			tempErr := p.Node.SendTo(tn, &prep)
+			if tempErr != nil {
+				err = tempErr
+			}
+		})
+	}
+	if err != nil {
+		dbg.Error("Error while broadcasting Prepare msg", err)
+	}
 }
 
 func (p *Protocol) handlePrepare(pre *Prepare) {
-
+	p.prepMsgCount++
+	dbg.Lvl4(p.Node.Name(), "We got", p.prepMsgCount, "Prepare msgs and threshold is", p.threshold)
+	if p.prepMsgCount >= p.threshold {
+		dbg.Lvl3(p.Node.Name(), "Threshold reached: broadcast Commit")
+		var err error
+		p.broadcast(func(tn *sda.TreeNode) {
+			prep := Commit{pre.HeaderHash}
+			tempErr := p.Node.SendTo(tn, &prep)
+			if tempErr != nil {
+				err = tempErr
+			}
+		})
+		if err != nil {
+			dbg.Error("Error while broadcasting Commit msg", err)
+		}
+	}
 }
 
 func (p *Protocol) handleCommit(com *Commit) {
-
+	// finish after threshold of Commit msgs
+	p.commitMsgCount++
+	if p.commitMsgCount >= p.threshold {
+		dbg.Lvl3(p.Node.Name(), "Threshold reached: We are done... CONSENSUS")
+		if p.IsRoot() && p.onDoneCB != nil {
+			dbg.Lvl3(p.Node.Name(), "Even better! We are root and threshold reached: We are really done and return to the simulation.")
+			p.onDoneCB()
+		}
+		p.Done()
+	}
 }
 
-// TODO
-// pre-prepare: send around block,
-// prepare: verify the structure of the block and broadcast prepare msg (with header hash of the block)
-// commit: count prep msgs, if > threshold -> broadcast commit msg (hash of the block header)
-// we don't do a MAC like in the PBFT paper but a hash (or even a signature)
+// sendCb should contain the real sendTo call and the msg to broadcast
+// example for sendCb:
+// func(tn *sda.TreeNode) { p.Node.SendTo(tn, &registerdMsg )}
+func (p *Protocol) broadcast(sendCb func(*sda.TreeNode)) {
+	for i, tn := range p.nodeList {
+		if i == p.index {
+			continue
+		}
+		sendCb(tn)
+	}
+}
+
+// verifyBlock is a simulation of a real block verification algorithm
+// FIXME merge with Nicolas' code (public method in bizcoin)
+func verifyBlock(block *blockchain.TrBlock, lastBlock, lastKeyBlock string) bool {
+	//We measure the average block verification delays is 174ms for an average
+	//block of 500kB.
+	//To simulate the verification cost of bigger blocks we multiply 174ms
+	//times the size/500*1024
+	b, _ := json.Marshal(block)
+	s := len(b)
+	var n time.Duration
+	n = time.Duration(s / (500 * 1024))
+	time.Sleep(150 * time.Millisecond * n) //verification of 174ms per 500KB simulated
+	// verification of the header
+	verified := block.Header.Parent == lastBlock && block.Header.ParentKey == lastKeyBlock
+	verified = verified && block.Header.MerkleRoot == blockchain.HashRootTransactions(block.TransactionList)
+	verified = verified && block.HeaderHash == blockchain.HashHeader(block.Header)
+
+	return verified
+}
--- protocols/bizcoin/pbft/simulation.go
+++ protocols/bizcoin/pbft/simulation.go
@@ -2,12 +2,17 @@ package pbft
 
 import (
 	"github.com/BurntSushi/toml"
+	"github.com/dedis/cothority/lib/dbg"
+	"github.com/dedis/cothority/lib/monitor"
 	"github.com/dedis/cothority/lib/sda"
+	"github.com/dedis/cothority/protocols/bizcoin/blockchain"
 )
+
+var magicNum = [4]byte{0xF9, 0xBE, 0xB4, 0xD9}
 
 func init() {
 	sda.SimulationRegister("PbftSimulation", NewSimulation)
-	sda.ProtocolRegisterName("BizCoin", func(n *sda.Node) (sda.ProtocolInstance, error) { return NewProtocol(n) })
+	sda.ProtocolRegisterName("PBFT", func(n *sda.Node) (sda.ProtocolInstance, error) { return NewProtocol(n) })
 }
 
 // Simulation implements sda.Simulation interface
@@ -16,6 +21,10 @@ type Simulation struct {
 	sda.SimulationBFTree
 	// pbft simulation specific fields:
 	// TODO
+	// Blocksize is the number of transactions in one block:
+	Blocksize int
+	//blocksDir is the directory where to find the transaction blocks (.dat files)
+	BlocksDir string
 }
 
 func NewSimulation(config string) (sda.Simulation, error) {
@@ -40,5 +49,45 @@ func (e *Simulation) Setup(dir string, hosts []string) (*sda.SimulationConfig, e
 
 func (e *Simulation) Run(sdaConf *sda.SimulationConfig) error {
 	// TODO
+	doneChan := make(chan bool)
+	doneCB := func() { doneChan <- true }
+	// FIXME use client instead
+	parser, err := blockchain.NewParser(e.BlocksDir, magicNum)
+	if err != nil {
+		dbg.Error("Couldn't parse blocks in", e.BlocksDir)
+		return err
+	}
+	transactions := parser.Parse(0, e.Blocksize)
+	node, err := sdaConf.Overlay.CreateNewNodeName("PBFT", sdaConf.Tree)
+	if err != nil {
+		return err
+	}
+	// FIXME c&p from bizcoin.go
+	trlist := blockchain.NewTransactionList(transactions, len(transactions))
+	header := blockchain.NewHeader(trlist, "", "")
+	trblock := blockchain.NewTrBlock(trlist, header)
+
+	proto, err := NewRootProtocol(node, trblock, doneCB)
+	if err != nil {
+		return err
+	}
+
+	for round := 0; round < e.Rounds; round++ {
+		dbg.Lvl1("Starting round", round)
+		r := monitor.NewMeasure("round_pbft")
+		err := proto.PrePrepare()
+		if err != nil {
+			dbg.Error("Couldn't start PrePrepare")
+			return err
+		}
+
+		// wait for finishing pbft:
+		<-doneChan
+		r.Measure()
+		dbg.Lvl1("Finished round", round)
+	}
 	return nil
 }
+
+// helper functions:
+// FIXME refactor, use the same mechanisms as in buîzcoin.go's simulation
