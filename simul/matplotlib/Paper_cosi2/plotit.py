0e41d4cf3dd37a594942dfd426bfbb835ea66031
Linus Gasser <linus.gasser@epfl.ch>
2016-03-14 10:12:47 +0100

    cleaning up graphs

--- simul/matplotlib/Paper_cosi2/plotit.py
+++ simul/matplotlib/Paper_cosi2/plotit.py
@@ -20,33 +20,16 @@ import matplotlib.patches as mpatches
 # directly on the plot
 def plotCoSi():
     mplot.plotPrepareLogLog()
-    cosi_old, cosi_3, cosi_3_check, jvss, naive_cosi, ntree_cosi = \
-        read_csvs('cosi_old', 'cosi_depth_3', 'cosi_depth_3_check', 'jvss', 'naive_cosi', 'ntree_cosi')
+    plots = read_csvs('jvss', 'naive_cosi', 'ntree_cosi', 'cosi_depth_3')
+    ranges = []
     plot_show('comparison_roundtime')
 
-    jv = mplot.plotMMA(jvss, 'round_wall', color2_light, 4,
-                       dict(label='JVSS', linestyle='-', marker='o', color=color2_dark, zorder=5))
-
-    na_co = mplot.plotMMA(naive_cosi, 'round_wall', color3_light, 4,
-                       dict(label='Naive', linestyle='-', marker='o', color=color3_dark, zorder=5))
-
-    nt_co = mplot.plotMMA(ntree_cosi, 'round_wall', color4_light, 4,
-                       dict(label='NTree', linestyle='-', marker='o', color=color4_dark, zorder=5))
-
-    co_3_c = mplot.plotMMA(cosi_3_check, 'round_wall', color1_light, 4,
-                       dict(label='CoSi check - depth 3', linestyle='-', marker='v', color=color1_dark, zorder=5))
-
-    co_3 = mplot.plotMMA(cosi_3, 'round_wall', color1_light, 4,
-                       dict(label='CoSi - depth 3', linestyle='-', marker='o', color=color1_dark, zorder=5))
-
-    #co_old = mplot.plotMMA(cosi_old, 'round_wall', color5_light, 4,
-    #                   dict(label='CoSi old', linestyle='-', marker='s', color=color5_dark, zorder=5))
-    #co_old_depth = cosi_old.get_old_depth()
-    #plt.plot(cosi_old.x, co_old_depth, linestyle='-', marker='v', color=color5_dark, label='CoSi old depth')
+    for index, label in enumerate(['JVSS', 'Naive', 'NTree', 'CoSi']):
+        ranges.append(mplot.plotMMA(plots[index], 'round_wall', colors[index][0], 4,
+                           dict(label=label, linestyle='-', marker='o', color=colors[index][1], zorder=5)))
 
     # Make horizontal lines and add arrows for JVSS
-    # xmin, xmax, ymin, ymax = CSVStats.get_min_max(na, co)
-    xmin, xmax, ymin, ymax = CSVStats.get_min_max(na_co, nt_co, jv, co_3, co_3_c)
+    xmin, xmax, ymin, ymax = CSVStats.get_min_max(*ranges)
     plt.ylim(0.5, 8)
     plt.xlim(16, xmax * 1.2)
     plt.ylabel('Seconds per round')
@@ -63,23 +46,15 @@ def plotCoSi():
 # directly on the plot
 def plotCoSiSysUser():
     mplot.plotPrepareLogLog()
-    cosi_old, cosi_3, jvss, naive_cosi, ntree_cosi = \
-        read_csvs('cosi_old', 'sysusr_cosi', 'jvss', 'naive_cosi', 'sysusr_ntree')
+    plots = read_csvs('jvss', 'naive_cosi', 'sysusr_ntree', 'sysusr_cosi')
     plot_show('comparison_sysusr')
 
     ymin = 0.05
-    bar_jvss, jvss_sys, jvss_usr = mplot.plotStackedBarsHatched(jvss, "round_system", "round_user", "JVSS", color2_light,
-                                                      ymin, delta_x=-2)
-
-    bar_naive, na_sys, na_usr = mplot.plotStackedBarsHatched(naive_cosi, "round_system", "round_user", "Naive", color3_light,
-                                                     ymin, delta_x=-1, limit_values=7)
-
-    bar_ntree, nt_sys, nt_usr = mplot.plotStackedBarsHatched(ntree_cosi, "round_system", "round_user", "Ntree", color4_light,
-                                                     ymin, delta_x=0, limit_values=7)
-
-    bar_cothority, co_sys, co_usr = mplot.plotStackedBarsHatched(cosi_3, "round_system", "round_user", "Cothority",
-                                                         color1_light, ymin, delta_x=1, limit_values=11)
-
+    bars = []
+    deltax = -2
+    for index, label in enumerate(['JVSS', 'Naive', 'NTree', 'CoSi']):
+        bars.append( mplot.plotStackedBarsHatched(plots[index], "round_system", "round_user", label, colors[index][0],
+                                                      ymin, delta_x=deltax +index)[0])
 
     ymax = 7
     xmax = 3192
@@ -89,26 +64,26 @@ def plotCoSiSysUser():
     usert = mpatches.Patch(color='white', ec='black', label='User time', hatch='//')
     syst = mpatches.Patch(color='white', ec='black', label='System time')
 
-    plt.legend(handles=[bar_jvss, bar_naive, bar_ntree, bar_cothority, usert, syst], loc=u'upper left')
-    ax = plt.axes()
-    #ax.set_xticks([16, 32, 64, 128, 256, 512, 1024, 4096, 16384, 65536])
+    plt.legend(handles=[bars[0], bars[1], bars[2], bars[3], usert, syst], loc=u'upper left')
     mplot.plotEnd()
 
 
 # Plots the branching factor
 def plotBF():
     mplot.plotPrepareLogLog(0, 0)
-    cosi_bf = read_csvs_xname('bf', 'cosi_bf')[0]
+    cosi_bf = read_csvs_xname('bf', 'cosi_bf_2048',
+                                                               'cosi_bf_4096', 'cosi_bf_8192')
     plot_show('cosi_bf')
+    cbf = []
 
-    cbf = mplot.plotMMA(cosi_bf, 'round_wall', color1_light, 4,
-                       dict(label='CoSi 2048', linestyle='-', marker='o', color=color1_dark, zorder=5))
-    plt.plot(cosi_bf.x, cosi_bf.columns['depth'], linestyle='-', marker='v', color=color2_dark,
-                     label='CoSi depth')
+    for index, label in enumerate(['2048', '4096', '8192']):
+        data = cosi_bf[index]
+        cbf.append(mplot.plotMMA(data, 'round_wall', colors[index][0], 4,
+                       dict(label='CoSi ' + label, linestyle='-', marker='o', color=colors[index][0], zorder=5)))
+        plt.plot(data.x, data.columns['depth'], linestyle=':', marker='v', color=colors[index][1],
+                     label='CoSi ' + label + ' depth')
 
-    # Make horizontal lines and add arrows for JVSS
-    # xmin, xmax, ymin, ymax = CSVStats.get_min_max(na, co)
-    xmin, xmax, ymin, ymax = CSVStats.get_min_max(cbf)
+    xmin, xmax, ymin, ymax = CSVStats.get_min_max(cbf[0], cbf[1], cbf[2])
     plt.ylim(ymin, ymax)
     plt.xlim(xmin, xmax * 1.2)
     plt.ylabel('Seconds per round')
@@ -122,22 +97,16 @@ def plotBF():
 # Plots the oversubscription
 def plotOver():
     mplot.plotPrepareLogLog()
-    cosi_over_1, cosi_over_2, cosi_over_3 = \
-        read_csvs('cosi_over_1', 'cosi_over_2', 'cosi_over_3')
+    plots = read_csvs('cosi_over_1', 'cosi_over_2', 'cosi_over_3')
     plot_show('cosi_over')
 
-    co1 = mplot.plotMMA(cosi_over_1, 'round_wall', color1_light, 4,
-                       dict(label='Cosi 8 servers', linestyle='-', marker='o', color=color1_dark, zorder=5))
+    ranges = []
+    for index, label in enumerate(['8', '16', '4']):
+        ranges.append(mplot.plotMMA(plots[index], 'round_wall', colors[index][0], 4,
+                       dict(label='Cosi ' + label + ' servers', linestyle='-', marker='o',
+                            color=colors[index][1], zorder=5)))
 
-    co2 = mplot.plotMMA(cosi_over_2, 'round_wall', color2_light, 4,
-                       dict(label='Cosi 16 servers', linestyle='-', marker='o', color=color2_dark, zorder=5))
-
-    co3 = mplot.plotMMA(cosi_over_3, 'round_wall', color3_light, 4,
-                       dict(label='Cosi 4 servers', linestyle='-', marker='o', color=color3_dark, zorder=5))
-
-    # Make horizontal lines and add arrows for JVSS
-    # xmin, xmax, ymin, ymax = CSVStats.get_min_max(na, co)
-    xmin, xmax, ymin, ymax = CSVStats.get_min_max(co1, co2, co3)
+    xmin, xmax, ymin, ymax = CSVStats.get_min_max(*ranges)
     plt.ylim(ymin, ymax)
     plt.xlim(xmin, xmax * 1.2)
     plt.ylabel('Seconds per round')
@@ -146,33 +115,23 @@ def plotOver():
     plt.axes().xaxis.grid(color='gray', linestyle='dashed', zorder=0)
     mplot.plotEnd()
 
+def plotNetwork():
+    return
+
+def plotChecking():
+    return
 
 # Colors for the Cothority
-color1_light = 'lightgreen'
-color1_dark = 'green'
-color2_light = 'lightblue'
-color2_dark = 'blue'
-color3_light = 'yellow'
-color3_dark = 'brown'
-color4_light = 'pink'
-color4_dark = 'red'
-color5_light = 'pink'
-color5_dark = 'red'
+colors = [['lightgreen', 'green'],
+          ['lightblue', 'blue'],
+          ['yellow', 'brown'],
+          ['pink', 'red'],
+          ['pink', 'red']]
 mplot = MPlot()
-write_file = True
-file_extension = 'png'
-
-def check_Args(args):
-    if len(sys.argv) < args + 1:
-        print("Error: Please give a mode and " + str(args) + " .csv-files as argument - " + str(len(sys.argv)) + "\n")
-        print("Mode: (0=printAverage, 1=printSystemUserTimes with bars, 2=printSystemUserTimes with areas)\n")
-        print("CSV: cothority.csv jvss.csv\n")
-        exit(1)
 
 def plot_show(file):
     if write_file:
         mplot.pngname = file + '.' + file_extension
-        mplot.show_fig = False
 
 def read_csvs_xname(xname, *values):
     stats = []
@@ -184,8 +143,17 @@ def read_csvs_xname(xname, *values):
 def read_csvs(*values):
     return read_csvs_xname("hosts", *values)
 
+# Write to file
+write_file = True
+# What file extension - .png, .eps
+file_extension = 'png'
+# Show figure
+mplot.show_fig = False
+
 # Call all plot-functions
 plotCoSi()
 plotCoSiSysUser()
 plotBF()
 plotOver()
+#plotNetwork()
+#plotChecking()
