00c8a29edbd2fef4cc03fcdf19a15eaa72d688ef
Liamsi <Liamsi@users.noreply.github.com>
2016-03-16 13:40:43 +0100

    fixed lint script

--- lint.sh
+++ lint.sh
@@ -2,7 +2,7 @@
 # script checks for missing comments using golint
 
 # list of lints we want to ensure:
-failOn="should have comment or be unexported \| by other packages, and that stutters; consider calling this"
+failOn="should have comment or be unexported\| by other packages, and that stutters; consider calling this"
 lintOut=`golint ./... | grep "$failOn"`
 
 # if the output isn't empty exit with an error
