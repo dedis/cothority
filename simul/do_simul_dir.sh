0cfeeb97cac12476fafc939136ceac6bbf36137a
Linus Gasser <linus.gasser@epfl.ch>
2016-03-17 08:56:50 +0100

    also show errors, warnings and fatals

--- simul/do_simul_dir.sh
+++ simul/do_simul_dir.sh
@@ -18,13 +18,13 @@ FLAGS="$@"
 if [ "$FLAGS" ]; then
     GREP=""
 else
-    GREP="Starting run with parameters"
+    GREP="(Starting run with parameters|^F :|^E :|^W :)"
 fi
 NOBUILD=""
 go build
 
 for simul in $DIR/*toml; do
     echo "Simulating file $(basename $simul)"
-    ./simul -platform $PLATFORM $NOBUILD $FLAGS $simul | grep "$GREP"
+    ./simul -platform $PLATFORM $NOBUILD $FLAGS $simul | egrep "$GREP"
     NOBUILD="-nobuild"
 done
