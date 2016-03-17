96adc4572c68cc94df8f7eb89bc4937c23a62930
Linus Gasser <linus.gasser@epfl.ch>
2016-03-17 08:16:20 +0100

    bash-script for running a whole directory

---
+++ simul/do_simul_dir.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+PLATFORM="deterlab"
+
+if [ -z "$1" ]; then
+    echo "Syntax: $0 directory [flags]"
+    exit 1
+fi
+
+if [ ! -d $1 ]; then
+    echo "Directory $0 doesn't exist"
+    exit 1
+fi
+
+DIR="$1"
+shift
+FLAGS="$@"
+if [ "$FLAGS" ]; then
+    GREP=""
+else
+    GREP="Starting run with parameters"
+fi
+NOBUILD=""
+go build
+
+for simul in $DIR/*toml; do
+    echo "Simulating file $(basename $simul)"
+    ./simul -platform $PLATFORM $NOBUILD $FLAGS $simul | grep "$GREP"
+    NOBUILD="-nobuild"
+done
