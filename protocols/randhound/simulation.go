19623c05059220e90b6ae08d69f3d562a3b3632a
Philipp Jovanovic <philipp@jovanovic.io>
2016-03-24 23:03:22 +0100

    rh fix simulation

--- protocols/randhound/simulation.go
+++ protocols/randhound/simulation.go
@@ -14,9 +14,8 @@ func init() {
 
 type RHSimulation struct {
 	sda.SimulationBFTree
-	Hosts   int
-	K       int
-	Purpose string
+	Trustees int
+	Purpose  string
 }
 
 func NewRHSimulation(config string) (sda.Simulation, error) {
@@ -28,27 +27,34 @@ func NewRHSimulation(config string) (sda.Simulation, error) {
 	return rhs, nil
 }
 
-func (rh *RHSimulation) Setup(dir string, hosts []string) (*sda.SimulationConfig, error) {
+func (rhs *RHSimulation) Setup(dir string, hosts []string) (*sda.SimulationConfig, error) {
 	sim := new(sda.SimulationConfig)
-	rh.CreateEntityList(sim, hosts, 2000)
-	err := rh.CreateTree(sim)
-	return sim, err
+	rhs.Hosts = len(hosts)
+	rhs.CreateEntityList(sim, hosts, 2000)
+	err := rhs.CreateTree(sim)
+	if err != nil {
+		return nil, err
+	}
+	return sim, nil
 }
 
-func (rh *RHSimulation) Run(config *sda.SimulationConfig) error {
-	node, err := config.Overlay.CreateNewNodeName("RandHound", config.Tree)
+func (rhs *RHSimulation) Run(config *sda.SimulationConfig) error {
+	leader, err := config.Overlay.CreateNewNodeName("RandHound", config.Tree)
 	if err != nil {
 		return err
 	}
-	proto := node.ProtocolInstance().(*RandHound)
-	proto.Setup(rh.Hosts, rh.K, rh.Purpose)
-	proto.Start()
+	rh := leader.ProtocolInstance().(*RandHound)
+	err = rh.Setup(rhs.Hosts, rhs.Trustees, rhs.Purpose)
+	if err != nil {
+		return err
+	}
+	rh.Start()
 
 	bytes := make([]byte, 32)
 	select {
-	case _ = <-proto.Leader.Done:
+	case _ = <-rh.Leader.Done:
 		log.Printf("RandHound - done")
-		bytes = <-proto.Leader.Result
+		bytes = <-rh.Leader.Result
 	case <-time.After(time.Second * 60):
 		log.Printf("RandHound - time out")
 	}
