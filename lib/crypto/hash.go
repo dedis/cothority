029ed5a362ef6a03c69e261aa3627761e28b44a2
Linus Gasser <linus.gasser@epfl.ch>
2016-03-29 09:13:55 +0200

    added slicing hasher

--- lib/crypto/hash.go
+++ lib/crypto/hash.go
@@ -1,20 +1,37 @@
 package crypto
 
 import (
+	"errors"
+	"github.com/dedis/cothority/lib/dbg"
 	"github.com/dedis/crypto/abstract"
-	"io/ioutil"
+	"os"
 )
 
-func HashFile(suite abstract.Suite, file string) ([]byte, error) {
+func HashFileSlice(suite abstract.Suite, file string, size int) ([]byte, error) {
+	if size == 0 {
+		return nil, errors.New("Cannot read 0 bytes")
+	}
 	hash := suite.Hash()
 	hash.Reset()
-	b, err := ioutil.ReadFile(file)
+
+	f, err := os.Open(file)
 	if err != nil {
 		return nil, err
 	}
-	_, err = hash.Write(b)
-	if err != nil {
-		return nil, err
+	b := make([]byte, size)
+	n := size
+	for n == size {
+		n, err = f.Read(b)
+		dbg.Lvl3("Read", n, "bytes of", size)
+		_, err = hash.Write(b)
+		if err != nil {
+			return nil, err
+		}
 	}
 	return hash.Sum(nil), nil
+}
+
+func HashFile(suite abstract.Suite, file string) ([]byte, error) {
+	b, err := HashFileSlice(suite, file, 1024)
+	return b, err
 }
--- lib/crypto/hash_test.go
+++ lib/crypto/hash_test.go
@@ -2,6 +2,7 @@ package crypto_test
 
 import (
 	"github.com/dedis/cothority/lib/crypto"
+	"github.com/dedis/cothority/lib/dbg"
 	"github.com/dedis/crypto/edwards/ed25519"
 	"io/ioutil"
 	"testing"
@@ -9,7 +10,7 @@ import (
 
 func TestHash(t *testing.T) {
 	tmpfile := "/tmp/hash_test.bin"
-	for i := range []int{16, 128, 1024} {
+	for _, i := range []int{16, 128, 1024} {
 		str := make([]byte, i)
 		err := ioutil.WriteFile(tmpfile, str, 0777)
 		if err != nil {
@@ -18,7 +19,28 @@ func TestHash(t *testing.T) {
 
 		hash, err := crypto.HashFile(ed25519.NewAES128SHA256Ed25519(false), tmpfile)
 		if err != nil {
-			t.Fatal("Couldn't hash", tmpfile)
+			t.Fatal("Couldn't hash", tmpfile, err)
+		}
+		if len(hash) != 32 {
+			t.Fatal("Length of sha256 should be 32")
+		}
+	}
+}
+
+func TestHashSlice(t *testing.T) {
+	tmpfile := "/tmp/hash_test.bin"
+	str := make([]byte, 1234)
+	err := ioutil.WriteFile(tmpfile, str, 0777)
+	if err != nil {
+		t.Fatal("Couldn't write file")
+	}
+
+	for _, i := range []int{16, 128, 1024} {
+		dbg.Lvl3("Reading", i, "bytes")
+		hash, err := crypto.HashFileSlice(ed25519.NewAES128SHA256Ed25519(false),
+			tmpfile, i)
+		if err != nil {
+			t.Fatal("Couldn't hash", tmpfile, err)
 		}
 		if len(hash) != 32 {
 			t.Fatal("Length of sha256 should be 32")
