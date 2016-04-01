f9c674ad92e1f3e6006cdb196d2206d7b16a87e5
Philipp Jovanovic <philipp@jovanovic.io>
2016-04-01 16:17:18 +0200

    jvss: setup communication

--- protocols/jvss/jvss.go
+++ protocols/jvss/jvss.go
@@ -1,6 +1,7 @@
 package jvss
 
 import (
+	"errors"
 	"sync"
 
 	"github.com/dedis/cothority/lib/dbg"
@@ -55,6 +56,16 @@ func NewJVSS(node *sda.Node) (sda.ProtocolInstance, error) {
 		Done:     make(chan bool, 1),
 	}
 
+	// Setup message handlers
+	handlers := []interface{}{
+		jv.handleSetup,
+	}
+	for _, h := range handlers {
+		if err := jv.RegisterHandler(h); err != nil {
+			return nil, errors.New("Couldn't register handler: " + err.Error())
+		}
+	}
+
 	//go jv.waitForSetup()
 
 	return jv, nil
@@ -63,7 +74,19 @@ func NewJVSS(node *sda.Node) (sda.ProtocolInstance, error) {
 // Start initiates the JVSS protocol.
 func (jv *JVSS) Start() error {
 
-	jv.setup()
+	// Initiate the long-term shared key pair which can be used later on by the
+	// JVSS group to sign messages.
+	deal := jv.newDeal()
+	jv.addDeal(jv.nodeIdx(), deal)
+	db, _ := deal.MarshalBinary()
+
+	// broadcast our deal and wait for the deals from the others
+	msg := &MsgSetup{
+		Src:  jv.nodeIdx(),
+		Deal: db,
+	}
+	jv.broadcast(msg)
+
 	jv.Done <- true
 
 	return nil
@@ -79,18 +102,6 @@ func (jv *JVSS) Sign(msg []byte) (*poly.SchnorrSig, error) {
 	return nil, nil
 }
 
-// Setup initiates the long-term shared key pair which can be used later on by
-// the JVSS group to sign messages.
-func (jv *JVSS) setup() {
-
-	// TODO: setup the SharedSecret jvss.secret
-	deal := jv.newDeal()
-	jv.addDeal(jv.nodeIdx(), deal)
-
-	// broadcast our deal and wait for the deals from the others
-
-}
-
 func (jv *JVSS) waitForSetup() {
 
 }
@@ -102,7 +113,7 @@ func (jv *JVSS) newDeal() *poly.Deal {
 
 func (jv *JVSS) addDeal(idx int, deal *poly.Deal) {
 	if _, err := jv.receiver.AddDeal(idx, deal); err != nil {
-		dbg.Error("Error adding deal to receiver", idx, err)
+		dbg.Error("Error adding deal to receiver %d: %v", idx, err)
 	}
 	jv.numDeals += 1
 }
@@ -111,6 +122,12 @@ func (jv *JVSS) nodeIdx() int {
 	return jv.Node.TreeNode().EntityIdx
 }
 
-func (jv *JVSS) broadcast() {
-
+func (jv *JVSS) broadcast(msg interface{}) {
+	for idx, node := range jv.nodeList {
+		if idx != jv.nodeIdx() {
+			if err := jv.SendTo(node, msg); err != nil {
+				dbg.Errorf("Couldn't send msg to node %d: %v", idx, err)
+			}
+		}
+	}
 }
