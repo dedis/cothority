00d57bbb40b6a76e1e13ce836a7f6454f4fe8c10
Philipp Jovanovic <philipp@jovanovic.io>
2016-04-02 09:24:43 +0200

    jvss: add handlers.go

---
+++ protocols/jvss/handlers.go
@@ -0,0 +1,36 @@
+package jvss
+
+import (
+	"fmt"
+
+	"github.com/dedis/cothority/lib/sda"
+	"github.com/dedis/crypto/poly"
+)
+
+type SetupMsg struct {
+	Src  int
+	Deal []byte
+}
+
+// WMsgSetup is a SDA-wrapper around R4
+type WSetupMsg struct {
+	*sda.TreeNode
+	SetupMsg
+}
+
+func (jv *JVSS) handleSetup(m WSetupMsg) error {
+	msg := m.SetupMsg
+	//dbg.Lvl1(fmt.Sprintf("Node %d: received setup msg from %d", jv.nodeIdx(), msg.Src))
+
+	// Create and send our own deal if not done so before
+	jv.setupDeal()
+
+	// Unmarshal received deal and store it
+	d := new(poly.Deal).UnmarshalInit(jv.info.T, jv.info.R, jv.info.N, jv.keyPair.Suite)
+	if err := d.UnmarshalBinary(msg.Deal); err != nil {
+		return fmt.Errorf("Node %d could not unmarshal deal received from %d: %v", jv.nodeIdx(), msg.Src, err)
+	}
+	jv.addDeal(jv.nodeIdx(), d) // TODO: why give jv.nodeIdx()?
+
+	return nil
+}
