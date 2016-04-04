296fce442da9bfe1030e698c758d79dfc23fffa0
Linus Gasser <linus.gasser@epfl.ch>
2016-04-04 09:30:48 +0200

    redoing broadcast

--- protocols/manage/broadcast.go
+++ protocols/manage/broadcast.go
@@ -26,8 +26,8 @@ func NewBroadcastProtocol(n *sda.Node) (sda.ProtocolInstance, error) {
 
 // Start will contact everyone and makes the connections
 func (b *Broadcast) Start() error {
-	for _, tn := range b.Tree().ListNodes() {
-		b.SendTo(tn, &Announce{})
+	for i, tn := range b.Tree().ListNodes()[1:] {
+		b.SendTo(tn, &ConnectToAll{i})
 	}
 	dbg.Lvl3(b.Name(), "Sent Announce to everyone")
 	return nil
@@ -35,54 +35,33 @@ func (b *Broadcast) Start() error {
 
 // handleAnnounce receive the announcement from another node
 // it reply with an ACK.
-func (b *Broadcast) handleAnnounce(tn *sda.TreeNode) {
-	b.SendTo(tn, &ACK{})
+func (b *Broadcast) handleConnectToAll(msg struct {
+	*sda.TreeNode
+	ConnectToAll
+}) {
+	// Only connect to all nodes that are not the server
+	for _, tn := range b.Tree().ListNodes()[msg.Index+1:] {
+		dbg.Lvl3("Connecting to", tn.Entity.String())
+		b.SendTo(tn, &Connected{})
+	}
+	b.SendTo(msg.TreeNode, &Connected{})
 }
 
 // It checks if we have sent an Announce to this treenode (hopefully yes^^)
 // if yes it checks if everyone has been ACK'd, if yes, it finishes.
-func (b *Broadcast) handleACK(tn *sda.TreeNode) {
-	if _, ok := b.listNode[tn.Id]; !ok {
-		dbg.Error(b.Name(), "Broadcast Received ACK from unknown treenode")
-	}
-
-	b.ackdNode++
-	if b.ackdNode == len(b.listNode) {
-		if !b.IsRoot() {
-			b.SendTo(b.Tree().Root, &OK{})
-			dbg.Lvl3(b.Name(), "Received ALL ACK (notified the root)")
-		}
-	}
-}
-
-func (b *Broadcast) handleOk(tn *sda.TreeNode) {
-	if _, ok := b.listNode[tn.Id]; !ok {
-		dbg.Error(b.Name(), "Broadcast Received ACK from unknown treenode")
-	}
-
-	b.okdNode++
-	dbg.Lvl2(b.Name(), "Received OK with ackNode=", b.ackdNode, " and okdNode=", b.okdNode)
-	if b.ackdNode == len(b.listNode) && b.okdNode == len(b.listNode) {
-		// Yahooo we are done
-		dbg.Lvl3(b.Name(), " Knows EVERYONE is connected to EVERYONE")
-		b.done <- true
-		if b.onDoneCb != nil {
-			b.onDoneCb()
-		}
-	}
-
+func (b *Broadcast) handleConnected(struct {
+	*sda.TreeNode
+	Connected
+}) {
 }
 
 func (b *Broadcast) RegisterOnDone(fn func()) {
 	b.onDoneCb = fn
 }
 
-type Announce struct {
+type ConnectToAll struct {
+	Index int
 }
 
-type ACK struct {
-}
-
-// OK means I am connected with everyone and I tell you this.
-type OK struct {
+type Connected struct {
 }
