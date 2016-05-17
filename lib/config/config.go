73c130a8420d431d6d52b54b2285a52147b0e087
nikkolasg <nikkolasg@gmail.com>
2016-05-17 13:04:52 +0200

    linter

--- lib/config/config.go
+++ lib/config/config.go
@@ -149,6 +149,7 @@ func ReadGroupToml(f io.Reader) (*sda.EntityList, error) {
 	return el, nil
 }
 
+// Save writes the grouptoml definition into the file
 func (gt *GroupToml) Save(fname string) error {
 	file, err := os.Create(fname)
 	if err != nil {
@@ -187,7 +188,7 @@ func (s *ServerToml) toEntity(suite abstract.Suite) (*network.Entity, error) {
 	return network.NewEntity(public, s.Addresses...), nil
 }
 
-// Returns a ServerToml out of a public key and some addresses => to be printed
+// NewServerToml returns  a ServerToml out of a public key and some addresses => to be printed
 // or written to a file
 func NewServerToml(suite abstract.Suite, public abstract.Point, addresses ...string) *ServerToml {
 	var buff bytes.Buffer
