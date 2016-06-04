89a1d598247f5c965ac9ed88c4e49a29828f8307
Ludovic Barman <ludovic.barman@gmail.com>
2016-06-04 15:42:18 +0200

    Fills in the Trustee's neff shuffle

---
+++ protocols/prifi/config/config.go
@@ -0,0 +1,43 @@
+package config
+
+import (
+	"github.com/dedis/cothority/protocols/prifi/dcnet"
+	"github.com/dedis/crypto/config"
+	"github.com/dedis/crypto/nist"
+	"github.com/dedis/crypto/suites"
+)
+
+//used to make sure everybody has the same version of the software. must be updated manually
+const LLD_PROTOCOL_VERSION = 3
+
+//sets the crypto suite used
+var CryptoSuite = nist.NewAES128SHA256P256()
+
+//sets the factory for the dcnet's cell encoder/decoder
+var Factory = dcnet.SimpleCoderFactory
+
+var configFile config.File
+
+// Dissent config file format
+type ConfigData struct {
+	Keys config.Keys // Info on configured key-pairs
+}
+
+var configData ConfigData
+var keyPairs []config.KeyPair
+
+func ReadConfig() error {
+
+	// Load the configuration file
+	configFile.Load("dissent", &configData)
+
+	// Read or create our public/private keypairs
+	pairs, err := configFile.Keys(&configData.Keys, suites.All(), CryptoSuite)
+	if err != nil {
+		return err
+	}
+	keyPairs = pairs
+	println("Loaded", len(pairs), "key-pairs")
+
+	return nil
+}
--- protocols/prifi/trustee.go
+++ protocols/prifi/trustee.go
@@ -1,19 +1,27 @@
 package prifi
 
 import (
+	"errors"
+
+	"github.com/dedis/cothority/lib/dbg"
+	"github.com/dedis/cothority/protocols/prifi/config"
 	"github.com/dedis/cothority/protocols/prifi/dcnet"
 	"github.com/dedis/crypto/abstract"
+	crypto_proof "github.com/dedis/crypto/proof"
+	"github.com/dedis/crypto/shuffle"
 )
 
 //State information to hold :
+var trusteeState TrusteeState
+
 type TrusteeState struct {
 	Name          string
 	TrusteeId     int
 	PayloadLength int
 	//activeConnection net.Conn //those are kept by the SDA stack
 
-	//PublicKey  abstract.Point //those are kept by the SDA stack
-	//privateKey abstract.Secret //those are kept by the SDA stack
+	PublicKey  abstract.Point  //those are kept by the SDA stack
+	privateKey abstract.Secret //those are kept by the SDA stack
 
 	nClients  int
 	nTrustees int
@@ -31,6 +39,53 @@ type TrusteeState struct {
 //REL_TRU_TELL_TRANSCRIPT
 
 func (p *PriFiProtocolHandlers) Received_REL_TRU_TELL_CLIENTS_PKS_AND_EPH_PKS_AND_BASE(msg Struct_REL_TRU_TELL_CLIENTS_PKS_AND_EPH_PKS_AND_BASE) error {
+
+	clientsPks := msg.Pks
+	clientsEphemeralPks := msg.EphPks
+	base := msg.Base
+
+	//sanity check
+	if len(clientsPks) < 2 || len(clientsEphemeralPks) < 2 || len(clientsPks) != len(clientsEphemeralPks) {
+		e := "One of the following check failed : len(clientsPks)>1, len(clientsEphemeralPks)>1, len(clientsPks)==len(clientsEphemeralPks)"
+		dbg.Error(e)
+		return errors.New(e)
+	}
+
+	//fill in the clients keys
+	for i := 0; i < len(clientsPks); i++ {
+		//trusteeState.ClientPublicKeys[i] = clientsPublicKeys[i] not sure this is needed since there is a tree ?
+		trusteeState.sharedSecrets[i] = config.CryptoSuite.Point().Mul(clientsPks[i], trusteeState.privateKey)
+	}
+
+	//TODO : THIS IS NOT SHUFFLING; THIS IS A PLACEHOLDER FOR THE ACTUAL SHUFFLE. NOT SHUFFLE IS DONE
+
+	//perform the neff-shuffle
+	rand := config.CryptoSuite.Cipher([]byte(trusteeState.Name)) //TODO: this should be random
+	H := trusteeState.PublicKey
+	X := clientsEphemeralPks
+	Y := X
+
+	_, _, prover := shuffle.Shuffle(config.CryptoSuite, nil, H, X, Y, rand)
+	_, err := crypto_proof.HashProve(config.CryptoSuite, "PairShuffle", rand, prover)
+	if err != nil {
+		e := "Could not neff-shuffle, error is " + err.Error()
+		dbg.Error(e)
+		return errors.New(e)
+	}
+
+	//base2, ephPublicKeys2, proof := NeffShuffle(base, ephPublicKey)
+	base2 := base
+	ephPublicKeys2 := clientsEphemeralPks
+	proof := make([]byte, 50)
+
+	//send the answer
+	toSend := &TRU_REL_TELL_NEW_BASE_AND_EPH_PKS{base2, ephPublicKeys2, proof}
+	err = p.SendTo(p.Parent(), toSend) //TODO : this should be the root ! make sure of it
+	if err != nil {
+		e := "Could not send REL_CLI_DOWNSTREAM_DATA, error is " + err.Error()
+		dbg.Error(e)
+		return errors.New(e)
+	}
 
 	return nil
 }
