273b9f87b0e6ad0fd0e2ecf9cd557e8b154c2514
nikkolasg <nikkolasg@gmail.com>
2016-06-16 14:50:22 +0200

    removed all {protocols,services}/cosi dependencies

--- services/services.go
+++ services/services.go
@@ -1,7 +1,4 @@
 package services
 
-import (
-	// Importing the services so they register their services to SDA
-	// automatically when importing github.com/dedis/cothority/services
-	_ "github.com/dedis/cothority/services/cosi"
-)
+// Importing the services so they register their services to SDA
+// automatically when importing github.com/dedis/cothority/services
