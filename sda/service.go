c8dbd126bc53c58a9bc91d157be3ac9559323166
Linus Gasser <linus.gasser@epfl.ch>
2016-07-21 18:42:17 +0200

    added a mutex to serviceFactory

--- sda/service.go
+++ sda/service.go
@@ -72,6 +72,7 @@ type serviceFactory struct {
 	translations map[string]ServiceID
 	// Inverse mapping of ServiceId => string
 	inverseTr map[ServiceID]string
+	mutex     sync.Mutex
 }
 
 // ServiceFactory is the global service factory to instantiate Services
@@ -85,6 +86,7 @@ var ServiceFactory = serviceFactory{
 // mapping and the creation function.
 func (s *serviceFactory) Register(name string, fn NewServiceFunc) {
 	id := ServiceID(uuid.NewV5(uuid.NamespaceURL, name))
+	s.mutex.Lock()
 	if _, ok := s.constructors[id]; ok {
 		// called at init time so better panic than to continue
 		log.Lvl1("RegisterService():", name)
@@ -92,6 +94,7 @@ func (s *serviceFactory) Register(name string, fn NewServiceFunc) {
 	s.constructors[id] = fn
 	s.translations[name] = id
 	s.inverseTr[id] = name
+	s.mutex.Unlock()
 }
 
 // RegisterNewService is a wrapper around service factory
@@ -101,6 +104,8 @@ func RegisterNewService(name string, fn NewServiceFunc) {
 
 // RegisteredServices returns all the services registered
 func (s *serviceFactory) registeredServicesID() []ServiceID {
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
 	var ids = make([]ServiceID, 0, len(s.constructors))
 	for id := range s.constructors {
 		ids = append(ids, id)
@@ -110,6 +115,8 @@ func (s *serviceFactory) registeredServicesID() []ServiceID {
 
 // RegisteredServicesByName returns all the names of the services registered
 func (s *serviceFactory) RegisteredServicesName() []string {
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
 	var names = make([]string, 0, len(s.translations))
 	for n := range s.translations {
 		names = append(names, n)
@@ -119,6 +126,8 @@ func (s *serviceFactory) RegisteredServicesName() []string {
 
 // ServiceID returns the ServiceID out of the name of the service
 func (s *serviceFactory) ServiceID(name string) ServiceID {
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
 	id, ok := s.translations[name]
 	if !ok {
 		return NilServiceID
@@ -128,6 +137,8 @@ func (s *serviceFactory) ServiceID(name string) ServiceID {
 
 // Name returns the Name out of the ID
 func (s *serviceFactory) Name(id ServiceID) string {
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
 	var name string
 	var ok bool
 	if name, ok = s.inverseTr[id]; !ok {
@@ -138,6 +149,8 @@ func (s *serviceFactory) Name(id ServiceID) string {
 
 // start launches a new service
 func (s *serviceFactory) start(name string, c *Context, path string) (Service, error) {
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
 	var id ServiceID
 	var ok bool
 	if id, ok = s.translations[name]; !ok {
