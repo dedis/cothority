9f3364e9d5e14fc1b0b24c1e1127d0458dd6fb61
nikkolasg <nikkolasg@gmail.com>
2016-08-02 15:11:44 +0200

    small details

--- install.sh
+++ install.sh
@@ -5,11 +5,11 @@
 ## `refactor_`, it will try to checkout the same branch name 
 ## in dedis/cosi.
 export PR=https://api.github.com/repos/$TRAVIS_REPO_SLUG/pulls/$TRAVIS_PULL_REQUEST
-export BRANCH=$(if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then echo $TRAVIS_BRANCH; else echo `curl -s $PR | jq -r .head.ref`; fi)
+export BRANCH=$(if [[ "$TRAVIS_PULL_REQUEST" == "false" ]]; then echo $TRAVIS_BRANCH; else echo `curl -s $PR | jq -r .head.ref`; fi)
 echo "TRAVIS_BRANCH=$TRAVIS_BRANCH, PR=$PR, BRANCH=$BRANCH"
 
 pattern="refactor_*"; \
-if [[ $BRANCH =~ $pattern ]]; then \
+if [[ "$BRANCH" =~ "$pattern" ]]; then \
     repo=github.com/dedis/cosi; \
     go get $$repo; \
     cd $GOPATH/src/$repo; \
