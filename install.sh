365a5b69cfa2b39727d443901a1c7d13925a5e3c
nikkolasg <nikkolasg@gmail.com>
2016-08-02 15:10:31 +0200

    moved everything into bash

--- install.sh
+++ install.sh
@@ -4,6 +4,10 @@
 ## If the branch from where the PR comes from starts with
 ## `refactor_`, it will try to checkout the same branch name 
 ## in dedis/cosi.
+export PR=https://api.github.com/repos/$TRAVIS_REPO_SLUG/pulls/$TRAVIS_PULL_REQUEST
+export BRANCH=$(if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then echo $TRAVIS_BRANCH; else echo `curl -s $PR | jq -r .head.ref`; fi)
+echo "TRAVIS_BRANCH=$TRAVIS_BRANCH, PR=$PR, BRANCH=$BRANCH"
+
 pattern="refactor_*"; \
 if [[ $BRANCH =~ $pattern ]]; then \
     repo=github.com/dedis/cosi; \
