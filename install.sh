25905a90a86ffa7870917af944ec731b2833f7cf
nikkolasg <nikkolasg@gmail.com>
2016-08-02 17:01:29 +0200

    does it really checkout the right branch ?

--- install.sh
+++ install.sh
@@ -9,14 +9,16 @@ export BRANCH=$(echo `curl -s $PR | jq -r .head.ref`)
 
 echo "TRAVIS_BRANCH=$TRAVIS_BRANCH, PR=$PR, BRANCH=$BRANCH"
 
-pattern="refactor_*"; \
-if [[ $BRANCH =~ $pattern ]]; then \
+pattern="refactor_*"; 
+if [[ $BRANCH =~ $pattern ]]; then 
     echo "Using refactor branch ..."
-    repo=github.com/dedis/cosi; \
-    go get $repo; \
-    cd $GOPATH/src/$repo; \
-    git checkout -f $BRANCH; \
-fi;\
-cd $GOPATH/src/github.com/dedis/cothority; \
+    repo=github.com/dedis/cosi; 
+    go get $repo; 
+    cd $GOPATH/src/$repo; 
+    git checkout -f $BRANCH; 
+    git pull;
+    echo "Checked out!";
+fi;
+cd $GOPATH/src/github.com/dedis/cothority; 
 go get -t ./...
 
