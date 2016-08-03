5d0509e44263bb75c97dc61766bc25cb7a1d7f2b
nikkolasg <nikkolasg@gmail.com>
2016-08-03 10:04:45 +0200

    local way of getting head branch of PR ?

--- install.sh
+++ install.sh
@@ -4,10 +4,16 @@
 ## If the branch from where the PR comes from starts with
 ## `refactor_`, it will try to checkout the same branch name 
 ## in dedis/cosi.
-export PR=https://api.github.com/repos/$TRAVIS_REPO_SLUG/pulls/$TRAVIS_PULL_REQUEST
-export BRANCH=$(echo `curl -s $PR | jq -r .head.ref`)
-
-echo "TRAVIS_BRANCH=$TRAVIS_BRANCH, PR=$PR, BRANCH=$BRANCH"
+# Method 1 from
+# http://graysonkoonce.com/getting-the-current-branch-name-during-a-pull-request-in-travis-ci/
+#export PR=https://api.github.com/repos/$TRAVIS_REPO_SLUG/pulls/$TRAVIS_PULL_REQUEST
+#export BRANCH=$(echo `curl -s $PR | jq -r .head.ref`)
+#echo "TRAVIS_BRANCH=$TRAVIS_BRANCH, PR=$PR, BRANCH=$BRANCH"
+# method 2 from https://github.com/travis-ci/travis-ci/issues/1633
+cd $GOPATH/src/github.com/dedis/cothority; 
+git fetch --tags
+git fetch --unshallow
+BRANCH=`git rev-parse --abbrev-ref HEAD`
 
 pattern="refactor_*"; 
 if [[ $BRANCH =~ $pattern ]]; then 
@@ -19,6 +25,5 @@ if [[ $BRANCH =~ $pattern ]]; then
     git pull;
     echo $(git rev-parse --abbrev-ref HEAD);
 fi;
-cd $GOPATH/src/github.com/dedis/cothority; 
 go get -t ./...
 
