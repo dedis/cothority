a8139b2027a45c855397d42152d60190249e53ff
nikkolasg <nikkolasg@gmail.com>
2016-08-04 10:40:04 +0200

    new ways of getting that branch..

--- install.sh
+++ install.sh
@@ -7,7 +7,15 @@
 # Method 1 from
 # http://graysonkoonce.com/getting-the-current-branch-name-during-a-pull-request-in-travis-ci/
 export PR=https://api.github.com/repos/$TRAVIS_REPO_SLUG/pulls/$TRAVIS_PULL_REQUEST
-export BRANCH=$(echo `curl -s $PR | jq -r .head.ref`)
+
+i=0
+while [ -z $BRANCH ]
+do
+    a=`expr $a + 1`
+    export BRANCH=$(echo `curl -s $PR | jq -r .head.ref`)
+    echo "Got BRANCH=$BRANCH (round $a)"
+done
+
 echo "TRAVIS_BRANCH=$TRAVIS_BRANCH, PR=$PR, BRANCH=$BRANCH"
 
 # method 2 from https://github.com/travis-ci/travis-ci/issues/1633
@@ -28,9 +36,8 @@ echo "TRAVIS_BRANCH=$TRAVIS_BRANCH, PR=$PR, BRANCH=$BRANCH"
 #fi;
 
 #BRANCH=$TRAVIS_BRANCH
-echo "Branch is $BRANCH"
 
-pattern="refactor_*"; 
+pattern="refactor_*";  
 if [[ $BRANCH =~ $pattern ]]; then 
     echo "Using refactor branch ..."
     repo=github.com/dedis/cosi; 
@@ -41,14 +48,14 @@ if [[ $BRANCH =~ $pattern ]]; then
     echo $(git rev-parse --abbrev-ref HEAD);
 fi;
 
-echo "Using refactor branch ..."
-repo=github.com/dedis/cosi; 
-go get -d $repo; 
-cd $GOPATH/src/$repo; 
-git checkout -f refactor_mocking;
-git pull;
-echo $(git rev-parse --abbrev-ref HEAD);
-
+##echo "Using refactor branch ..."
+##repo=github.com/dedis/cosi; 
+##go get -d $repo; 
+##cd $GOPATH/src/$repo; 
+##git checkout -f refactor_mocking;
+##git pull;
+##echo $(git rev-parse --abbrev-ref HEAD);
+##
 cd $GOPATH/src/github.com/dedis/cothority; 
 go get -t ./...
 
