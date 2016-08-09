0b9446b8ae8955f260bed957eb5ac22f026f5e33
Liamsi <Liamsi@users.noreply.github.com>
2016-08-09 21:15:09 +0200

    Is this in anyways more robust than the other curl approach (which fails more often with BRAN=null than works)

--- install.sh
+++ install.sh
@@ -9,10 +9,17 @@
 if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
   BRANCH=$TRAVIS_BRANCH
 else
-  # Method from
-  # http://graysonkoonce.com/getting-the-current-branch-name-during-a-pull-request-in-travis-ci/
-  PR=https://api.github.com/repos/$TRAVIS_REPO_SLUG/pulls/$TRAVIS_PULL_REQUEST
-  BRANCH=$(echo `curl -s $PR | jq -r .head.ref`)
+  # source: https://gist.github.com/derekstavis/0526ac13cfecb5d6ffe5#file-travis-github-pull-request-integration-sh
+  GITHUB_PR_URL=https://api.github.com/repos/$TRAVIS_REPO_SLUG/pulls/$TRAVIS_PULL_REQUEST
+  GITHUB_PR_BODY=$(curl -s $GITHUB_PR_URL 2>/dev/null)
+
+  if [[ $GITHUB_PR_BODY =~ \"ref\":\ *\"([a-zA-Z0-9_-]*)\" ]]; then
+    export TRAVIS_BRANCH=${BASH_REMATCH[1]}
+  fi
+
+  if [[ $GITHUB_PR_BODY =~ \"repo\":.*\"clone_url\":\ *\"https://github\.com/([a-zA-Z0-9_-]*/[a-zA-Z0-9_-]*)\.git.*\"base\" ]]; then
+    export TRAVIS_REPO_SLUG=${BASH_REMATCH[1]}
+  fi
 fi
 
 # If you don't believe in travis-magic:
