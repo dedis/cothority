0dfa6cbb67118a7c6856d5401c4b80f77aca1a0f
Philipp Jovanovic <philipp@jovanovic.io>
2016-08-16 14:19:07 +0200

    ZK-proofs

--- protocols/randhound/proof.go
+++ protocols/randhound/proof.go
@@ -1,8 +1,10 @@
 package randhound
 
 import (
-	"bytes"
+	"errors"
+	"fmt"
 
+	"github.com/dedis/cothority/crypto"
 	"github.com/dedis/crypto/abstract"
 	"github.com/dedis/crypto/random"
 )
@@ -10,81 +12,145 @@ import (
 // Package (XXX: name) provides functionality to show equality of discrete
 // logarithms (dlog) through non-interactive zero-knowledge (NIZK) proofs.
 
-// Proof resembles a NIZK dlog-equality proof.
+// Proof resembles a NIZK dlog-equality proof. Can hold multiple proofs.
 type Proof struct {
-	suite abstract.Suite  // suite
-	g     abstract.Point  // first base point G
-	h     abstract.Point  // second base point H
-	c     abstract.Scalar // challenge
-	r     abstract.Scalar // response
-	gv    abstract.Point  // public commitment with respect to G
-	hv    abstract.Point  // public commitment with respect to H
+	suite abstract.Suite
+	base  []ProofBase
+	core  []ProofCore
+}
+
+// ProofBase contains the base points for a proof
+type ProofBase struct {
+	g abstract.Point
+	h abstract.Point
+}
+
+// ProofCore ...
+type ProofCore struct {
+	c  abstract.Scalar // challenge
+	r  abstract.Scalar // response
+	gv abstract.Point  // public commitment with respect to base point g
+	hv abstract.Point  // public commitment with respect to base point h
 }
 
 // NewProof creates a new NIZK dlog-equality proof.
-func NewProof(suite abstract.Suite, g abstract.Point, h abstract.Point) *Proof {
-	return &Proof{suite: suite, g: g, h: h}
+func NewProof(suite abstract.Suite, point ...abstract.Point) (*Proof, error) {
+
+	if len(point)%2 != 0 {
+		return nil, errors.New("Received odd number of points")
+	}
+
+	base := make([]ProofBase, len(point)/2)
+
+	for i := 0; i < len(point)/2; i += 1 {
+		base[i] = ProofBase{g: point[2*i], h: point[2*i+1]}
+	}
+
+	return &Proof{suite: suite, base: base}, nil
 }
 
 // Setup initializes the proof by randomly selecting a commitment v and then
 // determining the challenge c = H(G,H,x,v) and the response r = v - cx.
-func (p *Proof) Setup(x abstract.Scalar) error {
+func (p *Proof) Setup(scalar ...abstract.Scalar) error {
 
-	// Commitment
-	v := p.suite.Scalar().Pick(random.Stream)
-	p.gv = p.suite.Point().Mul(p.g, v)
-	p.hv = p.suite.Point().Mul(p.h, v)
-
-	// Challenge
-	buf := new(bytes.Buffer)
-
-	gb, err := p.g.MarshalBinary()
-	if err != nil {
-		return err
+	if len(scalar) != len(p.base) {
+		return errors.New("Received unexpected number of scalars")
 	}
-	buf.Write(gb)
 
-	hb, err := p.h.MarshalBinary()
-	if err != nil {
-		return err
+	p.core = make([]ProofCore, len(scalar))
+	for i, x := range scalar {
+
+		gx := p.suite.Point().Mul(p.base[i].g, x)
+		hx := p.suite.Point().Mul(p.base[i].h, x)
+
+		// Commitment
+		v := p.suite.Scalar().Pick(random.Stream)
+		gv := p.suite.Point().Mul(p.base[i].g, v)
+		hv := p.suite.Point().Mul(p.base[i].h, v)
+
+		// Challenge
+		cb, err := crypto.HashArgsSuite(p.suite, gx, hx, x, v)
+		if err != nil {
+			return err
+		}
+		c := p.suite.Scalar().Pick(p.suite.Cipher(cb))
+
+		// Response
+		r := p.suite.Scalar()
+		r.Mul(x, c).Sub(v, r)
+
+		p.core[i] = ProofCore{c, r, gv, hv}
 	}
-	buf.Write(hb)
-
-	xb, err := x.MarshalBinary()
-	if err != nil {
-		return err
-	}
-	buf.Write(xb)
-
-	vb, err := v.MarshalBinary()
-	if err != nil {
-		return err
-	}
-	buf.Write(vb)
-
-	cb := abstract.Sum(p.suite, buf.Bytes())
-	c := p.suite.Scalar().Pick(p.suite.Cipher(cb))
-	p.c = c
-
-	// Response
-	r := p.suite.Scalar()
-	r.Mul(x, c).Sub(v, r)
-	p.r = r
 
 	return nil
 }
 
+// SetupCollective ...
+//func (p *Proof) SetupCollective(scalar ...abstract.Scalar) error {
+
+//if len(scalar) != len(p.base) {
+//return errors.New("Received number of points does not match number of base points")
+//}
+
+//p.core = make([]ProofCore, len(scalar))
+//v := make([]abstract.Scalar, len(scalar))
+//X := make([]abstract.Point, len(scalar))
+//Y := make([]abstract.Point, len(scalar))
+//V := make([]abstract.Point, 2*len(scalar))
+//for i, x := range scalar {
+
+//X[i] = p.suite.Point().Mul(p.base[i].g, x) // gx
+//Y[i] = p.suite.Point().Mul(p.base[i].h, x) // hx
+
+//// Commitments
+//v[i] = p.suite.Scalar().Pick(random.Stream)     // v
+//V[i] = p.suite.Point().Mul(p.base[i].g, v[i])   // gv
+//V[i+1] = p.suite.Point().Mul(p.base[i].h, v[i]) // hv
+//}
+
+//X = append(X, Y...)
+//X = append(X, V...)
+
+//// Collective challenge
+//cb, err := crypto.HashArgsSuite(p.suite, X...)
+//if err != nil {
+//return err
+//}
+//c := p.suite.Scalar().Pick(p.suite.Cipher(cb))
+
+//// Responses
+//for i, x := range scalar {
+//r := p.suite.Scalar()
+//r.Mul(x, c).Sub(v[i], r)
+//p.core[i] = ProofCore{c, r, V[i], V[i+1]}
+//}
+
+//return nil
+//}
+
 // Verify validates the proof against the given input by checking that
 // v * G == r * G + c * (x * G) and v * H == r * H + c * (x * H).
-func (p *Proof) Verify(gx abstract.Point, hx abstract.Point) bool {
+func (p *Proof) Verify(point ...abstract.Point) ([]int, error) {
 
-	gr := p.suite.Point().Mul(p.g, p.r)
-	hr := p.suite.Point().Mul(p.h, p.r)
-	gxc := p.suite.Point().Mul(gx, p.c)
-	hxc := p.suite.Point().Mul(hx, p.c)
+	if len(point) != 2*len(p.base) {
+		return nil, errors.New("Received unexpected number of points")
+	}
 
-	x := p.suite.Point().Add(gr, gxc)
-	y := p.suite.Point().Add(hr, hxc)
+	failed := make([]int, 0)
+	for i := 0; i < len(p.base); i += 2 {
 
-	return p.gv.Equal(x) && p.hv.Equal(y)
+		gr := p.suite.Point().Mul(p.base[i].g, p.core[i].r)
+		hr := p.suite.Point().Mul(p.base[i].h, p.core[i].r)
+		gxc := p.suite.Point().Mul(point[2*i], p.core[i].c)
+		hxc := p.suite.Point().Mul(point[2*i+1], p.core[i].c)
+		x := p.suite.Point().Add(gr, gxc)
+		y := p.suite.Point().Add(hr, hxc)
+
+		fmt.Println(p.core[i].gv.Equal(x), p.core[i].hv.Equal(y))
+
+		if !(p.core[i].gv.Equal(x) && p.core[i].hv.Equal(y)) {
+			failed = append(failed, i)
+		}
+	}
+	return failed, nil
 }
--- protocols/randhound/proof_test.go
+++ protocols/randhound/proof_test.go
@@ -1,6 +1,7 @@
 package randhound_test
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/dedis/cothority/protocols/randhound"
@@ -13,17 +14,30 @@ func TestProof(t *testing.T) {
 	suite := edwards.NewAES128SHA256Ed25519(false)
 
 	x := suite.Scalar().Pick(random.Stream)
-	g, _ := suite.Point().Pick([]byte("G"), random.Stream)
-	h, _ := suite.Point().Pick([]byte("H"), random.Stream)
+	y := suite.Scalar().Pick(random.Stream)
+	g1, _ := suite.Point().Pick([]byte("G1"), random.Stream)
+	h1, _ := suite.Point().Pick([]byte("H1"), random.Stream)
+	g2, _ := suite.Point().Pick([]byte("G2"), random.Stream)
+	h2, _ := suite.Point().Pick([]byte("H2"), random.Stream)
 
-	gx := suite.Point().Mul(g, x)
-	hx := suite.Point().Mul(h, x)
+	g1x := suite.Point().Mul(g1, x)
+	h1x := suite.Point().Mul(h1, x)
 
-	p := randhound.NewProof(suite, g, h)
-	p.Setup(x)
+	g2y := suite.Point().Mul(g2, y)
+	//h2y := suite.Point().Mul(h2, y)
 
-	if !p.Verify(gx, hx) {
-		t.Fatal("Verification of discrete logarithm proof failed")
+	p, _ := randhound.NewProof(suite, g1, h1, g2, h2)
+	p.Setup(x, y)
+
+	failed, err := p.Verify(g1x, h1x, g2y, g2y)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	fmt.Println(failed)
+
+	if len(failed) != 0 {
+		t.Fatal("Verification of discrete logarithm proof(s) failed:", failed)
 	}
 
 }
