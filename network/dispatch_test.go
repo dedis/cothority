58c3f02f75dc3586914950ef3d9fd4d0e69820f1
Nicolas Gailly <nicolas.gailly@epfl.ch>
2016-08-25 16:19:39 +0200

    added test blocking dispatcher

---
+++ network/dispatch_test.go
@@ -0,0 +1,43 @@
+package network
+
+import (
+	"testing"
+
+	"github.com/dedis/cothority/log"
+	"github.com/stretchr/testify/assert"
+)
+
+type basicProcessor struct {
+	msgChan chan Packet
+}
+
+func (bp *basicProcessor) Process(msg *Packet) {
+	bp.msgChan <- *msg
+}
+
+type basicMessage struct {
+	Value int
+}
+
+var basicMessageType = RegisterMessageType(&basicMessage{})
+
+func TestBlockingDispatcher(t *testing.T) {
+	defer log.AfterTest(t)
+
+	dispatcher := NewBlockingDispatcher()
+	processor := &basicProcessor{make(chan Packet, 1)}
+
+	dispatcher.RegisterProcessor(processor, basicMessageType)
+	dispatcher.Dispatch(&Packet{
+		Msg:     basicMessage{10},
+		MsgType: basicMessageType})
+
+	select {
+	case m := <-processor.msgChan:
+		msg, ok := m.Msg.(basicMessage)
+		assert.True(t, ok)
+		assert.Equal(t, msg.Value, 10)
+	default:
+		t.Error("No message received")
+	}
+}
