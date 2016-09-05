233cce7ba66ca43214cbed28676fd0cbfc16cffb
Nicolas Gailly <nicolas.gailly@epfl.ch>
2016-09-05 11:07:48 +0200

    comments on address

--- network/address_test.go
+++ network/address_test.go
@@ -14,8 +14,8 @@ func TestConnType(t *testing.T) {
 		{"tcp", PlainTCP},
 		{"tls", TLS},
 		{"purb", PURB},
-		{"tcp4", UnvalidConnType},
-		{"_tls", UnvalidConnType},
+		{"tcp4", InvalidConnType},
+		{"_tls", InvalidConnType},
 	}
 
 	for _, str := range tests {
@@ -37,13 +37,15 @@ func TestAddress(t *testing.T) {
 		{"tls://10.0.0.4:2000", true, TLS, "10.0.0.4:2000", "10.0.0.4", "2000"},
 		{"tcp://10.0.0.4:2000", true, PlainTCP, "10.0.0.4:2000", "10.0.0.4", "2000"},
 		{"purb://10.0.0.4:2000", true, PURB, "10.0.0.4:2000", "10.0.0.4", "2000"},
-		{"tls4://10.0.0.4:2000", false, UnvalidConnType, "", "", ""},
-		{"tls://1000.0.0.4:2000", false, UnvalidConnType, "", "", ""},
-		{"tlsx10.0.0.4:2000", false, UnvalidConnType, "", "", ""},
-		{"tls:10.0.0.4x2000", false, UnvalidConnType, "", "", ""},
-		{"tlsx10.0.0.4x2000", false, UnvalidConnType, "", "", ""},
-		{"tlxblurdie", false, UnvalidConnType, "", "", ""},
-		{"tls://blublublu", false, UnvalidConnType, "", "", ""},
+		{"tls4://10.0.0.4:2000", false, InvalidConnType, "", "", ""},
+		{"tls://1000.0.0.4:2000", false, InvalidConnType, "", "", ""},
+		{"tls://10.0.0.4:20000000", false, InvalidConnType, "", "", ""},
+		{"tls://10.0.0.4:-10", false, InvalidConnType, "", "", ""},
+		{"tlsx10.0.0.4:2000", false, InvalidConnType, "", "", ""},
+		{"tls:10.0.0.4x2000", false, InvalidConnType, "", "", ""},
+		{"tlsx10.0.0.4x2000", false, InvalidConnType, "", "", ""},
+		{"tlxblurdie", false, InvalidConnType, "", "", ""},
+		{"tls://blublublu", false, InvalidConnType, "", "", ""},
 	}
 
 	for i, str := range tests {
