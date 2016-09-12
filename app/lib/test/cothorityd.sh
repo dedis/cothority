c7a8100b5dbb6fa4bbd78c0fd01f461a5f9e89dd
Linus Gasser <linus.gasser@epfl.ch>
2016-09-12 10:43:01 +0200

    start

--- app/lib/test/cothorityd.sh
+++ app/lib/test/cothorityd.sh
@@ -3,6 +3,8 @@
 DBG_SRV=${DBG_SRV:-0}
 
 runCoCfg(){
+	rm -rf co$1
+	mkdir co$1
     echo -e "127.0.0.1:200$1\nco$1\n\n" | dbgRun runCo $1 setup
 }
 
@@ -21,16 +23,25 @@ runCo(){
     dbgRun ./cothorityd -d $DBG_SRV -c co$nb/config.toml $@
 }
 
+# first argument: number of configurations to create
+# second argument: number of cothorities to run
+# For each cothority that is started, its definition is copied to
+# the group.toml-file.
 cothoritySetup(){
+	nbrCfg=${1:-3}
+	nbrRun=${2:-2}
     DBG_OLD=$DBG_SHOW
     DBG_SHOW=0
-    runCoCfg 1
-    runCoCfg 2
-    runCoCfg 3
-    runCoBG 1
-    runCoBG 2
+    for n in $( seq $nbrCfg ); do
+    	runCoCfg $n
+    done
+    rm group.toml
+    for n in $( seq $nbrRun ); do
+    	runCoBG $n
+    	if [ $n != $nbrCfg ]; then
+    		tail -n 4 co$n/group.toml >> group.toml
+    	fi
+    done
     sleep 1
-    cp co1/group.toml .
-    tail -n 4 co2/group.toml >> group.toml
     DBG_SHOW=$DBG_OLD
 }
