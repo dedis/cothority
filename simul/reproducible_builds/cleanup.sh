af2b84db7345bfbe9b96b194944122defa24d1d1
Linus Gasser <linus.gasser@epfl.ch>
2016-09-15 16:18:59 +0200

    new data

--- simul/reproducible_builds/cleanup.sh
+++ simul/reproducible_builds/cleanup.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-/etc/init.d/docker stop
+/etc/init.d/docker restart
 # Remove compiled docker images
 docker rmi $(docker images | grep "reprod" | awk '{print $3}')
 docker rmi $(docker images | grep "none" | awk '{print $3}')
 
 # Remove log files
 rm platform/localhost/*.log
-/etc/init.d/docker start
+/etc/init.d/docker restart
