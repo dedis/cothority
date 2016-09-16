1cfbeef59fa1f56200ac646be842ed26ef2a21b0
Linus Gasser <linus.gasser@epfl.ch>
2016-09-16 10:09:32 +0200

    moved from setup to run

--- services/swupdate/reprobuild/cleanup.sh
+++ services/swupdate/reprobuild/cleanup.sh
@@ -2,8 +2,8 @@
 
 /etc/init.d/docker restart
 # Remove compiled docker images
-docker rmi $(docker images | grep "reprod" | awk '{print $3}')
-docker rmi $(docker images | grep "none" | awk '{print $3}')
+docker rmi -f $(docker images | grep "reprod" | awk '{print $3}')
+docker rmi -f $(docker images | grep "none" | awk '{print $3}')
 
 # Remove log files
 rm *.log
