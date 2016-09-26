424f3bde5f003d5e254b3fd7ab15245642698f0a
Nicolas Gailly <nicolas.gailly@epfl.ch>
2016-09-26 17:44:08 +0200

    better client tests

--- network/client_test.go
+++ network/client_test.go
@@ -2,9 +2,11 @@ package network
 
 import (
 	"testing"
+	"time"
 
 	"github.com/dedis/cothority/log"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestClientTCP(t *testing.T) {
@@ -24,20 +26,49 @@ func testClient(t *testing.T, fac routerFactory, cl clientFactory) {
 		t.Fatal(err)
 	}
 	go r.Start()
-	defer r.Stop()
 
 	proc := NewSendBackProc(t, r)
 	r.RegisterProcessor(proc, SimpleMessageType)
 
 	client := cl()
 	nm, err := client.Send(r.id, &SimpleMessage{3})
-	assert.Nil(t, err)
-	assert.Equal(t, 3, nm.Msg.(SimpleMessage).I)
+	require.Nil(t, err)
+	require.Equal(t, 3, nm.Msg.(SimpleMessage).I)
+
+	// client won't have any response
+	old := timeoutResponse
+	timeoutResponse = 10 * time.Millisecond
+	proc.drop = true
+	client = cl()
+	_, err = client.Send(r.id, &SimpleMessage{3})
+	if err == nil {
+		t.Fatal("Client should not be able to have a response")
+	}
+	// client will get an error message
+	proc.err = true
+	client = cl()
+	_, err = client.Send(r.id, &SimpleMessage{3})
+	if err == nil {
+		t.Fatal("Client should return an error")
+	}
+	timeoutResponse = old
+
+	// client won't connect
+	r.Stop()
+	c2 := cl()
+	_, err = c2.Send(r.id, &SimpleMessage{3})
+	if err == nil {
+		t.Fatal("Should not be able to send !!")
+	}
 }
 
 type SendBackProc struct {
 	r *Router
 	t *testing.T
+	// do we drop the connection or not
+	drop bool
+	// sendback error
+	err bool
 }
 
 func NewSendBackProc(t *testing.T, r *Router) *SendBackProc {
@@ -52,6 +83,18 @@ func (sbp *SendBackProc) Process(msg *Packet) {
 	if !ok {
 		sbp.t.Fatal("Not the message expected")
 	}
-	err := sbp.r.Send(msg.ServerIdentity, &simple)
-	assert.Nil(sbp.t, err)
+	if sbp.drop {
+		c := sbp.r.connection(msg.ServerIdentity.ID)
+		if c == nil {
+			sbp.t.Fatal("no connection?")
+		}
+		if err := c.Close(); err != nil {
+			sbp.t.Fatal("closing impossible??")
+		}
+	} else if sbp.err {
+		sbp.r.Send(msg.ServerIdentity, &StatusRet{"Error returning"})
+	} else {
+		err := sbp.r.Send(msg.ServerIdentity, &simple)
+		assert.Nil(sbp.t, err)
+	}
 }
