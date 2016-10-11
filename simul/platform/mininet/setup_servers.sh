174bbe706537456c3653fc57a1110db4177f61d2
Linus Gasser <linus.gasser@epfl.ch>
2016-10-11 16:20:36 +0200

    added scripts for setting up the servers

--- simul/platform/mininet/setup_servers.sh
+++ simul/platform/mininet/setup_servers.sh
@@ -1,22 +1,55 @@
 #!/usr/bin/env bash
+cd "$(dirname ${BASH_SOURCE[0]})"
 
-SERVER_GW="root@$1"
+SERVER_GW="$1"
 SERVERS="$@"
 KEYS=/tmp/server_keys
 
 rm $KEYS
 for s in $SERVERS; do
-	ssh-keygen -R root@$s
-	ssh-copy-id root@$s
-	ssh root@$s ssh-keygen
-	ssh root@$s cat .ssh/id_rsa.pub >> $KEYS
-	scp install_mininet.sh root@$s:
-	ssh root@$s "nohup . ./install_mininet.sh &"
+	echo Starting to install on $s
+	login=root@$s
+	ip=$( host $s | sed -e "s/.* //" )
+	ssh-keygen -R $s
+	ssh-keygen -R $ip
+	ssh-keyscan -t ssh-ed25519 $s >> ~/.ssh/known_hosts
+	ssh-copy-id $login
+	ssh $login "echo -e '\n\n\n' | ssh-keygen"
+	ssh $login cat .ssh/id_rsa.pub >> $KEYS
+	if ! ssh $login "grep -q 14.04 /etc/issue"; then
+		clear
+		echo "$s does not have Ubuntu 14.04 installed - aborting"
+		exit 1
+	fi
+	scp install_mininet.sh $login:
+	ssh -f $login "./install_mininet.sh &> /dev/null"
 done
 
-echo -n "Press <enter> when mininet is installed"
+echo -en "\n\nPlease press <enter> to start waiting on installation"
 read
 
+DONE=0
+echo $SERVERS
+NBR=$( echo $SERVERS | wc -w )
+while [ $DONE -lt $NBR ]; do
+	DONE=0
+	clear
+	echo "$( date ) - Waiting on $NBR servers - $DONE are done"
+	sleep 2
+	for s in $SERVERS; do
+		if ! ssh root@$s "ps ax | grep -v ps | grep install | grep -q mininet"; then
+			DONE=$(( DONE + 1 ))
+		fi
+	done
+done
+
+clear
+echo "All servers are done installing - copying ssh-keys"
+
 for s in $SERVERS; do
-	cat $KEYS | ssh $SERVER_GW "cat - >> .ssh/authorized_keys"
+	login=root@$s
+	cat $KEYS | ssh $login "cat - >> .ssh/authorized_keys"
+	ip=$( host $s | sed -e "s/.* //" )
+	ssh root@$SERVER_GW "ssh-keyscan -t ssh-ed25519 $s >> .ssh/known_hosts"
+	ssh root@$SERVER_GW "ssh-keyscan -t ssh-ed25519 $ip >> .ssh/known_hosts"
 done
