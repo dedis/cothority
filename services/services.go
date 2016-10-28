20ebfe97010939d113b3936bb6930aae41472cb4
Mahdi Zamani <mahdi.zamani@yale.edu>
2016-10-28 00:28:43 -0400

    Minor changes

--- services/services.go
+++ services/services.go
@@ -3,8 +3,8 @@ package services
 import (
 	// Importing the services so they register their services to SDA
 	// automatically when importing github.com/dedis/cothority/services
-	_ "github.com/dedis/cothority/services/guard"
-	_ "github.com/dedis/cothority/services/identity"
-	_ "github.com/dedis/cothority/services/skipchain"
-	_ "github.com/dedis/cothority/services/status"
+	//_ "github.com/dedis/cothority/services/guard"
+	//_ "github.com/dedis/cothority/services/identity"
+	//_ "github.com/dedis/cothority/services/skipchain"
+	//_ "github.com/dedis/cothority/services/status"
 )
