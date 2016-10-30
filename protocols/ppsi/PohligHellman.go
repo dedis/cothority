aaed75f370fcf58396e70b38b1dd5d0b0c50f718
lihiid <lihi.idan@yale.edu>
2016-10-30 16:38:04 -0700

    Create PohligHellman.go

---
+++ protocols/ppsi/PohligHellman.go
@@ -0,0 +1,18 @@
+package ppsi
+
+import (
+"github.com/dedis/crypto/abstract"
+)
+
+	func  PHDecrypt(set abstract.Point)(
+	M  abstract.Point, err error) {
+	
+	return
+	  
+	}
+	
+	func PHEncrypt(set abstract.Point)(
+	M  abstract.Point, err error) {
+	
+	  return
+	}
