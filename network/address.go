445edb79d9be48c538524d45dc11f74433de979e
Linus Gasser <linus.gasser@epfl.ch>
2016-11-03 09:11:09 +0100

    use common words

--- network/address.go
+++ network/address.go
@@ -122,7 +122,7 @@ func (a Address) Host() string {
 	if e != nil {
 		return ""
 	}
-	// IPv6 global address has to be in brackets.
+	// IPv6 unspecified address has to be in brackets.
 	if h == "::" {
 		h = "[::]"
 	}
