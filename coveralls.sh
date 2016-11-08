2f20cdd6e11f25c59893c99fa59fe9984086c342
Nicolas Gailly <nicolas.gailly@epfl.ch>
2016-11-08 09:57:19 +0100

    added -v switch to test

--- coveralls.sh
+++ coveralls.sh
@@ -27,7 +27,7 @@ all_tests_passed=true
 echo "mode: atomic" > profile.cov
 for dir in ${DIR_SOURCE};
 do
-    go test -short -race -covermode=atomic -coverprofile=$dir/profile.tmp $dir
+    go test -v -short -race -covermode=atomic -coverprofile=$dir/profile.tmp $dir
     if [ $? -ne 0 ]; then
         all_tests_passed=false
     fi
