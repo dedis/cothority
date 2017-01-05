213cec669d8917cfb0c60c42335db969fe06669e
Linus Gasser <linus.gasser@epfl.ch>
2017-01-05 09:39:41 +0100

    no race conditions

--- coveralls.sh
+++ coveralls.sh
@@ -2,7 +2,8 @@
 # Source: https://github.com/h12w/gosweep/blob/master/gosweep.sh
 
 DIR_SOURCE="$(find . -maxdepth 10 -type f -not -path '*/vendor*' -name '*.go' | xargs -I {} dirname {} | sort | uniq)"
-DIR_EXCLUDE="./cosi ./cisc"
+DIR_EXCLUDE="./cosi ./cisc ./byzcoin/*"
+echo $DIR_EXCLUDE
 
 BRANCH=$TRAVIS_PULL_REQUEST_BRANCH
 echo "Using branch $BRANCH"
