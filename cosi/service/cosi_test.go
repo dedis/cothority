e2e9f810c9e4e088cfbbc21fd74d5475cc301659
christophe <christophe@bertrand-feilen.ch>
2017-03-31 15:04:00 +0200

    refined test

--- cosi/service/cosi_test.go
+++ cosi/service/cosi_test.go
@@ -22,43 +22,23 @@ func TestServiceCosi(t *testing.T) {
 	hosts, el, _ := local.GenTree(5, false)
 	defer local.CloseAll()
 
-	// Send a request to the service
-	client := NewTestClient(local)
-	msg := []byte("hello cosi service")
-	log.Lvl1("Sending request to service...")
-	res, err := client.SignatureRequest(el, msg)
-	log.ErrFatal(err, "Couldn't send")
-
-	// verify the response still
-	assert.Nil(t, cosi.VerifySignature(hosts[0].Suite(), el.Publics(),
-		msg, res.Signature))
-}
-
-func TestServiceCosi2(t *testing.T) {
-	defer log.AfterTest(t)
-	log.TestOutput(testing.Verbose(), 4)
-	local := onet.NewTCPTest()
-	// generate 5 hosts, they don't connect, they process messages, and they
-	// don't register the tree or entitylist
-	hosts, el, _ := local.GenTree(5, false)
-	defer local.CloseAll()
-
-	// Send a request to the service to the second host
+	// Send a request to the service to all hosts
 	client := NewTestClient(local)
 	msg := []byte("hello cosi service")
 	serviceReq := &SignatureRequest{
 		Roster:  el,
 		Message: msg,
 	}
-	dst := el.List[1]
-	reply := &SignatureResponse{}
-	log.Lvl1("Sending request to service...")
-	cerr := client.SendProtobuf(dst, serviceReq, reply)
-	log.ErrFatal(cerr, "Couldn't send")
+	for _, dst := range el.List {
+		reply := &SignatureResponse{}
+		log.Lvl1("Sending request to service...")
+		cerr := client.SendProtobuf(dst, serviceReq, reply)
+		log.ErrFatal(cerr, "Couldn't send")
 
-	// verify the response still
-	assert.Nil(t, cosi.VerifySignature(hosts[0].Suite(), el.Publics(),
-		msg, reply.Signature))
+		// verify the response still
+		assert.Nil(t, cosi.VerifySignature(hosts[0].Suite(), el.Publics(),
+			msg, reply.Signature))
+	}
 }
 
 func TestCreateAggregate(t *testing.T) {
