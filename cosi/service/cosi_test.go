f36f56ee359cf4433337ec80913f8ba0e91b9db4
christophe <christophe@bertrand-feilen.ch>
2017-03-31 13:29:52 +0200

    cosi doesn't hang anymore 839

--- cosi/service/cosi_test.go
+++ cosi/service/cosi_test.go
@@ -34,6 +34,33 @@ func TestServiceCosi(t *testing.T) {
 		msg, res.Signature))
 }
 
+func TestServiceCosi2(t *testing.T) {
+	defer log.AfterTest(t)
+	log.TestOutput(testing.Verbose(), 4)
+	local := onet.NewTCPTest()
+	// generate 5 hosts, they don't connect, they process messages, and they
+	// don't register the tree or entitylist
+	hosts, el, _ := local.GenTree(5, false)
+	defer local.CloseAll()
+
+	// Send a request to the service to the second host
+	client := NewTestClient(local)
+	msg := []byte("hello cosi service")
+	serviceReq := &SignatureRequest{
+		Roster:  el,
+		Message: msg,
+	}
+	dst := el.List[1]
+	reply := &SignatureResponse{}
+	log.Lvl1("Sending request to service...")
+	cerr := client.SendProtobuf(dst, serviceReq, reply)
+	log.ErrFatal(cerr, "Couldn't send")
+
+	// verify the response still
+	assert.Nil(t, cosi.VerifySignature(hosts[0].Suite(), el.Publics(),
+		msg, reply.Signature))
+}
+
 func TestCreateAggregate(t *testing.T) {
 	defer log.AfterTest(t)
 	log.TestOutput(testing.Verbose(), 4)
