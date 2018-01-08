6a49f2f7dbb80a0cd87030553838339149539427
Jeff R. Allen <jeff.allen@epfl.ch>
2018-01-08 12:03:11 +0100

    Fix failing test case due to number of open files.

--- cosi/simulation/cosi_test.go
+++ cosi/simulation/cosi_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"syscall"
 	"testing"
 	"time"
 
@@ -13,6 +14,23 @@ import (
 var tSuite = suites.MustFind("Ed25519")
 
 func TestMain(m *testing.M) {
+	// Raising the FD limit like this might belong in a more central place,
+	// but for the moment, this is the only test where it is biting us.
+	// (and even then, only for Jeff's Mac: why?)
+	var rLimit syscall.Rlimit
+	err := syscall.Getrlimit(syscall.RLIMIT_NOFILE, &rLimit)
+	if err != nil {
+		log.Fatal("Error Getting Rlimit ", err)
+	}
+
+	if rLimit.Cur < 2048 {
+		rLimit.Cur = 2048
+		err = syscall.Setrlimit(syscall.RLIMIT_NOFILE, &rLimit)
+		if err != nil {
+			log.Fatal("Error Setting Rlimit ", err)
+		}
+	}
+
 	log.MainTest(m)
 }
 
