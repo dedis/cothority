5e033db736690d318b4c219bebfe6cc4a3799e10
Jeff R. Allen <jeff.allen@epfl.ch>
2018-01-09 16:30:47 +0100

    Make it build with Windows too.

--- cosi/simulation/cosi_test.go
+++ cosi/simulation/cosi_test.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"syscall"
 	"testing"
 	"time"
 
@@ -14,23 +13,7 @@ import (
 var tSuite = suites.MustFind("Ed25519")
 
 func TestMain(m *testing.M) {
-	// Raising the FD limit like this might belong in a more central place,
-	// but for the moment, this is the only test where it is biting us.
-	// (and even then, only for Jeff's Mac: why?)
-	var rLimit syscall.Rlimit
-	err := syscall.Getrlimit(syscall.RLIMIT_NOFILE, &rLimit)
-	if err != nil {
-		log.Fatal("Error Getting Rlimit ", err)
-	}
-
-	if rLimit.Cur < 2048 {
-		rLimit.Cur = 2048
-		err = syscall.Setrlimit(syscall.RLIMIT_NOFILE, &rLimit)
-		if err != nil {
-			log.Fatal("Error Setting Rlimit ", err)
-		}
-	}
-
+	raiseLimit()
 	log.MainTest(m)
 }
 
