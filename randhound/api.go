b86b192fb7c5260a805f9c3bc1655da8c6a30cf0
Linus Gasser <linus.gasser@epfl.ch>
2018-01-22 16:30:35 +0100

    imported randhound_659_2

---
+++ randhound/api.go
@@ -0,0 +1,43 @@
+package randhound
+
+import "gopkg.in/dedis/onet.v1"
+
+// Client is a Pulsar[RandHound] client that can communicate with the corresponding service.
+type Client struct {
+	*onet.Client
+}
+
+// NewClient constructor of Pulsar[RandHound] clients.
+func NewClient() *Client {
+	return &Client{Client: onet.NewClient("RandHound")}
+}
+
+// Setup sends a message to a node of the given roster (currently the one at
+// index 0) to request the setup of a Pulsar[RandHound] service.
+func (c *Client) Setup(roster *onet.Roster, groups int, purpose string,
+	interval int) (*SetupReply, onet.ClientError) {
+	dst := roster.List[0]
+	request := &SetupRequest{
+		Roster:   roster,
+		Groups:   groups,
+		Purpose:  purpose,
+		Interval: interval,
+	}
+	reply := &SetupReply{}
+	if err := c.SendProtobuf(dst, request, reply); err != nil {
+		return nil, err
+	}
+	return reply, nil
+}
+
+// Random allows to request collective randomness from a running
+// Pulsar[RandHound] service.
+func (c *Client) Random(roster *onet.Roster) (*RandReply, onet.ClientError) {
+	dst := roster.List[0]
+	request := &RandRequest{}
+	reply := &RandReply{}
+	if err := c.SendProtobuf(dst, request, reply); err != nil {
+		return nil, err
+	}
+	return reply, nil
+}
---
+++ randhound/protocol/randhound_test.go
@@ -0,0 +1,57 @@
+package protocol
+
+import (
+	"testing"
+	"time"
+
+	"gopkg.in/dedis/onet.v1"
+	"gopkg.in/dedis/onet.v1/log"
+)
+
+func TestRandHound(test *testing.T) {
+
+	name := "RandHound"
+	nodes := 11
+	groups := 2
+	purpose := "RandHound test run"
+
+	local := onet.NewLocalTest()
+	_, _, tree := local.GenTree(int(nodes), true)
+	defer local.CloseAll()
+
+	log.Lvlf1("RandHound - starting")
+	protocol, err := local.CreateProtocol(name, tree)
+	if err != nil {
+		test.Fatal("RandHound - couldn't initialise protocol:", err)
+	}
+	rh := protocol.(*RandHound)
+	if err := rh.Setup(nodes, groups, purpose); err != nil {
+		test.Fatal("RandHound - couldn't initialise protocol:", err)
+	}
+	if err := rh.Start(); err != nil {
+		test.Fatal(err)
+	}
+
+	select {
+	case <-rh.Done:
+		log.Lvlf1("RandHound - done")
+
+		random, transcript, err := rh.Random()
+		if err != nil {
+			test.Fatal(err)
+		}
+		log.Lvlf1("RandHound - collective randomness: ok")
+
+		//log.Lvlf1("RandHound - collective randomness: %v", random)
+		//_ = transcript
+
+		err = Verify(rh.Suite(), random, transcript)
+		if err != nil {
+			test.Fatal(err)
+		}
+		log.Lvlf1("RandHound - verification: ok")
+
+	case <-time.After(time.Second * time.Duration(nodes) * 2):
+		test.Fatal("RandHound – time out")
+	}
+}
