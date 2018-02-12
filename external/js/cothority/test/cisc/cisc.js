f7934ef3cf97340764d4665895a2f0a990b6d094
Ludovic Barman <lbarman@protonmail.ch>
2018-02-12 17:17:39 +0100

    Fixes JS CISC test, now contacts the go backend but gets no data

--- external/js/cothority/test/cisc/cisc.js
+++ external/js/cothority/test/cisc/cisc.js
@@ -6,38 +6,39 @@ const helpers = require("../helpers.js");
 const child_process = require("child_process");
 const fs = require("fs");
 
-const expect = chai.expect;
+const curve = new kyber.curve.edwards25519.Curve();
 const proto = cothority.protobuf;
 const skipchain = cothority.skipchain;
 const misc = cothority.misc;
 const net = cothority.net;
-const curve = new kyber.curve.edwards25519.Curve();
-
+const expect = chai.expect;
 
 describe.only("skipchain client", () => {
 
   it("can retrieve updates from conodes", done => {
 
     after(function(){
+      console.log("Cleanup...")
       killGolang();
     })
-
     runGolang()
       .then(data => {
         [roster, id] = data;
-        
-        //const socket = new net.RosterSocket(roster, "Skipchain");
-        var socket = new net.Socket(roster._identities[0].addr, "SkipChain");
 
+        const addr1 = roster.identities[0].websocketAddr;
+        const socket = new net.Socket(addr1, "Skipchain");
         const requestStr = "GetUpdateChain";
         const responseStr = "GetUpdateChainReply";
         const request = { latestId: misc.hexToUint8Array(id) };
-
         return socket.send(requestStr, responseStr, request);
       })
-      .then(data => {
-        console.log("Received data from Server");
-        console.log(data);
+      .then(skipblocks => {
+        console.log("Received data from the skipchain:", skipblocks);
+
+        for(var i=0; i<skipblocks.update.length; i++){
+          var skipblock = skipblocks.update[i];
+          console.log(skipblock.data);
+        }
         done();
       })
   });
@@ -45,12 +46,21 @@ describe.only("skipchain client", () => {
 
 var spawned_conodes;
 
-function runGolang() {  
-  const build_dir = process.cwd() + "/test/cisc/build";
-  const spawn = child_process.spawn;
+function killGolang() {
+  console.log("Killing all conodes...");
 
-  // start the process, and returns a promise
+  spawned_conodes.kill();
+  spawned_conodes.stdin.destroy();
+  spawned_conodes.stdout.destroy();
+  spawned_conodes.stderr.destroy();
+  child_process.execSync("pkill go");
+}
+
+function runGolang() {
+  const build_dir = process.cwd() + "/test/skipchain/build";
+  const spawn = child_process.spawn;
   return new Promise(function(resolve, reject) {
+
     console.log("build_dir = " + build_dir);
     spawned_conodes = spawn("go", ["run", "main.go"], {
       cwd: build_dir,
@@ -58,22 +68,23 @@ function runGolang() {
       detached: true
     });
     spawned_conodes.unref();
+
     console.log("Spawned Conode PID: " + spawned_conodes.pid);
     spawned_conodes.on("error", err => {
-      reject("Error on Conode: " + err);
+      reject("Conode Error: " + err);
     });
+
     spawned_conodes.stdout.setEncoding("utf8");
     spawned_conodes.stdout.on("data", data => {
       if (!data.match(/OK/)) {
-        reject("Data from Conode (NOT OK) => " + data);
+        reject("RECEIVED DATA (NOT OK) => " + data);
       }
-      console.log("Data from Conode (OK) => " + data);
+      console.log("RECEIVED DATA (OK) => " + data);
       resolve(data);
     });
-    spawned_conodes.on("exit", (code, signal) => {
-      console.log("Conode exited.");
-    });
+    
   }).then(data => {
+
     // read roster and genesis
     const group_file = build_dir + "/public.toml";
     const genesis_file = build_dir + "/genesis.txt";
@@ -81,16 +92,8 @@ function runGolang() {
     const groupToml = fs.readFileSync(group_file, "utf8");
     const genesisID = fs.readFileSync(genesis_file, "utf8");
     console.log("groupToml:  " + groupToml);
-
     const roster = cothority.Roster.fromTOML(groupToml);
-    return [roster, genesisID];
-  });
-}
 
-function killGolang() {
-  console.log("Killing spawned conodes");
-  spawned_conodes.stdout.destroy();
-  spawned_conodes.stderr.destroy();
-  spawned_conodes.kill();
-  child_process.execSync("pkill go");
+    return Promise.resolve([roster, genesisID]);
+  });
 }
