3e785d49ab2a3632eef352c787dc85cfd65f2dec
Ludovic Barman <lbarman@protonmail.ch>
2018-02-12 16:16:45 +0100

    WIP Test for CISC, cannot contact spawned server

--- external/js/cothority/test/cisc/cisc.js
+++ external/js/cothority/test/cisc/cisc.js
@@ -6,97 +6,91 @@ const helpers = require("../helpers.js");
 const child_process = require("child_process");
 const fs = require("fs");
 
-const curve = new kyber.curve.edwards25519.Curve();
+const expect = chai.expect;
 const proto = cothority.protobuf;
 const skipchain = cothority.skipchain;
 const misc = cothority.misc;
 const net = cothority.net;
-const expect = chai.expect;
+const curve = new kyber.curve.edwards25519.Curve();
 
-const build_dir = process.cwd() + "/test/cisc/build";
-describe.only("cisc client", () => {
+
+describe.only("skipchain client", () => {
+
   it("can retrieve updates from conodes", done => {
-    var proc;
-    after(function() {
-      helpers.killGolang(proc);
-    });
-    helpers
-      .runGolang(build_dir, data => data.match(/OK/))
-      .then(proces => {
-        proc = proces;
-        [roster, id] = helpers.readSkipchainInfo(build_dir);
 
-        const addr1 = roster.identities[0].websocketAddr;
-        const socket = new net.Socket(addr1, "Identity");
-        const requestStr = "DataUpdate";
-        const responseStr = "DataUpdateReply";
+    after(function(){
+      killGolang();
+    })
 
-        const request = { id: misc.hexToUint8Array(id) };
+    runGolang()
+      .then(data => {
+        [roster, id] = data;
+        
+        //const socket = new net.RosterSocket(roster, "Skipchain");
+        var socket = new net.Socket(roster._identities[0].addr, "SkipChain");
 
-        //console.log("Sending data", request);
+        const requestStr = "GetUpdateChain";
+        const responseStr = "GetUpdateChainReply";
+        const request = { latestId: misc.hexToUint8Array(id) };
+
         return socket.send(requestStr, responseStr, request);
       })
       .then(data => {
-        console.log("Received data from the identity skipchain:", data);
-        console.log("Storage: ", data.data.storage);
-        console.log("Keys: ", Object.keys(data.data.storage));
+        console.log("Received data from Server");
+        console.log(data);
         done();
-      });
-  }).timeout(5000);
+      })
+  });
 });
 
-//var spawned_conodes;
+var spawned_conodes;
 
-//function killGolang() {
-//console.log("Killing all conodes...");
+function runGolang() {  
+  const build_dir = process.cwd() + "/test/cisc/build";
+  const spawn = child_process.spawn;
 
-//spawned_conodes.kill();
-//spawned_conodes.stdin.destroy();
-//spawned_conodes.stdout.destroy();
-//spawned_conodes.stderr.destroy();
-//child_process.execSync("pkill go");
-//}
+  // start the process, and returns a promise
+  return new Promise(function(resolve, reject) {
+    console.log("build_dir = " + build_dir);
+    spawned_conodes = spawn("go", ["run", "main.go"], {
+      cwd: build_dir,
+      env: process.env,
+      detached: true
+    });
+    spawned_conodes.unref();
+    console.log("Spawned Conode PID: " + spawned_conodes.pid);
+    spawned_conodes.on("error", err => {
+      reject("Error on Conode: " + err);
+    });
+    spawned_conodes.stdout.setEncoding("utf8");
+    spawned_conodes.stdout.on("data", data => {
+      if (!data.match(/OK/)) {
+        reject("Data from Conode (NOT OK) => " + data);
+      }
+      console.log("Data from Conode (OK) => " + data);
+      resolve(data);
+    });
+    spawned_conodes.on("exit", (code, signal) => {
+      console.log("Conode exited.");
+    });
+  }).then(data => {
+    // read roster and genesis
+    const group_file = build_dir + "/public.toml";
+    const genesis_file = build_dir + "/genesis.txt";
 
-//function runGolang() {
-//const spawn = child_process.spawn;
-//return new Promise(function(resolve, reject) {
-//console.log("build_dir = " + build_dir);
-//env = process.env;
-////env["DEBUG_LVL"] = "1";
-////env["DEBUG_COLOR"] = "true";
-//spawned_conodes = spawn("go", ["run", "main.go"], {
-//cwd: build_dir,
-//env: env,
-//detached: true
-//});
-//spawned_conodes.unref();
+    const groupToml = fs.readFileSync(group_file, "utf8");
+    const genesisID = fs.readFileSync(genesis_file, "utf8");
+    console.log("groupToml:  " + groupToml);
 
-//console.log("Spawned Conode PID: " + spawned_conodes.pid);
-//spawned_conodes.on("error", err => {
-//reject("Conode Error: " + err);
-//});
+    const roster = cothority.Roster.fromTOML(groupToml);
+    return [roster, genesisID];
+  });
+}
 
-//spawned_conodes.stdout.setEncoding("utf8");
-//spawned_conodes.stdout.on("data", data => {
-//console.log(data);
-//if (!data.match(/OK/)) {
-////reject("RECEIVED DATA (NOT OK) => " + data);
-//return;
-//}
-//console.log("RECEIVED OK");
-//resolve();
-//});
-//}).then(() => {
-//// read roster and genesis
-//const group_file = build_dir + "/public.toml";
-//const genesis_file = build_dir + "/genesis.txt";
-
-//const groupToml = fs.readFileSync(group_file, "utf8");
-//const genesisID = fs.readFileSync(genesis_file, "utf8");
-//console.log("groupToml:  " + groupToml);
-//console.log("genesisID: " + genesisID);
-//const roster = cothority.Roster.fromTOML(groupToml);
-
-//return Promise.resolve([roster, genesisID]);
-//});
-/*}*/
+function killGolang() {
+  console.log("Killing spawned conodes");
+  spawned_conodes.stdout.destroy();
+  spawned_conodes.stderr.destroy();
+  spawned_conodes.kill();
+  child_process.execSync("pkill go");
+}
