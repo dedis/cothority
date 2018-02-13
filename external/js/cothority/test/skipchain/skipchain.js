5cd028672f34d05ff8ff203d009d7be8624070c8
nikkolasg <nicolas.gailly@epfl.ch>
2018-02-13 17:11:26 +0100

    better test for skipchain client

--- external/js/cothority/test/skipchain/skipchain.js
+++ external/js/cothority/test/skipchain/skipchain.js
@@ -14,6 +14,7 @@ const helpers = require("../helpers.js");
 
 const curve = new kyber.curve.edwards25519.Curve();
 const child_process = require("child_process");
+const co = require("co");
 
 describe.only("skipchain client", () => {
   after(function() {
@@ -22,19 +23,30 @@ describe.only("skipchain client", () => {
 
   it("can retrieve updates from conodes", done => {
     const build_dir = process.cwd() + "/test/skipchain/build";
+    var last_block;
+    var roster, id;
+    var fn = co.wrap(function*() {
+      [roster, id] = helpers.readSkipchainInfo(build_dir);
+      const client = new skipchain.Client(curve, roster, id);
+      last_block = yield client.getLatestBlock();
+      //console.log(last_block);
+      // try to read it from a roster socket
+      //  and compare if we have the same results
+      const socket = new net.RosterSocket(roster, "Skipchain");
+      const requestStr = "GetUpdateChain";
+      const responseStr = "GetUpdateChainReply";
+      const request = { latestId: misc.hexToUint8Array(id) };
+      allBlocks = yield socket.send(requestStr, responseStr, request);
+      var length = allBlocks.update.length;
+      lastReceived = allBlocks.update[length - 1];
+      expect(lastReceived).to.be.deep.equal(last_block);
+      done();
+    });
     helpers
       .runGolang(build_dir)
-      .then(data => {
-        [roster, id] = helpers.readSkipchainInfo(build_dir);
-        const client = new skipchain.Client(curve, roster, id);
-        return client.getLatestBlock();
-      })
-      .then(data => {
-        console.log(data);
-        expect(true).to.be.true;
-        done();
-      })
+      .then(fn)
       .catch(err => {
+        done();
         throw err;
       });
   });
