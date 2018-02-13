98e91dd785478cf753f2a5abaa123d69b287ade7
Ludovic Barman <lbarman@protonmail.ch>
2018-02-13 11:08:56 +0100

    WIP go script to run CISC - not working

--- external/js/cothority/test/cisc/build/main.go
+++ external/js/cothority/test/cisc/build/main.go
@@ -1,16 +1,8 @@
 package main
 
 import (
-	"encoding/hex"
-	"fmt"
-	"os"
-	"time"
-
 	"github.com/dedis/cothority"
 	"github.com/dedis/cothority/identity"
-	"github.com/dedis/kyber"
-	"github.com/dedis/kyber/sign/anon"
-	"github.com/dedis/kyber/util/key"
 	"github.com/dedis/onet"
 	"github.com/dedis/onet/app"
 	"github.com/dedis/onet/log"
@@ -19,16 +11,18 @@ import (
 func main() {
 	n := 5
 	l := onet.NewTCPTest(cothority.Suite)
-	hosts, el, _ := l.GenTree(n, true)
-	services := l.GetServices(hosts, identity.IdentityService())
+	_, ro, _ := l.GenTree(n, true)
 	defer l.CloseAll()
-	for _, s := range services {
-		log.Lvl3(s.(*identity.Service).Storage.Identities)
-	}
-	c1 := createIdentity(l, services, el, "one1")
 
+	// creates the clients
+	clients := make(map[int]*identity.Identity)
+	for i := range [8]byte{} {
+		clients[i] = newTestIdentity(l, ro)
+	}
+
+	// saves the roster (of clients) into public.toml
 	serversToml := make([]*app.ServerToml, n)
-	for i, si := range el.List {
+	for i, si := range ro.List {
 		serversToml[i] = app.NewServerToml(
 			cothority.Suite,
 			si.Public,
@@ -39,42 +33,36 @@ func main() {
 	group := app.NewGroupToml(serversToml...)
 	log.ErrFatal(group.Save("public.toml"))
 
-	log.Lvl3("Before data add", c1.Data.Storage)
-	data2 := c1.Data.Copy()
-	data2.Storage["two2"] = "public2"
-	log.ErrFatal(c1.ProposeSend(data2))
-	log.ErrFatal(c1.ProposeUpdate())
-	log.ErrFatal(c1.ProposeVote(true))
-	log.ErrFatal(c1.DataUpdate())
+	c0 := clients[0] // = newTestIdentity(localTCPTest, roster)
+	log.Lvlf1("Initial data %+v, %+v\n", c0.Data.Storage, c0.Data.Votes) // map[], map[]
 
-	log.Lvl3("After data add", c1.Data.Storage)
+	newData := c0.Data.Copy()
+	newData.Storage["ninja"] = "test"
+	log.ErrFatal(c0.ProposeSend(newData))
+	log.ErrFatal(c0.ProposeUpdate())
+	log.ErrFatal(c0.ProposeVote(true))
 
-	id := hex.EncodeToString(c1.ID)
-	fd, err := os.Create("genesis.txt")
-	log.ErrFatal(err)
-	fd.WriteString(id)
-	fd.Close()
-	fmt.Println("OK")
-	time.Sleep(3600 * time.Second)
+	log.Lvlf1("After propose %+v, %+v\n", c0.Data.Storage, c0.Data.Votes) // map[], map[]
+
+	// creates a CISC
+	//_, inter, err := clients[0].CreateRootControl(ro, ro, nil, 1, 1, 1)
+	//log.ErrFatal(err)
+	//
+	//_, err = clients[2%8].GetUpdateChain(inter.Roster, inter.Hash)
+	//log.ErrFatal(err)
+	//
+	//id := hex.EncodeToString(inter.Hash)
+	//fd, err := os.Create("genesis.txt")
+	//log.ErrFatal(err)
+	//fd.WriteString(id)
+	//fd.Close()
+	//fmt.Println("OK")
+	//time.Sleep(3600 * time.Second)
+
 }
 
-func createIdentity(l *onet.LocalTest, services []onet.Service, el *onet.Roster, name string) *identity.Identity {
-	kp1 := key.NewKeyPair(cothority.Suite)
-	kp2 := key.NewKeyPair(cothority.Suite)
-	set := anon.Set([]kyber.Point{kp1.Public, kp2.Public})
-	for _, srvc := range services {
-		s := srvc.(*identity.Service)
-		s.Storage.Auth.AddToSet(set)
-	}
-
-	c := NewTestIdentity(el, 50, name, l, kp1)
-	log.ErrFatal(c.CreateIdentity(identity.PoPAuth, set, kp1.Private))
-	return c
-}
-
-// NewTestIdentity returns a identity.Identity client
-func NewTestIdentity(cothority *onet.Roster, majority int, owner string, local *onet.LocalTest, kp *key.Pair) *identity.Identity {
-	id := identity.NewIdentity(cothority, majority, owner, kp)
-	id.Client = local.NewClient(identity.ServiceName)
+func newTestIdentity(l *onet.LocalTest, ro *onet.Roster) *identity.Identity {
+	id := identity.NewIdentity(ro, 1, "owner", nil)
+	id.Client = l.NewClient("Cisc")
 	return id
 }
