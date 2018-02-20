82ec38ce7900bc0b2a75310d94c5d9c86d385624
nikkolasg <nicolas.gailly@epfl.ch>
2018-02-20 15:30:30 +0100

    working but unclean version

--- external/js/cothority/lib/net/net.js
+++ external/js/cothority/lib/net/net.js
@@ -65,7 +65,9 @@ function Socket(addr, service) {
       };
 
       ws.onerror = error => {
-        reject(new Error("could not connect to " + this.url + ":" + error));
+        //reject(new Error("could not connect to " + this.url + ":", error));
+        console.log(error);
+        reject(error);
       };
     });
   };
