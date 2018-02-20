582ece48001875b6da7f548a1a201a032d004d76
nikkolasg <nicolas.gailly@epfl.ch>
2018-02-20 17:39:19 +0100

    helpers now returns process

--- external/js/cothority/test/cisc/cisc.js
+++ external/js/cothority/test/cisc/cisc.js
@@ -13,14 +13,18 @@ const misc = cothority.misc;
 const net = cothority.net;
 const expect = chai.expect;
 
+const build_dir = process.cwd() + "/test/cisc/build";
 describe.only("cisc client", () => {
   it("can retrieve updates from conodes", done => {
+    var proc;
     after(function() {
-      killGolang();
+      helpers.killGolang(proc);
     });
-    runGolang()
-      .then(data => {
-        [roster, id] = data;
+    helpers
+      .runGolang(build_dir, data => data.match(/OK/))
+      .then(proces => {
+        proc = proces;
+        [roster, id] = helpers.readSkipchainInfo(build_dir);
 
         const addr1 = roster.identities[0].websocketAddr;
         const socket = new net.Socket(addr1, "Identity");
@@ -41,59 +45,58 @@ describe.only("cisc client", () => {
   }).timeout(5000);
 });
 
-var spawned_conodes;
+//var spawned_conodes;
 
-function killGolang() {
-  console.log("Killing all conodes...");
+//function killGolang() {
+//console.log("Killing all conodes...");
 
-  spawned_conodes.kill();
-  spawned_conodes.stdin.destroy();
-  spawned_conodes.stdout.destroy();
-  spawned_conodes.stderr.destroy();
-  child_process.execSync("pkill go");
-}
+//spawned_conodes.kill();
+//spawned_conodes.stdin.destroy();
+//spawned_conodes.stdout.destroy();
+//spawned_conodes.stderr.destroy();
+//child_process.execSync("pkill go");
+//}
 
-function runGolang() {
-  const build_dir = process.cwd() + "/test/cisc/build";
-  const spawn = child_process.spawn;
-  return new Promise(function(resolve, reject) {
-    console.log("build_dir = " + build_dir);
-    env = process.env;
-    //env["DEBUG_LVL"] = "1";
-    //env["DEBUG_COLOR"] = "true";
-    spawned_conodes = spawn("go", ["run", "main.go"], {
-      cwd: build_dir,
-      env: env,
-      detached: true
-    });
-    spawned_conodes.unref();
+//function runGolang() {
+//const spawn = child_process.spawn;
+//return new Promise(function(resolve, reject) {
+//console.log("build_dir = " + build_dir);
+//env = process.env;
+////env["DEBUG_LVL"] = "1";
+////env["DEBUG_COLOR"] = "true";
+//spawned_conodes = spawn("go", ["run", "main.go"], {
+//cwd: build_dir,
+//env: env,
+//detached: true
+//});
+//spawned_conodes.unref();
 
-    console.log("Spawned Conode PID: " + spawned_conodes.pid);
-    spawned_conodes.on("error", err => {
-      reject("Conode Error: " + err);
-    });
+//console.log("Spawned Conode PID: " + spawned_conodes.pid);
+//spawned_conodes.on("error", err => {
+//reject("Conode Error: " + err);
+//});
 
-    spawned_conodes.stdout.setEncoding("utf8");
-    spawned_conodes.stdout.on("data", data => {
-      console.log(data);
-      if (!data.match(/OK/)) {
-        //reject("RECEIVED DATA (NOT OK) => " + data);
-        return;
-      }
-      console.log("RECEIVED OK");
-      resolve();
-    });
-  }).then(() => {
-    // read roster and genesis
-    const group_file = build_dir + "/public.toml";
-    const genesis_file = build_dir + "/genesis.txt";
+//spawned_conodes.stdout.setEncoding("utf8");
+//spawned_conodes.stdout.on("data", data => {
+//console.log(data);
+//if (!data.match(/OK/)) {
+////reject("RECEIVED DATA (NOT OK) => " + data);
+//return;
+//}
+//console.log("RECEIVED OK");
+//resolve();
+//});
+//}).then(() => {
+//// read roster and genesis
+//const group_file = build_dir + "/public.toml";
+//const genesis_file = build_dir + "/genesis.txt";
 
-    const groupToml = fs.readFileSync(group_file, "utf8");
-    const genesisID = fs.readFileSync(genesis_file, "utf8");
-    console.log("groupToml:  " + groupToml);
-    console.log("genesisID: " + genesisID);
-    const roster = cothority.Roster.fromTOML(groupToml);
+//const groupToml = fs.readFileSync(group_file, "utf8");
+//const genesisID = fs.readFileSync(genesis_file, "utf8");
+//console.log("groupToml:  " + groupToml);
+//console.log("genesisID: " + genesisID);
+//const roster = cothority.Roster.fromTOML(groupToml);
 
-    return Promise.resolve([roster, genesisID]);
-  });
-}
+//return Promise.resolve([roster, genesisID]);
+//});
+/*}*/
