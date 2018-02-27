bccb296759c4fd0be8f147aae94184f1300e1639
Gaurav Narula <gnarula94@gmail.com>
2018-02-27 20:13:26 +0100

    display vote count

--- external/js/evoting/frontend/src/components/ElectionResult.vue
+++ external/js/evoting/frontend/src/components/ElectionResult.vue
@@ -16,26 +16,19 @@
                 <p><v-icon>comment</v-icon>{{ election.description }}</p>
               </v-flex>
             </v-layout>
-            <v-layout row wrap>
+            <v-layout
+              v-for="(val, key) in counts"
+              :key="key"
+              row
+              wrap>
               <v-flex xs5 md3>
-                <p class="candidate">Foobar</p>
+                <p class="candidate">{{ key }}</p>
               </v-flex>
               <v-flex xs5 md7>
-                <v-progress-linear color="success" value="90"></v-progress-linear>
+                <v-progress-linear color="success" :value="percentage(val, totalCount)"></v-progress-linear>
               </v-flex>
               <v-flex xs2 md2 class="text-md-center">
-                <span class="count">(180/200)</span>
-              </v-flex>
-            </v-layout>
-            <v-layout>
-              <v-flex xs5 md3>
-                <p class="candidate">Foobar</p>
-              </v-flex>
-              <v-flex xs5 md7>
-                <v-progress-linear value="10"></v-progress-linear>
-              </v-flex>
-              <v-flex xs2 md2 class="text-md-center">
-                <span class="count">(20/200)</span>
+                <span class="count">({{ val }}, {{ totalCount }})</span>
               </v-flex>
             </v-layout>
           </v-container>
@@ -46,9 +39,9 @@
 </template>
 
 <script>
-// import kyber from '@dedis/kyber-js'
+import kyber from '@dedis/kyber-js'
 
-// const curve = new kyber.curve.edwards25519.Curve()
+const curve = new kyber.curve.edwards25519.Curve()
 export default {
   computed: {
     election () {
@@ -57,18 +50,48 @@ export default {
       })
     }
   },
+  methods: {
+    candidates (data) {
+      const arr = []
+      for (let i = 0; i < data.length; i += 3) {
+        const num = data[i] + data[i + 1] * (1 << 8) + data[i + 2] * (1 << 16)
+        arr.push(num)
+      }
+      return arr
+    },
+    percentage (num, den) {
+      return num / den * 100
+    }
+  },
   data () {
+    const counts = {}
+    const c = this.candidates(this.election.data)
+    for (let i = 0; i < c.length; i++) {
+      counts[c[i]] = 0
+    }
     return {
-
+      counts,
+      totalCount: 0
     }
   },
   created () {
     const { socket } = this.$store.state
+    console.log(this.election)
     socket.send('Reconstruct', 'ReconstructReply', {
       id: this.election.id,
       token: this.$store.state.loginReply.token
     })
       .then(data => {
+        const { points } = data
+        console.log(points)
+        for (let i = 0; i < points.length; i++) {
+          const point = curve.point()
+          point.unmarshalBinary(points[i])
+          const d = point.data()
+          const sciper = d[0] + d[1] * (1 << 8) + d[2] * (1 << 16)
+          this.counts[sciper] += 1
+          this.totalCount += 1
+        }
       })
       .catch(e => {
         console.error(e.message)
