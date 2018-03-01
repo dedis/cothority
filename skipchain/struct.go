f08a705f97ee41e3aab7de9fa3b508f72a95de12
Jeff R. Allen <jeff.allen@epfl.ch>
2018-03-01 16:34:48 +0100

    Fix typo.

--- skipchain/struct.go
+++ skipchain/struct.go
@@ -260,7 +260,7 @@ type SkipBlockFix struct {
 	// is nil if this is the Root-roster
 	ParentBlockID SkipBlockID
 	// GenesisID is the ID of the genesis-block. For the genesis-block, this
-	// is null. The SkipBlockID() methods returns the correct ID both for
+	// is null. The SkipBlockID() method returns the correct ID both for
 	// the genesis block and for later blocks.
 	GenesisID SkipBlockID
 	// Data is any data to be stored in that SkipBlock
