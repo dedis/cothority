4aa833c14c4107c0f2070d26fd0a632ac1d66dff
Gaurav Narula <gnarula94@gmail.com>
2018-03-07 15:59:00 +0100

    Fix vote count

--- external/js/evoting/frontend/src/components/ElectionResult.vue
+++ external/js/evoting/frontend/src/components/ElectionResult.vue
@@ -17,18 +17,18 @@
               </v-flex>
             </v-layout>
             <v-layout
-              v-for="(val, key) in counts"
-              :key="key"
+              v-for="(val, idx) in sortCounts(counts)"
+              :key="val.sciper"
               row
               wrap>
               <v-flex xs5 md3>
-                <p class="candidate">{{ candidateNames[key] }}</p>
+                <p class="candidate">{{ candidateNames[val.sciper] }}</p>
               </v-flex>
               <v-flex xs5 md7>
-                <v-progress-linear color="success" :value="percentage(val, totalCount)"></v-progress-linear>
+                <v-progress-linear :color="colors[idx % colors.length]" :value="percentage(val.count, totalCount)"></v-progress-linear>
               </v-flex>
               <v-flex xs2 md2 class="text-md-center">
-                <span class="count">({{ val }}/{{ totalCount }})</span>
+                <span class="count">({{ val.count }}/{{ totalCount }})</span>
               </v-flex>
             </v-layout>
           </v-container>
@@ -65,6 +65,13 @@ export default {
     },
     endDate (timestamp) {
       return timestampToString(timestamp, true)
+    },
+    sortCounts (counts) {
+      const arr = []
+      for (let sciper in counts) {
+        arr.push({ sciper, count: counts[sciper] })
+      }
+      return arr.sort((a, b) => b.count - a.count)
     }
   },
   data () {
@@ -72,7 +79,28 @@ export default {
       counts: {},
       totalCount: 0,
       creatorName: '',
-      candidateNames: {}
+      candidateNames: {},
+      colors: [
+        'green accent-4',
+        'green accent-3',
+        'green accent-2',
+        'yellow lighten-2',
+        'yellow lighten-1',
+        'yellow',
+        'yellow darken-1',
+        'yellow darken-2',
+        'yellow darken-3',
+        'amber',
+        'amber darken-1',
+        'amber darken-2',
+        'amber darken-3',
+        'amber darken-4',
+        'orange',
+        'orange darken-1',
+        'orange darken-2',
+        'orange darken-3',
+        'orange darken-4'
+      ]
     }
   },
   created () {
@@ -88,11 +116,11 @@ export default {
           this.$store.state.names[this.creator] = this.creatorName
         })
     }
-    const c = this.candidates(this.election.data)
+    const c = this.candidates(this.election.candidates)
     for (let i = 0; i < c.length; i++) {
       this.counts[c[i]] = 0
     }
-    const scipers = this.candidates(this.election.data)
+    const scipers = this.candidates(this.election.candidates)
     for (let i = 0; i < scipers.length; i++) {
       const sciper = scipers[i]
       this.candidateNames[sciper] = this.$store.state.names[sciper] || null
@@ -105,7 +133,6 @@ export default {
       })
         .then(response => {
           this.candidateNames = {...this.candidateNames, [sciper]: response.fullName}
-          console.log('Updated candidateNames', this.candidateNames)
           // cache
           this.$store.state.names[sciper] = this.candidateNames[sciper]
         })
@@ -117,14 +144,15 @@ export default {
     })
       .then(data => {
         const { points } = data
-        console.log(points)
         for (let i = 0; i < points.length; i++) {
           const point = curve.point()
           point.unmarshalBinary(points[i])
           const d = point.data()
-          const sciper = d[0] + d[1] * (1 << 8) + d[2] * (1 << 16)
-          this.counts[sciper] += 1
-          this.totalCount += 1
+          for (let j = 0; j < d.length; j += 3) {
+            const sciper = d[j] + d[j + 1] * (1 << 8) + d[j + 2] * (1 << 16)
+            this.counts[sciper] += 1
+          }
+          this.totalCount += d.length / 3
         }
       })
       .catch(e => {
@@ -133,7 +161,8 @@ export default {
   },
   watch: {
     candidateNames: {
-      deep: true
+      deep: true,
+      handler (val, oldVal) {}
     }
   }
 }
