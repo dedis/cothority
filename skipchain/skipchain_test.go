fc077ace398caac3d3b38cb62cf9627e72697378
Linus Gasser <linus.gasser@epfl.ch>
2018-05-03 08:26:57 +0200

    remove flaky tests for now

--- skipchain/skipchain_test.go
+++ skipchain/skipchain_test.go
@@ -192,6 +192,9 @@ func TestService_SetChildrenSkipBlock(t *testing.T) {
 }
 
 func TestService_MultiLevel(t *testing.T) {
+	if testing.Short() {
+		t.Skip("flaky test on travis - skip")
+	}
 	local := onet.NewLocalTest(cothority.Suite)
 	defer waitPropagationFinished(t, local)
 	defer local.CloseAll()
