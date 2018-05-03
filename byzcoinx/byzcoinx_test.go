7730c064384e559b06220e7c14f5b8619c4c57d4
Linus Gasser <linus.gasser@epfl.ch>
2018-05-03 11:16:29 +0200

    increased timeout and improved code-flow

--- byzcoinx/byzcoinx_test.go
+++ byzcoinx/byzcoinx_test.go
@@ -17,7 +17,7 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-var defaultTimeout = time.Second * 2
+var defaultTimeout = time.Second * 4
 var testSuite = cothority.Suite
 
 type Counter struct {
