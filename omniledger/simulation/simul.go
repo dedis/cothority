00e03528de3c77fed9ae9a8eb6d0a0ae7abc9787
Linus Gasser <linus.gasser@epfl.ch>
2018-06-25 08:05:24 +0200

    use omniledger

--- omniledger/simulation/simul.go
+++ omniledger/simulation/simul.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	// Service needs to be imported here to be instantiated.
-	_ "github.com/dedis/cothority_template/service"
+	_ "github.com/dedis/cothority/omniledger/service"
 	"github.com/dedis/onet/simul"
 )
 
