a64a87d5cb2882c7304da3e007dfad5ecc9a3ba8
Linus Gasser <linus.gasser@epfl.ch>
2018-07-02 13:37:51 +0200

    don't use tags for building docker image

--- omniledger/contracts/service.go
+++ omniledger/contracts/service.go
@@ -6,6 +6,10 @@ import (
 	"github.com/dedis/onet/log"
 )
 
+// This service is only used because we need to register our contracts to
+// the omniledger service. So we create this stub and add contracts to it
+// from the `contracts` directory.
+
 func init() {
 	_, err := onet.RegisterNewService("contracts", newService)
 	log.ErrFatal(err)
