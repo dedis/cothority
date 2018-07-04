48fce213181d849e97325453325ec7f1c211eefe
Pablo Lorenceau <pablo.lorenceau@epfl.ch>
2018-07-04 10:19:45 +0200

    registering ContractCoin for simulation

--- omniledger/contracts/coins/simul.go
+++ omniledger/contracts/coins/simul.go
@@ -3,6 +3,7 @@ package main
 import (
 	// Service needs to be imported here to be instantiated.
 	_ "github.com/dedis/cothority/omniledger/service"
+	_ "github.com/dedis/cothority/omniledger/contracts"
 	"github.com/dedis/onet/simul"
 )
 
