39f15f7ac2ffb954bf5f63070541984acebc1c63
Linus Gasser <linus.gasser@epfl.ch>
2018-09-05 09:57:39 +0200

    Implementing coin-types Coins can be of different types and we need to store that information.

---
+++ omniledger/contracts/proto.go
@@ -0,0 +1,31 @@
+package contracts
+
+import "github.com/dedis/onet/network"
+
+func init() {
+	network.RegisterMessages()
+}
+
+// PROTOSTART
+// type :skipchain.SkipBlockID:bytes
+// type :darc.ID:bytes
+// type :Arguments:[]Argument
+// type :Instructions:[]Instruction
+// type :ClientTransactions:[]ClientTransaction
+// type :InstanceID:bytes
+// package omnicontracts;
+//
+// option java_package = "ch.epfl.dedis.proto";
+// option java_outer_classname = "OmniLedgerContracts";
+
+// CoinInstance represents the data stored for a coin-account. It holds the
+// type of the coin and the actual balance, which is always positive.
+type CoinInstance struct {
+	// Type denotes what coin this is. Every coin can have a type, and only
+	// compatible coins can be directly transferred. For incompatible coins,
+	// you need an exchange (not yet implemented).
+	Type []byte
+	// Balance holds how many coins are stored in this account. It can only
+	// be positive.
+	Balance uint64
+}
