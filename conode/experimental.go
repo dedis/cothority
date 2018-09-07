d51609af63ff215ec118160dbb34f446b894798b
Linus Gasser <linus.gasser@epfl.ch>
2018-09-07 14:37:06 +0200

    updating stable/directories to reflect pop uses omniledger now

--- conode/experimental.go
+++ conode/experimental.go
@@ -8,6 +8,7 @@ stable branch.
 import (
 	_ "github.com/dedis/cothority/eventlog"
 	_ "github.com/dedis/cothority/evoting/service"
+	_ "github.com/dedis/cothority/identity"
 	_ "github.com/dedis/cothority/ocs/service"
 	_ "github.com/dedis/cothority/omniledger/contracts"
 	_ "github.com/dedis/cothority/omniledger/service"
