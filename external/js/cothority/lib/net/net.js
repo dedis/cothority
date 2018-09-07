75955b8ce43af6090208fd4f8746476c1351f6e2
Linus Gasser <linus.gasser@epfl.ch>
2018-09-07 10:28:00 +0200

    updating proto

--- external/js/cothority/lib/net/net.js
+++ external/js/cothority/lib/net/net.js
@@ -43,6 +43,7 @@ function Socket(addr, service) {
       if (requestModel === undefined){
         reject(new Error("Model " + request + " not found"));
       }
+      console.dir("request model is:", requestModel);
 
       const responseModel = this.protobuf.lookup(response);
       if (responseModel === undefined){
@@ -69,6 +70,7 @@ function Socket(addr, service) {
           reject(new Error(errMsg));
         }
         const message = requestModel.create(data);
+        console.dir("message to send is:", message);
         const marshal = requestModel.encode(message).finish();
         ws.send(marshal);
       };
