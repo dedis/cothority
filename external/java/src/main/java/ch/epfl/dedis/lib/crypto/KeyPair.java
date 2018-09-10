6a7028c0569186b65fbbcdfdb1801f777a19e2be
Jeff R. Allen <jeff.allen@epfl.ch>
2018-09-10 09:56:40 +0200

    Use SecureRandom when making key pairs
    Fixes #1471.

--- external/java/src/main/java/ch/epfl/dedis/lib/crypto/KeyPair.java
+++ external/java/src/main/java/ch/epfl/dedis/lib/crypto/KeyPair.java
@@ -1,6 +1,6 @@
 package ch.epfl.dedis.lib.crypto;
 
-import java.util.Random;
+import java.security.SecureRandom;
 
 public class KeyPair {
     public Scalar scalar;
@@ -8,7 +8,7 @@ public class KeyPair {
 
     public KeyPair() {
         byte[] seed = new byte[Ed25519.field.getb() / 8];
-        new Random().nextBytes(seed);
+        new SecureRandom().nextBytes(seed);
         scalar = new Ed25519Scalar(seed);
         point = Ed25519Point.base().mul(scalar);
     }
