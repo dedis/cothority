66e632efd31018c639116eafc3c0b103a1cdbc38
Gaylor Bosson <gaylor.bosson@epfl.ch>
2018-11-07 16:51:57 +0100

    Fix state trie already exists error
    This adds a check to prevent from creating a state trie for a
    genesis block if it has already been created.
    
    Fixes #1567

--- byzcoin/service.go
+++ byzcoin/service.go
@@ -520,8 +520,9 @@ func (s *Service) updateTrieCallback(sbID skipchain.SkipBlockID) error {
 			"programmer error if you see this message.")
 	}
 
-	// If we are the genesis block, create the trie.
-	if sb.Index == 0 {
+	// Create the trie for the genesis block if it has not been
+	// created yet
+	if sb.Index == 0 && !s.hasStateTrie(sb.SkipChainID()) {
 		var body DataBody
 		err := protobuf.DecodeWithConstructors(sb.Payload, &body, network.DefaultConstructors(cothority.Suite))
 		if err != nil {
@@ -702,6 +703,13 @@ func isViewChangeTx(txs TxResults) *viewchange.View {
 // skipchain.
 func (s *Service) GetReadOnlyStateTrie(scID skipchain.SkipBlockID) (ReadOnlyStateTrie, error) {
 	return s.getStateTrie(scID)
+}
+
+func (s *Service) hasStateTrie(id skipchain.SkipBlockID) bool {
+	idStr := fmt.Sprintf("%x", id)
+	_, ok := s.stateTries[idStr]
+
+	return ok
 }
 
 func (s *Service) getStateTrie(id skipchain.SkipBlockID) (*stateTrie, error) {
