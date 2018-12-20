99a1fbfd727469755306b02a7acc814897d71b70
kc1212 <kc1212@users.noreply.github.com>
2018-12-20 15:28:16 +0100

    Add randomised test

--- external/java/src/test/java/ch/epfl/dedis/lib/crypto/BlsSigTest.java
+++ external/java/src/test/java/ch/epfl/dedis/lib/crypto/BlsSigTest.java
@@ -3,6 +3,7 @@ package ch.epfl.dedis.lib.crypto;
 import org.junit.jupiter.api.Test;
 
 import java.security.SecureRandom;
+import java.util.Arrays;
 import java.util.Random;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -28,5 +29,20 @@ class BlsSigTest {
         byte[] msg = "two legs good four legs better".getBytes();
         BlsSig goodSig = BlsSig.sign(pair.scalar, msg);
         assertTrue(goodSig.verify(msg, (Bn256G2Point) pair.point));
+    }
+
+    @Test
+    void random() {
+        for (int i = 0; i < 10; i++) {
+            byte[] msg = new byte[256];
+            rnd.nextBytes(msg);
+
+            Bn256KeyPair pair = new Bn256KeyPair(rnd);
+            BlsSig goodSig = BlsSig.sign(pair.scalar, msg);
+            assertTrue(goodSig.verify(msg, (Bn256G2Point) pair.point));
+
+            byte[] badMsg = Arrays.copyOfRange(msg, 0, 255);
+            assertFalse(goodSig.verify(badMsg, (Bn256G2Point) pair.point));
+        }
     }
 }
