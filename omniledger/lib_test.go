177b8a5cd963546a5a362762eb80a7eddab1783e
QM Nguyen <qm.nguyen@hotmail.co.uk>
2019-01-11 10:11:22 +0100

    Refactored ChangeRoster tests

--- omniledger/lib_test.go
+++ omniledger/lib_test.go
@@ -1,10 +1,12 @@
 package omniledger
 
 import (
+	"fmt"
 	"github.com/dedis/cothority"
 	"github.com/dedis/cothority/omniledger/lib"
 	"github.com/dedis/onet"
 	"github.com/stretchr/testify/assert"
+	"log"
 	"testing"
 	"time"
 )
@@ -42,13 +44,15 @@ func TestLib_ChangeRoster(t *testing.T) {
 
 	r1 = lib.ChangeRoster(r1, r2)
 	assert.True(t, len(r1.List) == i+3)
-	// r1 should be A,B,C,D,E,F
-	assert.True(t, r1.List[0].Equal(roster1.List[0]))
-	assert.True(t, r1.List[1].Equal(roster1.List[1]))
-	assert.True(t, r1.List[2].Equal(roster1.List[2]))
-	assert.True(t, r1.List[3].Equal(r2.List[0]))
-	assert.True(t, r1.List[4].Equal(r2.List[1]))
-	assert.True(t, r1.List[5].Equal(r2.List[2]))
+	// r1 should be D,E,F,A,B,C
+	assert.True(t, r1.List[0].Equal(r2.List[0]))
+	assert.True(t, r1.List[1].Equal(r2.List[1]))
+	assert.True(t, r1.List[2].Equal(r2.List[2]))
+	assert.True(t, r1.List[3].Equal(roster1.List[0]))
+	assert.True(t, r1.List[4].Equal(roster1.List[1]))
+	assert.True(t, r1.List[5].Equal(roster1.List[2]))
+	fmt.Println(roster1, r1)
+	log.Print(r1)
 
 	r1 = lib.ChangeRoster(r1, r2)
 	assert.True(t, len(r1.List) == i+j-1)
@@ -83,7 +87,7 @@ func TestLib_ChangeRoster2(t *testing.T) {
 
 	_, roster1, _ := l.GenTree(i, true)
 
-	// Suppose r1 contains nodes A,B,C and r2 contains nodes D,E,F
+	// Suppose r1 contains nodes A,B,C and r2 contains nodes C,B,A
 	r1 := *roster1
 	r2 := *roster1
 
@@ -92,6 +96,60 @@ func TestLib_ChangeRoster2(t *testing.T) {
 
 	r1 = lib.ChangeRoster(r1, r2)
 
+	// r1 should be C,B,A
+	for k := 0; k < len(r1.List); k++ {
+		assert.True(t, r1.List[k].Equal(r2.List[k]))
+	}
+}
+
+func TestLib_ChangeRoster3(t *testing.T) {
+	l := onet.NewLocalTest(cothority.Suite)
+	defer l.CloseAll()
+
+	i := 3
+	j := 3
+
+	_, roster1, _ := l.GenTree(i, true)
+	_, roster2, _ := l.GenTree(j, true)
+
+	// Suppose r1 contains nodes A,B,C and r2 contains nodes C,D,E
+	r1 := *roster1
+	r2 := *roster2
+
+	r2.List[0] = r1.List[2]
+
+	r1 = lib.ChangeRoster(r1, r2)
+	assert.True(t, len(r1.List) == i+1)
+	// r1 should be A,B,C,D
+	assert.True(t, r1.List[0].Equal(roster1.List[0]))
+	assert.True(t, r1.List[1].Equal(roster1.List[1]))
+	assert.True(t, r1.List[2].Equal(roster1.List[2]))
+	assert.True(t, r1.List[3].Equal(roster2.List[1]))
+
+	r1 = lib.ChangeRoster(r1, r2)
+	assert.True(t, len(r1.List) == i+2)
+	// r1 should be C,D,E,A,B
+	assert.True(t, r1.List[0].Equal(roster2.List[0]))
+	assert.True(t, r1.List[1].Equal(roster2.List[1]))
+	assert.True(t, r1.List[2].Equal(roster2.List[2]))
+	assert.True(t, r1.List[3].Equal(roster1.List[0]))
+	assert.True(t, r1.List[4].Equal(roster1.List[1]))
+	fmt.Println(roster1, r1)
+	log.Print(r1)
+
+	r1 = lib.ChangeRoster(r1, r2)
+	assert.True(t, len(r1.List) == i+j-2)
+	// r1 should be C,D,E,B
+	assert.True(t, r1.List[0].Equal(roster2.List[0]))
+	assert.True(t, r1.List[1].Equal(roster2.List[1]))
+	assert.True(t, r1.List[2].Equal(roster2.List[2]))
+	assert.True(t, r1.List[3].Equal(roster1.List[1]))
+	fmt.Println(roster1, r1)
+	log.Print(r1)
+
+	r1 = lib.ChangeRoster(r1, r2)
+	assert.True(t, len(r1.List) == i+j-3)
+	// r1 should be D,E,F <=> r1 should contains the same nodes as r2 and have the same order
 	for k := 0; k < len(r1.List); k++ {
 		assert.True(t, r1.List[k].Equal(r2.List[k]))
 	}
