a26d4202280018d09f5ee9529d54e0043f4529ed
kc1212 <kc1212@users.noreply.github.com>
2019-02-07 10:57:04 +0100

    Return true when comparing null extraData
    Fixes #1593

---
+++ external/java/src/test/java/ch/epfl/dedis/calypso/DocumentTest.java
@@ -0,0 +1,27 @@
+package ch.epfl.dedis.calypso;
+
+import ch.epfl.dedis.lib.Hex;
+import ch.epfl.dedis.lib.darc.DarcId;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DocumentTest {
+
+    @Test
+    void equals() {
+        DarcId id = new DarcId(Hex.parseHexBinary("aaaaaaaabbbbbbbbccccccccddddddddaaaaaaaabbbbbbbbccccccccdddddddd"));
+
+        Document doc1 = new Document("some data".getBytes(), "key material".getBytes(), "extra data".getBytes(), id);
+        Document doc2 = new Document("some data".getBytes(), "key material".getBytes(), "extra data".getBytes(), id);
+        assertEquals(doc1, doc2);
+
+        doc1 = new Document("some data".getBytes(), "key material".getBytes(), "extra data".getBytes(), id);
+        doc2 = new Document("some data".getBytes(), "key material".getBytes(), null, id);
+        assertNotEquals(doc1, doc2);
+
+        doc1 = new Document("some data".getBytes(), "key material".getBytes(), null, id);
+        doc2 = new Document("some data".getBytes(), "key material".getBytes(), null, id);
+        assertEquals(doc1, doc2);
+    }
+}
