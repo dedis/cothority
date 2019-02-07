e402281b40ed9eb0295fdc15c5d3d2c1cfd752e9
Gaylor Bosson <gaylor.bosson@epfl.ch>
2019-02-07 16:20:51 +0100

    KyberJS: remove generated files from git
    Remove the dist and doc folders because that will be easier to maintain
    and review.

--- external/js/kyber/dist/constants.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference types="node" />
-import BN from "bn.js";
-export declare type BNType = number | string | number[] | Buffer | BN;
-export declare const zeroBN: BN;
--- external/js/kyber/dist/sign/index.d.ts
+++
@@ -1,5 +0,0 @@
-import * as schnorr from "./schnorr/schnorr";
-import * as bls from "./bls";
-import * as anon from "./anon";
-import Mask from './mask';
-export { schnorr, bls, anon, Mask };
