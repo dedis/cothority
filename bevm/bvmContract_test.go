344024913bcda6d6fd831904b26adaa394a69e25
Christian Grigis <christian.grigis@epfl.ch>
2019-04-02 18:59:32 +0200

    Updated all tests with proper checks using view methods

--- bevm/bvmContract_test.go
+++ bevm/bvmContract_test.go
@@ -1,7 +1,6 @@
 package bevm
 
 import (
-	"fmt"
 	"math/big"
 	"testing"
 	"time"
@@ -29,7 +28,30 @@ type TransactionParameters struct {
 
 var txParams TransactionParameters = TransactionParameters{GasLimit: uint64(1e7), GasPrice: big.NewInt(1)}
 
-//Spawn a bvm
+var testAddresses = []string{
+	"0x627306090abab3a6e1400e9345bc60c78a8bef57",
+	"0xf17f52151ebef6c7334fad080c5704d77216b732",
+	"0xB8C7e1fAA6Cb23690fc068D6Be3d7Ad4dC16Ba78",
+	"0xf3250dbB0640b292e33d44De3D7E6C94E4D034C9",
+}
+var testPrivateKeys = []string{
+	"c87509a1c067bbde78beb793e6fa76530b6382a4c0241e5e4a9ec0a0f44dc0d3",
+	"ae6ae8e5ccbfb04590405997ee2d52d2b330726137b875053c36d94e974d162f",
+	"8503d4206b83002eee8ffe8a11c2b09885a0912f5cddd2401d96c3abccca7401",
+	"f78572bd69fbd3118ab756e3544d23821a2002b137c9037a3b8fd5b09169a73c",
+}
+
+// Helper function to handle the case of the "strange big.Int 0", as can be
+// returned sometimes with the Ethereum unpacking of a result, and causes
+// require.Equal() to return false:
+//
+// 		big.NewInt(0).Bits()	--> []big.Word(nil)
+// 		value.Bits()			--> []big.Word([])
+func assertBigInt0(t *testing.T, actual *big.Int) {
+	require.Equal(t, 0, big.NewInt(0).Cmp(actual))
+}
+
+// Spawn a bvm
 func Test_Spawn(t *testing.T) {
 	log.LLvl1("test: instantiating evm")
 
@@ -42,7 +64,7 @@ func Test_Spawn(t *testing.T) {
 	bct.createInstance(byzcoin.Arguments{})
 }
 
-//Credits and displays an account balance
+// Credit and display an account balance
 func Test_InvokeCredit(t *testing.T) {
 	log.LLvl1("test: crediting and displaying an account balance")
 
@@ -54,17 +76,23 @@ func Test_InvokeCredit(t *testing.T) {
 	// Spawn a new BEVM instance
 	instID := bct.createInstance(byzcoin.Arguments{})
 
-	address := common.HexToAddress("0x2afd357E96a3aCbcd01615681C1D7e3398d5fb61")
-	amount := uint64(3.1415926535 * WeiPerEther)
+	// Initialize an account
+	account, err := NewEvmAccount(testAddresses[0], testPrivateKeys[0])
+	require.Nil(t, err)
 
-	// Credit an account
-	bct.creditAccounts(instID, amount, address)
+	amount := big.NewInt(3.1415926535 * WeiPerEther)
 
-	// Display its balance
-	bct.displayAccounts(instID, address)
+	// Credit the account
+	bct.creditAccounts(instID, amount, account.Address)
+
+	// Retrieve its balance
+	balance, err := getAccountBalance(bct.cl, instID, account.Address)
+	require.Nil(t, err)
+
+	require.Equal(t, amount, balance)
 }
 
-//Credits and displays three accounts balances
+// Credit and display three accounts balances
 func Test_InvokeCreditAccounts(t *testing.T) {
 	log.LLvl1("test: crediting and checking accounts balances")
 
@@ -76,26 +104,32 @@ func Test_InvokeCreditAccounts(t *testing.T) {
 	// Spawn a new BEVM instance
 	instID := bct.createInstance(byzcoin.Arguments{})
 
-	addresses := [3]string{
-		"0x627306090abab3a6e1400e9345bc60c78a8bef57",
-		"0xf17f52151ebef6c7334fad080c5704d77216b732",
-		"0xc5fdf4076b8f3a5357c5e395ab970b5b54098fef",
+	// Initialize some accounts
+	accounts := []*EvmAccount{}
+	for i, _ := range testAddresses {
+		account, err := NewEvmAccount(testAddresses[i], testPrivateKeys[i])
+		require.Nil(t, err)
+		accounts = append(accounts, account)
 	}
 
-	for i, addrHex := range addresses {
-		address := common.HexToAddress(addrHex)
-		amount := uint64((i + 1) * WeiPerEther)
+	// Credit each account and check its balance
+	for i, account := range accounts {
+		amount := big.NewInt(int64((i + 1) * WeiPerEther))
 
-		bct.creditAccounts(instID, amount, address)
-		bct.displayAccounts(instID, address)
+		bct.creditAccounts(instID, amount, account.Address)
+
+		balance, err := getAccountBalance(bct.cl, instID, account.Address)
+		require.Nil(t, err)
+
+		require.Equal(t, amount, balance)
 	}
 }
 
-func (bct *bcTest) creditAccounts(instID byzcoin.InstanceID, amount uint64, addresses ...common.Address) {
+func (bct *bcTest) creditAccounts(instID byzcoin.InstanceID, amount *big.Int, addresses ...common.Address) {
 	for _, address := range addresses {
 		bct.invokeInstance(instID, "credit", byzcoin.Arguments{
 			{Name: "address", Value: address.Bytes()},
-			{Name: "amount", Value: new(big.Int).SetUint64(amount).Bytes()},
+			{Name: "amount", Value: amount.Bytes()},
 		})
 	}
 }
@@ -133,6 +167,8 @@ func (bct *bcTest) deploy(instID byzcoin.InstanceID, txParams TransactionParamet
 }
 
 func (bct *bcTest) transact(instID byzcoin.InstanceID, txParams TransactionParameters, value uint64, account *EvmAccount, contract EvmContract, method string, args ...interface{}) error {
+	log.LLvl1("Invoking Byzcoin for EVM method:", method)
+
 	callData, err := contract.packMethod(method, args...)
 	if err != nil {
 		return err
@@ -151,6 +187,52 @@ func (bct *bcTest) transact(instID byzcoin.InstanceID, txParams TransactionParam
 	return nil
 }
 
+func getEvmDb(client *byzcoin.Client, instID byzcoin.InstanceID) (*EvmDb, error) {
+	// Retrieve the proof of the Byzcoin instance
+	proofResponse, err := client.GetProof(instID[:])
+	if err != nil {
+		return nil, err
+	}
+
+	// Validate the proof
+	err = proofResponse.Proof.Verify(client.ID)
+	if err != nil {
+		return nil, err
+	}
+
+	// Extract the value from the proof
+	_, value, _, _, err := proofResponse.Proof.KeyValue()
+	if err != nil {
+		return nil, err
+	}
+
+	// Decode the proof value into an EVM State
+	var es ES
+	err = protobuf.Decode(value, &es)
+	if err != nil {
+		return nil, err
+	}
+	evmDb, err := NewEvmDb(&es)
+	if err != nil {
+		return nil, err
+	}
+
+	return evmDb, nil
+}
+
+func getAccountBalance(client *byzcoin.Client, instID byzcoin.InstanceID, address common.Address) (*big.Int, error) {
+	evmDb, err := getEvmDb(client, instID)
+	if err != nil {
+		return nil, err
+	}
+
+	balance := evmDb.stateDb.GetBalance(address)
+
+	log.Lvl1("balance of", address.Hex(), ":", balance, "wei")
+
+	return balance, nil
+}
+
 func (bct *bcTest) call(instID byzcoin.InstanceID, account *EvmAccount, result interface{}, contract EvmContract, method string, args ...interface{}) error {
 	// Pack the method call and arguments
 	callData, err := contract.packMethod(method, args...)
@@ -158,31 +240,8 @@ func (bct *bcTest) call(instID byzcoin.InstanceID, account *EvmAccount, result i
 		return err
 	}
 
-	// Retrieve the proof of the Byzcoin instance
-	proofResponse, err := bct.cl.GetProof(instID[:])
-	if err != nil {
-		return err
-	}
-
-	// Validate the proof
-	err = proofResponse.Proof.Verify(bct.cl.ID)
-	if err != nil {
-		return err
-	}
-
-	// Extract the value from the proof
-	_, value, _, _, err := proofResponse.Proof.KeyValue()
-	if err != nil {
-		return err
-	}
-
-	// Decode the proof value into an EVM State
-	var es ES
-	err = protobuf.Decode(value, &es)
-	if err != nil {
-		return err
-	}
-	evmDb, err := NewEvmDb(&es)
+	// Retrieve the EVM state
+	evmDb, err := getEvmDb(bct.cl, instID)
 	if err != nil {
 		return err
 	}
@@ -216,58 +275,66 @@ func Test_InvokeToken(t *testing.T) {
 	// Spawn a new BEVM instance
 	instID := bct.createInstance(byzcoin.Arguments{})
 
+	// Initialize two accounts
+	a, err := NewEvmAccount(testAddresses[0], testPrivateKeys[0])
+	require.Nil(t, err)
+	b, err := NewEvmAccount(testAddresses[1], testPrivateKeys[1])
+	require.Nil(t, err)
+
+	// Credit the accounts
+	bct.creditAccounts(instID, big.NewInt(5*WeiPerEther), a.Address, b.Address)
+
+	// Deploy an ERC20 Token contract
 	erc20Contract, err := getSmartContract("ERC20Token")
 	require.Nil(t, err)
-
-	/*
-		A, AKey := GenerateKeys()
-		B, BKey := GenerateKeys()
-	*/
-	a, err := NewEvmAccount(
-		"0x627306090abab3a6e1400e9345bc60c78a8bef57",
-		"c87509a1c067bbde78beb793e6fa76530b6382a4c0241e5e4a9ec0a0f44dc0d3")
-	require.Nil(t, err)
-	b, err := NewEvmAccount(
-		"0xf17f52151ebef6c7334fad080c5704d77216b732",
-		"ae6ae8e5ccbfb04590405997ee2d52d2b330726137b875053c36d94e974d162f")
-	require.Nil(t, err)
-
-	bct.creditAccounts(instID, 5*WeiPerEther, a.Address, b.Address)
 	err = bct.deploy(instID, txParams, 0, a, erc20Contract)
+	require.Nil(t, err)
+
+	// Rretrieve the total supply
+	supply := big.NewInt(0)
+	err = bct.call(instID, a, &supply, *erc20Contract, "totalSupply")
 	require.Nil(t, err)
 
 	balance := big.NewInt(0)
 
+	// A's initial balance should be the total supply, as he is the owner
 	err = bct.call(instID, a, &balance, *erc20Contract, "balanceOf", a.Address)
 	require.Nil(t, err)
-	fmt.Printf("Balance of A --> %d\n", balance)
+	require.Equal(t, supply, balance)
+
+	// B's initial balance should be empty
 	err = bct.call(instID, a, &balance, *erc20Contract, "balanceOf", b.Address)
 	require.Nil(t, err)
-	fmt.Printf("Balance of B --> %d\n", balance)
+	assertBigInt0(t, balance)
 
+	// Transfer 100 tokens from A to B
 	err = bct.transact(instID, txParams, 0, a, *erc20Contract, "transfer", b.Address, big.NewInt(100))
 	require.Nil(t, err)
 
-	bct.displayAccounts(instID, a.Address, b.Address)
+	// Check the new balances
+	newA := new(big.Int).Sub(supply, big.NewInt(100))
+	newB := big.NewInt(100)
 
 	err = bct.call(instID, a, &balance, *erc20Contract, "balanceOf", a.Address)
 	require.Nil(t, err)
-	fmt.Printf("Balance of A --> %d\n", balance)
+	require.Equal(t, newA, balance)
+
 	err = bct.call(instID, a, &balance, *erc20Contract, "balanceOf", b.Address)
 	require.Nil(t, err)
-	fmt.Printf("Balance of B --> %d\n", balance)
+	require.Equal(t, newB, balance)
 
+	// Try to transfer 101 tokens from B to A; this should fail
 	err = bct.transact(instID, txParams, 0, b, *erc20Contract, "transfer", a.Address, big.NewInt(101))
 	require.Nil(t, err)
 
+	// Check that the balances have not changed
 	err = bct.call(instID, a, &balance, *erc20Contract, "balanceOf", a.Address)
 	require.Nil(t, err)
-	fmt.Printf("Balance of A --> %d\n", balance)
+	require.Equal(t, newA, balance)
+
 	err = bct.call(instID, a, &balance, *erc20Contract, "balanceOf", b.Address)
 	require.Nil(t, err)
-	fmt.Printf("Balance of B --> %d\n", balance)
-
-	bct.displayAccounts(instID, a.Address, b.Address)
+	require.Equal(t, newB, balance)
 }
 
 func Test_InvokeLoanContract(t *testing.T) {
@@ -280,72 +347,87 @@ func Test_InvokeLoanContract(t *testing.T) {
 	// Spawn a new BEVM instance
 	instID := bct.createInstance(byzcoin.Arguments{})
 
-	// Fetch LoanContract ABI and bytecode
-	loanContract, err := getSmartContract("LoanContract")
+	// Initialize two accounts
+	a, err := NewEvmAccount(testAddresses[0], testPrivateKeys[0])
+	require.Nil(t, err)
+	b, err := NewEvmAccount(testAddresses[1], testPrivateKeys[1])
 	require.Nil(t, err)
 
-	// Fetch erc20 bytecode
+	// Credit the accounts
+	bct.creditAccounts(instID, big.NewInt(5*WeiPerEther), a.Address, b.Address)
+
+	// Deploy an ERC20 Token contract
 	erc20Contract, err := getSmartContract("ERC20Token")
 	require.Nil(t, err)
-
-	/*
-		A, AKey := GenerateKeys()
-		B, Bkey := GenerateKeys()
-	*/
-	a, err := NewEvmAccount(
-		"0x627306090abab3a6e1400e9345bc60c78a8bef57",
-		"c87509a1c067bbde78beb793e6fa76530b6382a4c0241e5e4a9ec0a0f44dc0d3")
-	require.Nil(t, err)
-	b, err := NewEvmAccount(
-		"0xf17f52151ebef6c7334fad080c5704d77216b732",
-		"ae6ae8e5ccbfb04590405997ee2d52d2b330726137b875053c36d94e974d162f")
-	require.Nil(t, err)
-
-	bct.creditAccounts(instID, 5*WeiPerEther, a.Address, b.Address)
-	bct.displayAccounts(instID, a.Address, b.Address)
-
 	err = bct.deploy(instID, txParams, 0, a, erc20Contract)
 	require.Nil(t, err)
-	log.LLvl1("erc20 deployed", erc20Contract)
 
-	//Constructor LoanContract
-	//constructor (uint256 _wantedAmount, uint256 _interest, uint256 _tokenAmount, string _tokenName, ERC20Token _tokenContractAddress, uint256 _length) public {
-	err = bct.deploy(instID, txParams, 0, a, loanContract,
-		big.NewInt(1*1e18), big.NewInt(0), big.NewInt(10000), "TestCoin", erc20Contract.Address, big.NewInt(0))
+	// Deploy a Loan contract
+	guarantee := big.NewInt(10000)
+	loanAmount := big.NewInt(1.5 * WeiPerEther)
+
+	loanContract, err := getSmartContract("LoanContract")
 	require.Nil(t, err)
-	log.LLvl1("LoanContract deployed", loanContract)
+	err = bct.deploy(instID, txParams, 0, a, loanContract,
+		loanAmount,            // wantedAmount: the amount in Ether that the borrower wants to borrow
+		big.NewInt(0),         // interest: the amount in Ether that the borrower will pay pack in addition to the borrowed amount
+		guarantee,             // tokenAmount: the number of tokens provided by the borrower as guarantee
+		"TestCoin",            // tokenName: the name of the tokens used as guarantee
+		erc20Contract.Address, // tokenContractAddress: the address of the ERC20 contract handling the tokens used as guarantee
+		big.NewInt(0),         // length: the duration of the loan, in days
+	)
+	require.Nil(t, err)
 
-	// Check if there are enough tokens
+	getBalances := func(account *EvmAccount, address common.Address) (tokenBalance, balance *big.Int) {
+		tokenBalance = big.NewInt(0)
+		err = bct.call(instID, account, &tokenBalance, *erc20Contract, "balanceOf", address)
+		require.Nil(t, err)
+		balance, err = getAccountBalance(bct.cl, instID, address)
+		require.Nil(t, err)
+		return
+	}
+
+	var tokBal, bal *big.Int
+
+	// Initially, the guarantee is empty
+	tokBal, _ = getBalances(a, loanContract.Address)
+	initTokBalA, _ := getBalances(a, a.Address)
+	assertBigInt0(t, tokBal)
+
+	// Transfer tokens from A as a guarantee (A owns all the tokens as he deployed the Token contract)
+	err = bct.transact(instID, txParams, 0, a, *erc20Contract, "transfer", loanContract.Address, guarantee)
+	require.Nil(t, err)
+
+	tokBal, _ = getBalances(a, a.Address)
+	newTokBalA := new(big.Int).Sub(initTokBalA, guarantee)
+	require.Equal(t, newTokBalA, tokBal)
+
+	tokBal, _ = getBalances(a, loanContract.Address)
+	require.Equal(t, guarantee, tokBal)
+
+	// Check that there are enough tokens
 	err = bct.transact(instID, txParams, 0, a, *loanContract, "checkTokens")
 	require.Nil(t, err)
-	log.LLvl1("check tokens passed")
 
-	log.LLvl1("test avant lend")
-	bct.displayAccounts(instID, a.Address, b.Address, loanContract.Address)
+	// Lend
+	_, initEtherBalA := getBalances(a, a.Address)
 
-	/*
-		balanceOfTest, err := abiMethodPack(erc20ABI, "balanceOf", common.HexToAddress(A))
-		require.Nil(t, err)
-		nonceA = transact(nonceA, 0, string(balanceOfTest), erc20Address.Hex(), AKey)
-		log.LLvl1("balance of test")
-
-		/*
-		log.LLvl1("transafering token from B which has no tokens")
-		checkBalance, err := abiMethodPack(erc20ABI, "transfer", common.HexToAddress(A), big.NewInt(1))
-		require.Nil(t, err)
-		nonceB = transact(nonceB, 0, string(checkBalance), erc20Address.Hex(), Bkey)
-		log.LLvl1("this should fail")
-	*/
-
-	err = bct.transact(instID, txParams, 2*WeiPerEther, b, *loanContract, "lend")
+	err = bct.transact(instID, txParams, loanAmount.Uint64(), b, *loanContract, "lend")
 	require.Nil(t, err)
 
-	bct.displayAccounts(instID, a.Address, b.Address, loanContract.Address)
+	_, bal = getBalances(a, a.Address)
+	newEtherBalA := new(big.Int).Add(initEtherBalA, loanAmount)
+	require.Equal(t, newEtherBalA, bal)
 
-	err = bct.transact(instID, txParams, 2*WeiPerEther, a, *loanContract, "payback")
+	// Pay back
+	_, initEtherBalB := getBalances(a, b.Address)
+
+	err = bct.transact(instID, txParams, loanAmount.Uint64(), a, *loanContract, "payback")
 	require.Nil(t, err)
 
-	bct.displayAccounts(instID, a.Address, b.Address, loanContract.Address)
+	_, bal = getBalances(a, b.Address)
+	newEtherBalB := new(big.Int).Add(initEtherBalB, loanAmount)
+	require.Equal(t, newEtherBalB, bal)
 }
 
 //Signs the transaction with a private key and returns the transaction in byte format, ready to be included into the Byzcoin transaction
