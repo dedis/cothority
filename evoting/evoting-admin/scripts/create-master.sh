324a9806bb627944e837cef2d37a6c5b097b4653
Jeff R. Allen <jeff.allen@epfl.ch>
2019-05-01 16:55:56 +0200

    Usability improvements

--- evoting/evoting-admin/scripts/create-master.sh
+++ evoting/evoting-admin/scripts/create-master.sh
@@ -5,12 +5,18 @@ if [ -z "$PIN" ]; then
         exit 1
 fi
 
+if [ -z "$1" ]; then
+	echo "Need roster.toml as first argument."
+	exit 1
+fi
+
 sc_jallen=289938
 sc_lindo=128871
 sc_giovanni=121769
-admins=$sc_jallen,$sc_lindo,$sc_giovanni
+sc_nkcr=228271
+admins=$sc_jallen,$sc_lindo,$sc_giovanni,$sc_nkcr
 
-../evoting-admin \
-	-roster roster.toml \
+evoting-admin \
+	-roster $1 \
 	-pin $PIN \
 	-admins $admins
