013fc99903ef10d67c7dc92058102bdc99c3a121
Noémien Kocher <nkcr.je@gmail.com>
2019-05-09 15:21:36 +0200

    bcadmin: adds --redirect for value contract
    When using
    
    ```
    $ bcadmin contract value spawn --value myValue
    ```
    
    with the `--redirect` option, it does not spawn the value contract but redirects the transaction to stdout, later allowing this output to be used as the input of a spawn:deferred with pipe. (`bcadmin contract deferred spawn`)
    
    This is a small step towards using bcadmin with deferred contract.

--- byzcoin/bcadmin/clicontracts/value_test.sh
+++ byzcoin/bcadmin/clicontracts/value_test.sh
@@ -1,7 +1,12 @@
 # This method should be called from the byzcoin/bcadmin/test.sh script
 
 testContractValue() {
-    # In this test we spawn a value contract and then update its value
+    run testSpawn
+    run testSpawnRedirect
+}
+
+testSpawn() {
+   # In this test we spawn a value contract and then update its value
     runCoBG 1 2 3
     runGrepSed "export BC=" "" runBA create --roster public.toml --interval .5s
     eval $SED
@@ -12,9 +17,9 @@ testContractValue() {
     ID=`cat ./darc_id.txt`
     KEY=`cat ./darc_key.txt`
     testOK runBA darc rule -rule "spawn:value" --identity "$KEY" --darc "$ID" --sign "$KEY"
+    testOK runBA darc rule -rule "invoke:value.update" --identity "$KEY" --darc "$ID" --sign "$KEY"
 
     # Spawn a new value contract, we save the output to the res.txt file
-    testOK runBA darc rule -rule "invoke:value.update" --identity "$KEY" --darc "$ID" --sign "$KEY"
     OUTFILE=res.txt && testOK runBA contract value spawn --value "myValue" --darc "$ID" --sign "$KEY"
     OUTFILE=""
 
@@ -26,4 +31,26 @@ testContractValue() {
 
     # Update the value instance based on the instance ID
     testOK runBA contract value invoke update --value "newValue" --instID $VALUE_INSTANCE_ID --darc "$ID" --sign "$KEY"
+}
+
+testSpawnRedirect() {
+   # In this test we spawn a value with the --redirect flag
+    runCoBG 1 2 3
+    runGrepSed "export BC=" "" runBA create --roster public.toml --interval .5s
+    eval $SED
+    [ -z "$BC" ] && exit 1
+
+    # Add the spawn:value and invoke:value.update rules
+    testOK runBA darc add -out_id ./darc_id.txt -out_key ./darc_key.txt -unrestricted
+    ID=`cat ./darc_id.txt`
+    KEY=`cat ./darc_key.txt`
+    testOK runBA darc rule -rule "spawn:value" --identity "$KEY" --darc "$ID" --sign "$KEY"
+    testOK runBA darc rule -rule "invoke:value.update" --identity "$KEY" --darc "$ID" --sign "$KEY"
+
+    # Spawn a new value contract, we save the output to the res.txt file
+    OUTFILE=res.txt && testOK runBA contract value spawn --value "myValue" --redirect --darc "$ID" --sign "$KEY"
+    OUTFILE=""
+
+    # Check if we got the expected output
+    testGrep "myValue" cat res.txt
 }
