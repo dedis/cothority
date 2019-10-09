460e0020dc97e8696e49adfa093b2df418e7b9fe
Linus Gasser <linus.gasser@epfl.ch>
2019-10-09 12:58:24 +0200

    add calypso ropascis to personhood
    The new personhood Rock-Paper-Scissors contract allows to use calypso for
    making sure that the 2nd player can finalize the game. This adds the
    javascript support for this functionality.
    
    This is a cherry-pick to v3 of #2093.
    
    Fixes #2134.

--- external/js/cothority/src/personhood/pop-party-instance.ts
+++ external/js/cothority/src/personhood/pop-party-instance.ts
@@ -116,7 +116,7 @@ export class PopPartyInstance extends Instance {
     /**
      * Returns a copy of the list of all attendees.
      */
-    getAttendees(): Point[] {
+    getTmpAttendees(): Point[] {
         return this.tmpAttendees.slice();
     }
 
