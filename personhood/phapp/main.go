520bb80a34d4e6a8832daa51fceb11aaf7b1d602
Jeff R. Allen <jeff.allen@epfl.ch>
2019-11-26 17:05:46 +0100

    Fix broken merge.

--- personhood/phapp/main.go
+++ personhood/phapp/main.go
@@ -17,6 +17,7 @@ import (
 	"go.dedis.ch/cothority/v3/darc"
 	"go.dedis.ch/cothority/v3/darc/expression"
 	"go.dedis.ch/cothority/v3/personhood"
+	"go.dedis.ch/onet/v3/app"
 	"go.dedis.ch/onet/v3/cfgpath"
 	"go.dedis.ch/onet/v3/log"
 	"go.dedis.ch/onet/v3/network"
