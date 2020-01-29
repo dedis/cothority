f0ab8621a02e99e365343231bcdff438a21ec27b
Noémien Kocher <nkcr.je@gmail.com>
2020-01-29 16:37:25 +0100

    Updates the jasmine test on PaginateRequest

--- external/js/cothority/spec/byzcoin/stream.spec.ts
+++ external/js/cothority/spec/byzcoin/stream.spec.ts
@@ -5,7 +5,7 @@ import { WebSocketConnection } from "../../src/network/connection";
 import { SkipchainRPC } from "../../src/skipchain";
 import { BLOCK_INTERVAL, ROSTER, SIGNER, startConodes } from "../support/conondes";
 
-describe("Stream Tests", () => {
+fdescribe("Stream Tests", () => {
     const roster = ROSTER.slice(0, 4);
     let originalTimeout: number;
 
@@ -15,26 +15,32 @@ describe("Stream Tests", () => {
         jasmine.DEFAULT_TIMEOUT_INTERVAL = 2000;
     });
 
-    it("should send and receive data", async (done) => {
+    it("should send a PaginateRequest and receive a PaginateResponse", async (done) => {
         const darc = ByzCoinRPC.makeGenesisDarc([SIGNER], roster);
         const rpc = await ByzCoinRPC.newByzCoinRPC(roster, darc, BLOCK_INTERVAL);
 
-        const conn = new WebSocketConnection(roster.list[0].getWebSocketAddress(), SkipchainRPC.serviceName);
+        const conn = new WebSocketConnection(roster.list[0].getWebSocketAddress(), ByzCoinRPC.serviceName);
 
         const msg = new PaginateRequest({startid: rpc.genesisID, pagesize: 1, numpages: 1,  backward: false});
 
         const foo = {
             onClose: (code: number, reason: string) => {
-                fail("onClose should not be called");
+                expect(code).toEqual(1000);
                 done();
             },
             onError: (err: Error) => {
-                fail("onError should not be called: " + err);
+                fail("onError should not be called: " + err.message);
                 done();
             },
             onMessage: (message: PaginateResponse, ws: WebSocketAdapter) => {
                 expect(message.blocks.length).toEqual(1);
-                done();
+                expect(message.blocks[0].hash).toEqual(rpc.genesisID);
+                expect(message.backward).toBe(false);
+                expect(message.errorcode.toString()).toEqual("0");
+                expect(message.errortext.length).toEqual(0);
+                expect(message.pagenumber.toString()).toEqual("0");
+                expect(message.streamid.toString("hex")).not.toEqual("");
+                ws.close(1000);
             },
         };
 
