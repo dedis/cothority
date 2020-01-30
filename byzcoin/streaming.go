380161c9240d8c272d88746fb543199436bc856c
Noémien Kocher <nkcr.je@gmail.com>
2020-01-30 11:18:04 +0100

    Adds a PaginateBlocks service to byzcoin

--- byzcoin/streaming.go
+++ byzcoin/streaming.go
@@ -1,14 +1,30 @@
 package byzcoin
 
 import (
+	"fmt"
+	"math/rand"
 	"sync"
+
+	"golang.org/x/xerrors"
 
 	"go.dedis.ch/cothority/v3/skipchain"
 	"go.dedis.ch/onet/v3/network"
 )
 
+var paginateChans = paginateChansWrapper{
+	chans:     make(map[string]chan *PaginateResponse),
+	closeChan: make(map[string]chan bool),
+}
+
+type paginateChansWrapper struct {
+	sync.Mutex
+	chans     map[string]chan *PaginateResponse
+	closeChan map[string]chan bool
+}
+
 func init() {
-	network.RegisterMessages(&StreamingRequest{}, &StreamingResponse{})
+	network.RegisterMessages(&StreamingRequest{}, &StreamingResponse{},
+		&PaginateRequest{}, &PaginateResponse{})
 }
 
 type streamingManager struct {
@@ -102,6 +118,185 @@ func (s *Service) StreamTransactions(msg *StreamingRequest) (chan *StreamingResp
 		<-stopChan
 		// In both cases we clean the listener.
 		s.streamingMan.stopListener(key, outChan)
+	}()
+	return outChan, stopChan, nil
+}
+
+// PaginateBlocks return blocks with pagination, ie. N asynchounous requests
+// that contain each K consecutive blocks. If a streamID is given, it uses the
+// same PaginateResponse chan AND the same close chan. The caller is responsible
+// for closing the close chan when the caller wants to close the connection. The
+// close chan should be closed only when no other requests are being processed using
+// the same streamID.
+func (s *Service) PaginateBlocks(msg *PaginateRequest) (chan *PaginateResponse, chan bool, error) {
+	key := msg.StreamID
+	paginateChans.Lock()
+	if key == nil || len(key) == 0 {
+		key = make([]byte, 32)
+		rand.Read(key)
+		paginateChans.chans[string(key)] = make(chan *PaginateResponse)
+		paginateChans.closeChan[string(key)] = make(chan bool)
+	}
+
+	stopChan, okStop := paginateChans.closeChan[string(key)]
+	outChan, okChan := paginateChans.chans[string(key)]
+	paginateChans.Unlock()
+
+	if !okStop {
+		outChan = make(chan *PaginateResponse)
+		go func() {
+			outChan <- &PaginateResponse{
+				StreamID:  key,
+				ErrorCode: 1,
+				ErrorText: []string{fmt.Sprintf("StreamID %x unkown for closeChan", msg.StreamID)},
+			}
+		}()
+		return outChan, stopChan, xerrors.Errorf("StreamID %x unkown for closeChan", msg.StreamID)
+	}
+
+	if !okChan {
+		outChan = make(chan *PaginateResponse)
+		go func() {
+			outChan <- &PaginateResponse{
+				StreamID:  key,
+				ErrorCode: 1,
+				ErrorText: []string{fmt.Sprintf("StreamID %x unkown for chans", msg.StreamID)},
+			}
+		}()
+		return outChan, stopChan, xerrors.Errorf("StreamID %x unkown for chans", msg.StreamID)
+	}
+
+	go func() {
+
+		if msg.PageSize < 1 {
+			outChan <- &PaginateResponse{
+				StreamID:  key,
+				ErrorCode: 1,
+				ErrorText: []string{fmt.Sprintf("PageSize should be >= 1, "+
+					"but we found %d", msg.PageSize)},
+			}
+			return
+		}
+
+		if msg.NumPages < 1 {
+			outChan <- &PaginateResponse{
+				StreamID:  key,
+				ErrorCode: 1,
+				ErrorText: []string{fmt.Sprintf("NumPages should be >= 1, "+
+					"but we found %d", msg.NumPages)},
+			}
+			return
+		}
+
+		if msg.StartID == nil {
+			outChan <- &PaginateResponse{
+				StreamID:  key,
+				ErrorCode: 1,
+				ErrorText: []string{"StartID is nil"},
+			}
+			return
+		}
+
+		nextID := msg.StartID
+
+		for pageNum := uint64(0); pageNum < msg.NumPages; pageNum++ {
+			_, skipBlock, err := s.getBlockTx(nextID)
+
+			blocks := make([]*skipchain.SkipBlock, msg.PageSize)
+			if err != nil {
+				outChan <- &PaginateResponse{
+					StreamID:  key,
+					ErrorCode: 1,
+					ErrorText: []string{"failed to get the first block with ID",
+						fmt.Sprintf("%x", msg.StartID), fmt.Sprintf("%v", err)},
+				}
+				return
+			}
+			blocks[0] = skipBlock
+
+			if msg.Backward {
+				if len(skipBlock.BackLinkIDs) != 0 {
+					nextID = skipBlock.BackLinkIDs[0]
+				} else {
+					nextID = nil
+				}
+			} else {
+				if len(skipBlock.ForwardLink) != 0 {
+					nextID = skipBlock.ForwardLink[0].To
+				} else {
+					nextID = nil
+				}
+			}
+
+			for i := uint64(1); i < msg.PageSize; i++ {
+
+				if nextID == nil {
+					outChan <- &PaginateResponse{
+						StreamID:  key,
+						ErrorCode: 1,
+						ErrorText: []string{"couldn't find a nextID for block",
+							fmt.Sprintf("%x", skipBlock.Hash), "page number",
+							fmt.Sprintf("%d", pageNum), "index", fmt.Sprintf("%d", i)},
+					}
+					return
+				}
+
+				_, skipBlock, err = s.getBlockTx(nextID)
+				if err != nil {
+					outChan <- &PaginateResponse{
+						StreamID:  key,
+						ErrorCode: 1,
+						ErrorText: []string{"failed to get block with ID",
+							fmt.Sprintf("%x", nextID), "page number",
+							fmt.Sprintf("%d", pageNum), "index",
+							fmt.Sprintf("%d", i), fmt.Sprintf("%v", err)},
+					}
+					return
+				}
+				blocks[i] = skipBlock
+
+				if msg.Backward {
+					if len(skipBlock.BackLinkIDs) != 0 {
+						nextID = skipBlock.BackLinkIDs[0]
+					} else {
+						nextID = nil
+					}
+				} else {
+					if len(skipBlock.ForwardLink) != 0 {
+						nextID = skipBlock.ForwardLink[0].To
+					} else {
+						nextID = nil
+					}
+				}
+			}
+			response := &PaginateResponse{
+				Blocks:     blocks,
+				PageNumber: pageNum,
+				StreamID:   key,
+				Backward:   msg.Backward,
+			}
+			// Allows the service to exit prematurely if the connection stops
+			select {
+			case <-stopChan:
+				return
+			default:
+				outChan <- response
+			}
+		}
+		// Waiting for the streaming connection to stop. This signal comes
+		// from onet, which sets it when the client closes the connection.
+		<-stopChan
+		paginateChans.Lock()
+		_, ok := paginateChans.chans[string(key)]
+		if ok {
+			delete(paginateChans.chans, string(key))
+		}
+		_, ok = paginateChans.closeChan[string(key)]
+		if ok {
+			close(outChan)
+			delete(paginateChans.closeChan, string(key))
+		}
+		paginateChans.Unlock()
 	}()
 	return outChan, stopChan, nil
 }
---
+++ byzcoin/streaming_test.go
@@ -0,0 +1,402 @@
+package byzcoin
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+	"go.dedis.ch/cothority/v3/skipchain"
+)
+
+var chanTimeout = time.Millisecond * 100
+
+func TestStreamingService_PaginateBlocks(t *testing.T) {
+	// Creating a service with only the genesis block
+	s := newSerN(t, 1, testInterval, 4, disableViewChange)
+	defer s.local.CloseAll()
+	service := s.service()
+
+	// We should be able to get 1 page with one item, which is the genesis block
+	paginateRequest := &PaginateRequest{
+		StartID:  s.genesis.Hash,
+		PageSize: 1,
+		NumPages: 1,
+		Backward: false,
+		StreamID: nil,
+	}
+	paginateResponse, closeChan, err := service.PaginateBlocks(paginateRequest)
+	require.NoError(t, err)
+
+	select {
+	case response := <-paginateResponse:
+		if response.ErrorCode != 0 {
+			t.Errorf("expected to find error code 0, but found %d, here are "+
+				"the messages: %v", response.ErrorCode, response.ErrorText)
+		}
+		require.Equal(t, 1, len(response.Blocks))
+		require.Equal(t, response.Blocks[0].Hash, s.genesis.Hash)
+	case <-time.After(chanTimeout):
+		t.Fatal("didn't get a papginateResponse in the channel after timeout")
+	}
+	select {
+	case <-paginateResponse:
+		t.Fatal("there shouldn't be additional element in the channel")
+	case <-time.After(chanTimeout):
+	}
+
+	close(closeChan)
+
+	// Trying to fetch 2 items per page should raise an error since there is
+	// only the genesis block
+	paginateRequest = &PaginateRequest{
+		StartID:  s.genesis.Hash,
+		PageSize: 2,
+		NumPages: 1,
+		Backward: false,
+		StreamID: nil,
+	}
+	paginateResponse, closeChan, err = service.PaginateBlocks(paginateRequest)
+	require.NoError(t, err)
+
+	select {
+	case response := <-paginateResponse:
+		require.Greater(t, response.ErrorCode, uint64(0))
+		require.Equal(t, 0, len(response.Blocks))
+	case <-time.After(chanTimeout):
+		t.Fatal("didn't get a papginateResponse in the channel after timeout")
+	}
+	select {
+	case <-paginateResponse:
+		t.Fatal("there shouldn't be additional element in the channel")
+	case <-time.After(chanTimeout):
+	}
+
+	close(closeChan)
+
+	// Trying to fetch 2 pages should raise an error in the second page since
+	// there is only the genesis block
+	paginateRequest = &PaginateRequest{
+		StartID:  s.genesis.Hash,
+		PageSize: 1,
+		NumPages: 2,
+		Backward: false,
+		StreamID: nil,
+	}
+	paginateResponse, closeChan, err = service.PaginateBlocks(paginateRequest)
+	require.NoError(t, err)
+
+	select {
+	case response := <-paginateResponse:
+		if response.ErrorCode != 0 {
+			t.Errorf("expected to find error code 0, but found %d, here are "+
+				"the messages: %v", response.ErrorCode, response.ErrorText)
+		}
+		require.Equal(t, 1, len(response.Blocks))
+		require.Equal(t, response.Blocks[0].Hash, s.genesis.Hash)
+	case <-time.After(chanTimeout):
+		t.Fatal("didn't get a papginateResponse in the channel after timeout")
+	}
+	select {
+	case response := <-paginateResponse:
+		require.Greater(t, response.ErrorCode, uint64(0))
+		require.Equal(t, 0, len(response.Blocks))
+	case <-time.After(chanTimeout):
+		t.Fatal("didn't get a papginateResponse in the channel after timeout")
+	}
+	select {
+	case <-paginateResponse:
+		t.Fatal("there shouldn't be additional element in the channel")
+	case <-time.After(chanTimeout):
+	}
+
+	close(closeChan)
+
+	// Adding a new block so we can fetch a page of two blocks, or two pages
+	// with one item each.
+	tx, err := createOneClientTx(s.darc.GetBaseID(), dummyContract, s.value, s.signer)
+	require.NoError(t, err)
+	s.tx = tx
+	resp, err := s.service().AddTransaction(&AddTxRequest{
+		Version:       CurrentVersion,
+		SkipchainID:   s.genesis.SkipChainID(),
+		Transaction:   tx,
+		InclusionWait: 10,
+	})
+	transactionOK(t, resp, err)
+
+	// Fetching two items in one page
+	paginateRequest = &PaginateRequest{
+		StartID:  s.genesis.Hash,
+		PageSize: 2,
+		NumPages: 1,
+		Backward: false,
+		StreamID: nil,
+	}
+	paginateResponse, closeChan, err = service.PaginateBlocks(paginateRequest)
+	require.NoError(t, err)
+	var secondBlockHash skipchain.SkipBlockID
+
+	select {
+	case response := <-paginateResponse:
+		if response.ErrorCode != 0 {
+			t.Errorf("expected to find error code 0, but found %d, here are "+
+				"the messages: %v", response.ErrorCode, response.ErrorText)
+		}
+		require.Equal(t, 2, len(response.Blocks))
+		require.Equal(t, response.Blocks[0].Hash, s.genesis.Hash)
+		secondBlockHash = response.Blocks[1].Hash
+	case <-time.After(chanTimeout):
+		t.Fatal("didn't get a papginateResponse in the channel after timeout")
+	}
+	select {
+	case <-paginateResponse:
+		t.Fatal("there shouldn't be additional element in the channel")
+	case <-time.After(chanTimeout):
+	}
+
+	close(closeChan)
+
+	// Fecthing two pages with 1 item each
+	paginateRequest = &PaginateRequest{
+		StartID:  s.genesis.Hash,
+		PageSize: 1,
+		NumPages: 2,
+		Backward: false,
+		StreamID: nil,
+	}
+	paginateResponse, closeChan, err = service.PaginateBlocks(paginateRequest)
+	require.NoError(t, err)
+
+	select {
+	case response := <-paginateResponse:
+		if response.ErrorCode != 0 {
+			t.Errorf("expected to find error code 0, but found %d, here are "+
+				"the messages: %v", response.ErrorCode, response.ErrorText)
+		}
+		require.Equal(t, 1, len(response.Blocks))
+		require.Equal(t, response.Blocks[0].Hash, s.genesis.Hash)
+	case <-time.After(chanTimeout):
+		t.Fatal("didn't get a papginateResponse in the channel after timeout")
+	}
+	select {
+	case response := <-paginateResponse:
+		if response.ErrorCode != 0 {
+			t.Errorf("expected to find error code 0, but found %d, here are "+
+				"the messages: %v", response.ErrorCode, response.ErrorText)
+		}
+		require.Equal(t, 1, len(response.Blocks))
+		require.Equal(t, response.Blocks[0].Hash, secondBlockHash)
+	case <-time.After(chanTimeout):
+		t.Fatal("didn't get a papginateResponse in the channel after timeout")
+	}
+	select {
+	case <-paginateResponse:
+		t.Fatal("there shouldn't be additional element in the channel")
+	case <-time.After(chanTimeout):
+	}
+
+	close(closeChan)
+
+	// If we get the page in reverse order from the genesis block we should get
+	// an error
+	paginateRequest = &PaginateRequest{
+		StartID:  s.genesis.Hash,
+		PageSize: 2,
+		NumPages: 1,
+		Backward: true,
+		StreamID: nil,
+	}
+	paginateResponse, closeChan, err = service.PaginateBlocks(paginateRequest)
+	require.NoError(t, err)
+
+	select {
+	case response := <-paginateResponse:
+		require.Greater(t, response.ErrorCode, uint64(0))
+		require.Equal(t, 0, len(response.Blocks))
+	case <-time.After(chanTimeout):
+		t.Fatal("didn't get a papginateResponse in the channel after timeout")
+	}
+	select {
+	case <-paginateResponse:
+		t.Fatal("there shouldn't be additional element in the channel")
+	case <-time.After(chanTimeout):
+	}
+
+	close(closeChan)
+
+	// Trying to fetch 2 pages from the genesis block in reverse order should
+	// raise an error in the second page
+	paginateRequest = &PaginateRequest{
+		StartID:  s.genesis.Hash,
+		PageSize: 1,
+		NumPages: 2,
+		Backward: true,
+		StreamID: nil,
+	}
+	paginateResponse, closeChan, err = service.PaginateBlocks(paginateRequest)
+	require.NoError(t, err)
+
+	select {
+	case response := <-paginateResponse:
+		if response.ErrorCode != 0 {
+			t.Errorf("expected to find error code 0, but found %d, here are "+
+				"the messages: %v", response.ErrorCode, response.ErrorText)
+		}
+		require.Equal(t, 1, len(response.Blocks))
+		require.Equal(t, response.Blocks[0].Hash, s.genesis.Hash)
+	case <-time.After(chanTimeout):
+		t.Fatal("didn't get a papginateResponse in the channel after timeout")
+	}
+	select {
+	case response := <-paginateResponse:
+		require.Greater(t, response.ErrorCode, uint64(0))
+		require.Equal(t, 0, len(response.Blocks))
+	case <-time.After(chanTimeout):
+		t.Fatal("didn't get a papginateResponse in the channel after timeout")
+	}
+	select {
+	case <-paginateResponse:
+		t.Fatal("there shouldn't be additional element in the channel")
+	case <-time.After(chanTimeout):
+	}
+
+	close(closeChan)
+
+	// Fetching two items in one page from the second block in reverse order
+	// should be allright
+	paginateRequest = &PaginateRequest{
+		StartID:  secondBlockHash,
+		PageSize: 2,
+		NumPages: 1,
+		Backward: true,
+		StreamID: nil,
+	}
+	paginateResponse, closeChan, err = service.PaginateBlocks(paginateRequest)
+	require.NoError(t, err)
+
+	select {
+	case response := <-paginateResponse:
+		if response.ErrorCode != 0 {
+			t.Errorf("expected to find error code 0, but found %d, here are "+
+				"the messages: %v", response.ErrorCode, response.ErrorText)
+		}
+		require.Equal(t, 2, len(response.Blocks))
+		require.Equal(t, response.Blocks[0].Hash, secondBlockHash)
+		require.Equal(t, response.Blocks[1].Hash, s.genesis.Hash)
+	case <-time.After(chanTimeout):
+		t.Fatal("didn't get a papginateResponse in the channel after timeout")
+	}
+	select {
+	case <-paginateResponse:
+		t.Fatal("there shouldn't be additional element in the channel")
+	case <-time.After(chanTimeout):
+	}
+
+	close(closeChan)
+
+	// Fecthing two pages with 1 item each per page from the second block should
+	// also be allright
+	paginateRequest = &PaginateRequest{
+		StartID:  secondBlockHash,
+		PageSize: 1,
+		NumPages: 2,
+		Backward: true,
+		StreamID: nil,
+	}
+	paginateResponse, closeChan, err = service.PaginateBlocks(paginateRequest)
+	require.NoError(t, err)
+
+	select {
+	case response := <-paginateResponse:
+		if response.ErrorCode != 0 {
+			t.Errorf("expected to find error code 0, but found %d, here are "+
+				"the messages: %v", response.ErrorCode, response.ErrorText)
+		}
+		require.Equal(t, 1, len(response.Blocks))
+		require.Equal(t, response.Blocks[0].Hash, secondBlockHash)
+	case <-time.After(chanTimeout):
+		t.Fatal("didn't get a papginateResponse in the channel after timeout")
+	}
+	select {
+	case response := <-paginateResponse:
+		if response.ErrorCode != 0 {
+			t.Errorf("expected to find error code 0, but found %d, here are "+
+				"the messages: %v", response.ErrorCode, response.ErrorText)
+		}
+		require.Equal(t, 1, len(response.Blocks))
+		require.Equal(t, response.Blocks[0].Hash, s.genesis.Hash)
+	case <-time.After(chanTimeout):
+		t.Fatal("didn't get a papginateResponse in the channel after timeout")
+	}
+	select {
+	case <-paginateResponse:
+		t.Fatal("there shouldn't be additional element in the channel")
+	case <-time.After(chanTimeout):
+	}
+
+	close(closeChan)
+
+	// Making two call with the second one specifying the streamID should use
+	// the same chan to return the two results.
+	paginateRequest = &PaginateRequest{
+		StartID:  s.genesis.Hash,
+		PageSize: 1,
+		NumPages: 1,
+		Backward: false,
+		StreamID: nil,
+	}
+	paginateResponse, closeChan, err = service.PaginateBlocks(paginateRequest)
+	require.NoError(t, err)
+	var streamID []byte
+
+	select {
+	case response := <-paginateResponse:
+		if response.ErrorCode != 0 {
+			t.Errorf("expected to find error code 0, but found %d, here are "+
+				"the messages: %v", response.ErrorCode, response.ErrorText)
+		}
+		require.Equal(t, 1, len(response.Blocks))
+		require.Equal(t, response.Blocks[0].Hash, s.genesis.Hash)
+		streamID = response.StreamID
+	case <-time.After(chanTimeout):
+		t.Fatal("didn't get a papginateResponse in the channel after timeout")
+	}
+
+	select {
+	case <-paginateResponse:
+		t.Fatal("there shouldn't be additional element in the channel")
+	case <-time.After(chanTimeout):
+	}
+
+	paginateRequest = &PaginateRequest{
+		StartID:  secondBlockHash,
+		PageSize: 1,
+		NumPages: 1,
+		Backward: false,
+		StreamID: streamID,
+	}
+	// Should be the same paginateResponse chan
+	_, _, err = service.PaginateBlocks(paginateRequest)
+	require.NoError(t, err)
+
+	select {
+	case response := <-paginateResponse:
+		if response.ErrorCode != 0 {
+			t.Errorf("expected to find error code 0, but found %d, here are "+
+				"the messages: %v", response.ErrorCode, response.ErrorText)
+		}
+		require.Equal(t, 1, len(response.Blocks))
+		require.Equal(t, response.Blocks[0].Hash, secondBlockHash)
+		streamID = response.StreamID
+	case <-time.After(chanTimeout):
+		t.Fatal("didn't get a papginateResponse in the channel after timeout")
+	}
+
+	select {
+	case <-paginateResponse:
+		t.Fatal("there shouldn't be additional element in the channel")
+	case <-time.After(chanTimeout):
+	}
+
+	close(closeChan)
+}
