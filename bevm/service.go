bab3d8ca4460ec795e3bf81f34b2fef0e76c0187
Christian Grigis <christian.grigis@epfl.ch>
2020-02-20 10:18:33 +0100

    bevm: simplify contract registration

--- bevm/service.go
+++ bevm/service.go
@@ -2,9 +2,7 @@ package bevm
 
 import (
 	"go.dedis.ch/cothority/v3/byzcoin"
-	"go.dedis.ch/onet/v3"
 	"go.dedis.ch/onet/v3/log"
-	"golang.org/x/xerrors"
 )
 
 func init() {
@@ -13,43 +11,9 @@ func init() {
 	log.AddUserUninterestingGoroutine(
 		"go-ethereum/core.(*txSenderCacher).cache")
 
-	_, err := onet.RegisterNewService(ContractBEvmID, newServiceBEvm)
-	log.ErrFatal(err)
-	_, err = onet.RegisterNewService(ContractBEvmValueID, newServiceBEvmValue)
-	log.ErrFatal(err)
-}
-
-// Service structure for BEvm contracts
-type Service struct {
-	// We need to embed the ServiceProcessor, so that incoming messages
-	// are correctly handled.
-	*onet.ServiceProcessor
-}
-
-func newServiceBEvm(c *onet.Context) (onet.Service, error) {
-	s := &Service{
-		ServiceProcessor: onet.NewServiceProcessor(c),
-	}
-
-	err := byzcoin.RegisterContract(c, ContractBEvmID, contractBEvmFromBytes)
-	if err != nil {
-		return nil, xerrors.Errorf("failed to registe BEvm contract: %v", err)
-	}
-
-	return s, nil
-}
-
-func newServiceBEvmValue(c *onet.Context) (onet.Service, error) {
-	s := &Service{
-		ServiceProcessor: onet.NewServiceProcessor(c),
-	}
-
-	// BEvmValue does not support explicit creation, so we can pass nil
-	err := byzcoin.RegisterContract(c, ContractBEvmValueID, nil)
-	if err != nil {
-		return nil, xerrors.Errorf("failed to register BEvmValue "+
-			"contract: %v", err)
-	}
-
-	return s, nil
+	// Register contracts
+	log.ErrFatal(byzcoin.RegisterGlobalContract(ContractBEvmID,
+		contractBEvmFromBytes))
+	log.ErrFatal(byzcoin.RegisterGlobalContract(ContractBEvmValueID,
+		nil))
 }
