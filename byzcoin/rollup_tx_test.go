c5c4e8237418afd56977640c1ec3b1d8d922871b
Louis Merlin <louis_merlin1@yahoo.fr>
2020-05-15 15:31:42 +0200

    Remove useless comment in rollup_tx_test

--- byzcoin/rollup_tx_test.go
+++ byzcoin/rollup_tx_test.go
@@ -18,8 +18,6 @@ import (
 	"go.dedis.ch/onet/v3/network"
 )
 
-//var tSuite = suites.MustFind("Ed25519")
-
 // Tests a 2, 5 and 13-node system. It is good practice to test different
 // sizes of trees to make sure your protocol is stable.
 func TestNode(t *testing.T) {
