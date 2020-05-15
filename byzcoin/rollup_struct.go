9f85d5b0e0ea36de166228d22bd3565b9277a377
Louis Merlin <louis_merlin1@yahoo.fr>
2020-05-15 16:39:21 +0200

    Remove unused import

--- byzcoin/rollup_struct.go
+++ byzcoin/rollup_struct.go
@@ -7,8 +7,6 @@ with the `*onet.TreeNode` embedded. The latter is used in the handler-function
 so that it can find out who sent the message.
 */
 
-import "go.dedis.ch/onet/v3"
-
 // Name can be used from other packages to refer to this protocol.
 const Name = "Rollup"
 
