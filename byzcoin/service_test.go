3541066de25901727d1e57bf34bdb0e22a198ede
Linus Gasser <linus.gasser@epfl.ch>
2020-05-18 08:24:51 +0200

    one more

--- byzcoin/service_test.go
+++ byzcoin/service_test.go
@@ -1877,8 +1877,10 @@ func TestService_SetConfigRosterReplace(t *testing.T) {
 		ctx, _ := createConfigTxWithCounter(t, testInterval, *goodRoster, defaultMaxBlockSize, s, counter)
 		counter++
 		cl := NewClient(s.genesis.SkipChainID(), *goodRoster)
+		require.NoError(t, cl.UseNode(1))
 		resp, err := cl.AddTransactionAndWait(ctx, 10)
 		transactionOK(t, resp, err)
+		s.waitPropagation(t, -1)
 
 		log.Lvl1("Removing", goodRoster.List[0])
 		goodRoster = onet.NewRoster(goodRoster.List[1:])
@@ -1886,6 +1888,7 @@ func TestService_SetConfigRosterReplace(t *testing.T) {
 		counter++
 		resp, err = cl.AddTransactionAndWait(ctx, 10)
 		transactionOK(t, resp, err)
+		s.waitPropagation(t, -1)
 	}
 }
 
