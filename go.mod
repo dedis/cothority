aef80babfcddec929ec658877b91e87b6dc7bb3d
Linus Gasser <linus.gasser@epfl.ch>
2020-08-24 17:01:47 +0200

    Remove two gopkg.in
    Both urfave/cli and satori/uuid are now available as go-modules.

--- go.mod
+++ go.mod
@@ -38,7 +38,6 @@ require (
 	golang.org/x/sys v0.0.0-20200523222454-059865788121
 	golang.org/x/xerrors v0.0.0-20191204190536-9bdfabe68543
 	gopkg.in/natefinch/npipe.v2 v2.0.0-20160621034901-c1b8fa8bdcce // indirect
-	gopkg.in/satori/go.uuid.v1 v1.2.0
 	gopkg.in/square/go-jose.v2 v2.4.1 // indirect
 	gopkg.in/yaml.v2 v2.2.8 // indirect
 )
--- skipchain/skipchain_test.go
+++ skipchain/skipchain_test.go
@@ -9,6 +9,7 @@ import (
 	"testing"
 	"time"
 
+	uuid "github.com/satori/go.uuid"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"go.dedis.ch/cothority/v3"
@@ -19,7 +20,6 @@ import (
 	"go.dedis.ch/onet/v3/log"
 	"go.dedis.ch/onet/v3/network"
 	bbolt "go.etcd.io/bbolt"
-	uuid "gopkg.in/satori/go.uuid.v1"
 )
 
 func init() {
