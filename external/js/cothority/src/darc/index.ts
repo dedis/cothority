04e7681721ddfd5001135f1c26b94ab21fdcc2db
Linus Gasser <linus.gasser@epfl.ch>
2022-02-17 09:31:13 +0100

    Adding a new test for testing the IdentityTSM

--- external/js/cothority/src/darc/index.ts
+++ external/js/cothority/src/darc/index.ts
@@ -2,7 +2,7 @@ import Darc from "./darc";
 import IdentityDarc from "./identity-darc";
 import IdentityDid from "./identity-did";
 import IdentityEd25519 from "./identity-ed25519";
-import IdentityTsm from './identity-tsm';
+import IdentityTsm from "./identity-tsm";
 import IdentityWrapper, { IIdentity } from "./identity-wrapper";
 import Rules, { Rule } from "./rules";
 import Signer from "./signer";
